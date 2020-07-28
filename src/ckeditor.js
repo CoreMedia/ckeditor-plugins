e94da03f3920ad680b61149e6ec403d279cea480
Mark Michaelis <mark.michaelis@coremedia.com>
2020-07-28 13:28:24 +0200

    CMS-13587: CKEditor 5 Initial Commit
    This commit contains the initial result when downloading CKEditor 5 via
    Online Builder. It contains the CKEditor in Standard Mode, and some
    pre-selected plugins.
    
    LICENSE.md and README.md are provided as is, i.e., they were part of
    the original download artifact.
    
    Note, that first evaluation shows, that this setup misses some required
    configuration regarding file-/image-upload. Fixing this will be task
    of subsequent changes to this repository during the evaluation phase.
    
    See Also:
    
    * CKEditor Online Builder
      https://ckeditor.com/ckeditor-5/online-builder/
    * [CMS-13587] Evaluate the future of Studio-RichText-Editing: CKEditor 5 or what?
      https://jira.coremedia.com/browse/CMS-13587

---
+++ src/ckeditor.js
@@ -0,0 +1,69 @@
+/**
+ * @license Copyright (c) 2014-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder.js';
+import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
+import SpecialCharactersArrows from '@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows.js';
+import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js';
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js';
+import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Alignment,
+	Autosave,
+	BlockQuote,
+	Bold,
+	CKFinder,
+	CKFinderUploadAdapter,
+	Essentials,
+	Heading,
+	Image,
+	ImageUpload,
+	Indent,
+	Italic,
+	Link,
+	List,
+	Paragraph,
+	PasteFromOffice,
+	RemoveFormat,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersText,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Table,
+	TableToolbar,
+	Underline
+];
+
+export default Editor;
