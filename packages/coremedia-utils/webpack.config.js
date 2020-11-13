6f6e8327fd9d2d46539831cd3a78d87e50cd2a02
jdallman <Jens.Dallmann@coremedia.com>
2020-11-13 09:12:49 +0100

    [CMS-18463] Add babel and webpack
    I'm not sure if we need this for a library... some further investigations have to be done to get a clue how we want to deliver libraries.

---
+++ packages/coremedia-utils/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
