aeb1effaef238ceb23cecb49b55870e1ca0a00cd
jdallman <Jens.Dallmann@coremedia.com>
2020-11-13 12:27:28 +0100

    [CMS-18463] Add the most simple CKEditor generated by https://ckeditor.com/ckeditor-5/online-builder/

---
+++ app/src/ckeditor.js
@@ -0,0 +1,17 @@
+/**
+ * @license Copyright (c) 2014-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Essentials,
+	Paragraph
+];
+
+export default Editor;
