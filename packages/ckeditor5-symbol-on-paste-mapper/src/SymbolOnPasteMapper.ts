9cb8e8493ea002f46e0bea8892ea4761e3e0f38c
jdallman <Jens.Dallmann@coremedia.com>
2020-11-16 12:33:48 +0100

    [CMS-18463] Activate logging via new classes

--- packages/ckeditor5-symbol-on-paste-mapper/src/SymbolOnPasteMapper.ts
+++ packages/ckeditor5-symbol-on-paste-mapper/src/SymbolOnPasteMapper.ts
@@ -2,16 +2,17 @@ import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import Editor from "@ckeditor/ckeditor5-core/src/editor/editor";
 import PasteFromOffice, {PasteFromOfficeClipboardEventData} from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
 import Clipboard from "@ckeditor/ckeditor5-clipboard/src/clipboard";
-/*import coremedia from "@coremedia/coremedia-utils";*/
+import LoggerProvider from "@coremedia/coremedia-utils/dist/logging/LoggerProvider";
+import Logger from "@coremedia/coremedia-utils/dist/logging/Logger";
 
 export default class SymbolOnPasteMapper extends Plugin {
-  static readonly pluginName: "SymbolFontMapper";
+  static readonly pluginName: string =  "SymbolOnPasteMapper";
   // TODO[cke] Copied from PasteFromOffice... should be some set of constants
   //   which tell, if there is any font to consider for replacement.
-  static readonly msWordMatch = /<meta\s*name="?generator"?\s*content="?microsoft\s*word\s*\d+"?\/?>/i;
+  static readonly msWordMatch: RegExp = /<meta\s*name="?generator"?\s*content="?microsoft\s*word\s*\d+"?\/?>/i;
   // TODO[cke] Would be great adding context information (editor/sourceElement-id or similar)
   //    to get the actual editor we write log entries for.
-  /*private readonly logger:coremedia.Logger = coremedia.getLogger(SymbolOnPasteMapper.pluginName);*/
+  private readonly logger:Logger = LoggerProvider.getLogger(SymbolOnPasteMapper.pluginName);
 
   constructor(ed: Editor) {
     super(ed);
@@ -27,14 +28,14 @@ export default class SymbolOnPasteMapper extends Plugin {
   init(): Promise<void> | null {
     const editor = this.editor;
     const viewDocument = editor.editing.view.document;
-
+    this.logger.info("SymbolOnPastePlugin initialized");
     let clipboard = editor.plugins.get('Clipboard');
     if (clipboard instanceof Clipboard) {
       clipboard.on(
         'inputTransformation',
         (evt: any, data: PasteFromOfficeClipboardEventData) => {
           if (!data.isTransformedWithPasteFromOffice) {
-            // this.logger.debug("Nothing to do.")
+            this.logger.debug("Nothing to do.");
             return;
           }
 
@@ -47,7 +48,7 @@ export default class SymbolOnPasteMapper extends Plugin {
         }
       )
     } else {
-      // this.logger.error("Unexpected Clipboard plugin.");
+      this.logger.error("Unexpected Clipboard plugin.");
     }
     return null;
   }
