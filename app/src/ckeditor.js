0b4fb701f6323d51670292e971012d6b4f1207d9
Mark Michaelis <mark.michaelis@coremedia.com>
2020-11-30 15:20:54 +0100

    CKEditor App: Add Required Plugins
    To show more features of the CKEditor we need to add plugins, as otherwise for example
    on paste all unsupported formats are stripped away.

--- app/src/ckeditor.js
+++ app/src/ckeditor.js
@@ -1,13 +1,77 @@
-/**
- * @license Copyright (c) 2014-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import SymbolOnPasteMapper from '@coremedia/ckeditor5-symbol-on-paste-mapper/dist/SymbolOnPasteMapper.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
+import CoreMediaSymbolOnPasteMapper from '@coremedia/ckeditor5-symbol-on-paste-mapper/dist/SymbolOnPasteMapper.js';
 
 ClassicEditor.create(document.querySelector( '.editor' ), {
-  plugins: [Essentials, Paragraph, SymbolOnPasteMapper],
-  toolbar: ['undo', 'redo']
+  licenseKey: '',
+  placeholder: 'Type your text here...',
+  plugins: [
+    Alignment,
+    BlockQuote,
+    Bold,
+    Essentials,
+    Heading,
+    Indent,
+    Italic,
+    Link,
+    List,
+    Paragraph,
+    PasteFromOffice,
+    RemoveFormat,
+    Strikethrough,
+    Subscript,
+    Superscript,
+    Table,
+    TableToolbar,
+    Underline,
+    CoreMediaSymbolOnPasteMapper
+  ],
+  toolbar: {
+    items: [
+      'undo',
+      'redo',
+      '|',
+      'heading',
+      '|',
+      'bold',
+      'italic',
+      'blockQuote',
+      'underline',
+      'strikethrough',
+      'superscript',
+      'subscript',
+      'removeFormat',
+      '|',
+      'link',
+      '|',
+      'alignment',
+      '|',
+      'insertTable',
+      '|',
+      'numberedList',
+      'bulletedList',
+      '|',
+      'indent',
+      'outdent',
+    ]
+  },
+  language: 'en'
 });
