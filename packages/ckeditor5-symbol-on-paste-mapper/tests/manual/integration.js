9daeeb5f3c6b944cfa670140b2f445c140daebcc
Mark Michaelis <mark.michaelis@coremedia.com>
2020-12-02 16:18:56 +0100

    Get Manual Tests Going
    First manual test for symbol-on-paste-mapper. Not nice yet, as
    I failed to apply the lark-theme.

--- packages/ckeditor5-symbol-on-paste-mapper/tests/manual/integration.js
+++ packages/ckeditor5-symbol-on-paste-mapper/tests/manual/integration.js
@@ -5,11 +5,25 @@
 
 /* globals console, window, document */
 
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-
-import Clipboard from "@ckeditor/ckeditor5-clipboard/src/clipboard";
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 
 import SymbolOnPasteMapper from '../../dist/SymbolOnPasteMapper';
 
@@ -24,9 +38,24 @@ ClassicEditor
     licenseKey: '',
     placeholder: 'Type your text here...',
     plugins: [
-      Clipboard,
+      Alignment,
+      BlockQuote,
+      Bold,
       Essentials,
+      Heading,
+      Indent,
+      Italic,
+      Link,
+      List,
+      Paragraph,
       PasteFromOffice,
+      RemoveFormat,
+      Strikethrough,
+      Subscript,
+      Superscript,
+      Table,
+      TableToolbar,
+      Underline,
       SymbolOnPasteMapper
     ],
     toolbar: {
