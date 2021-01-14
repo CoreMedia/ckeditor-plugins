8adcc4caeda042f8f8dd432fc5a12941eb08b07b
Mark Michaelis <mark.michaelis@coremedia.com>
2021-01-14 16:05:55 +0100

    First Sketch for Filtering Approach
    This commit contains a first sketch of a filtering approach, which is
    similar to the filter rules in CKEditor 4, thus, having a map of rules
    to apply. To be continued.

--- packages/ckeditor5-coremedia-richtext/src/RichTextDataProcessor.ts
+++ packages/ckeditor5-coremedia-richtext/src/RichTextDataProcessor.ts
@@ -12,6 +12,198 @@ import DomConverter from "@ckeditor/ckeditor5-engine/src/view/domconverter";
 import HtmlWriter from "@ckeditor/ckeditor5-engine/src/dataprocessor/htmlwriter";
 import RichTextHtmlWriter from "./RichTextHtmlWriter";
 
+type AttributeValue = string | null | undefined;
+
+interface Attributes {
+  [index: string]: AttributeValue;
+}
+
+class MutableElement {
+  private readonly _delegate: Element;
+  /**
+   * If the name is set to empty string, the element itself is removed,
+   * but its children added to the element's parent.
+   */
+  private _name: string | undefined = undefined;
+  private _attributes: Attributes = {};
+
+  constructor(delegate: Element) {
+    this._delegate = delegate;
+  }
+
+  get element(): Element {
+    return this._delegate;
+  }
+
+  get isReplaceWithChildren(): boolean {
+    return this._name === "";
+  }
+
+  get isReplace(): boolean {
+    if (!this._name) {
+      return false;
+    }
+    return this._name.toLowerCase() !== this._delegate.tagName.toLowerCase();
+  }
+
+  get parent(): HTMLElement | null {
+    return this._delegate.parentElement;
+  }
+
+  get children(): HTMLCollection {
+    return this._delegate.children;
+  }
+
+  get name(): string {
+    return this._name || this._delegate.tagName;
+  }
+
+  set name(newName: string) {
+    this._name = newName;
+  }
+
+  // https://javascript.info/proxy
+  // Do we require this, e.g. for custom attributes: https://github.com/ckeditor/ckeditor5/issues/1462
+  get attributes(): Attributes {
+    const element: Element = this._delegate;
+    return new Proxy(this._attributes, {
+      defineProperty(target: Attributes, p: PropertyKey, attributes: PropertyDescriptor): boolean {
+        console.log("defineProperty", {
+          target: target,
+          p: p,
+          attributes: attributes,
+        });
+        return Reflect.defineProperty(target, p, attributes);
+      },
+      get(target: Attributes, p: PropertyKey, receiver: never): AttributeValue {
+        console.log("get", {
+          target: target,
+          p: p,
+          receiver: receiver,
+        });
+        if (Reflect.has(target, p)) {
+          return Reflect.get(target, p, receiver);
+        }
+        if (typeof p === "string") {
+          return element.getAttribute(p);
+        }
+        return undefined;
+      },
+      getOwnPropertyDescriptor(target: Attributes, p: PropertyKey): PropertyDescriptor | undefined {
+        console.log("getOwnPropertyDescriptor", {
+          target: target,
+          p: p,
+        });
+        if (Reflect.has(target, p)) {
+          const value = Reflect.get(target, p);
+          if (value === undefined) {
+            return undefined;
+          }
+          return {
+            configurable: true,
+            enumerable: true,
+
+            get(): unknown {
+              console.log("target.get", {
+                p: p,
+                value: value,
+              });
+              return value;
+            },
+
+            set(v: unknown): void {
+              console.log("target.set", {
+                p: p,
+                value: v,
+              });
+              Reflect.set(target, p, v);
+            },
+          };
+        }
+        if (typeof p === "string" && element.hasAttribute(p)) {
+          return {
+            configurable: true,
+            enumerable: true,
+
+            get(): string | null {
+              console.log("element.get", {
+                p: p,
+                value: element.getAttribute(p),
+              });
+              return element.getAttribute(p);
+            },
+
+            set(v: unknown): void {
+              console.log("element.set", {
+                p: p,
+                value: v,
+              });
+              Reflect.set(target, p, v);
+            },
+          };
+        }
+        return undefined;
+      },
+      set(target: Attributes, p: PropertyKey, value: unknown): boolean {
+        console.log("set", {
+          target: target,
+          p: p,
+          value: value,
+        });
+        return Reflect.set(target, p, value);
+      },
+      deleteProperty(target: Attributes, p: PropertyKey): boolean {
+        console.log("deleteProperty", {
+          target: target,
+          p: p,
+        });
+        if (typeof p === "string") {
+          target[p] = undefined;
+          return true;
+        }
+        return false;
+      },
+      has(target: Attributes, p: PropertyKey): boolean {
+        console.log("has", {
+          target: target,
+          p: p,
+        });
+        const targetHas: boolean = Reflect.has(target, p);
+        if (targetHas) {
+          return Reflect.get(target, p) !== undefined;
+        }
+        if (typeof p === "string") {
+          return element.hasAttribute(p);
+        }
+        return false;
+      },
+      ownKeys(target: Attributes): PropertyKey[] {
+        console.log("ownKeys", {
+          target: target,
+        });
+        const targetKeys: PropertyKey[] = Reflect.ownKeys(target);
+        const elementAttrs: PropertyKey[] = element.getAttributeNames();
+        // Join distinct keys, skip forcibly deleted.
+        const result: PropertyKey[] = elementAttrs
+          .concat(targetKeys.filter((k) => elementAttrs.indexOf(k) < 0))
+          .filter((k) => !Reflect.has(target, k) || Reflect.get(target, k) !== undefined);
+        console.log("ownKeys", { target: target, keys: result });
+        return result;
+      },
+    });
+  }
+}
+
+type ElementFilterFunctionResult = MutableElement | Element | boolean;
+type ElementFilterFunction = (el: MutableElement) => ElementFilterFunctionResult;
+type TextFilterFunctionResult = Text | string | boolean;
+type TextFilterFunction = (text: string | null, textNode: Text) => TextFilterFunctionResult;
+
+interface FilterRuleSet {
+  elements?: { [key: string]: ElementFilterFunction };
+  text?: TextFilterFunction;
+}
+
 export default class RichTextDataProcessor implements DataProcessor {
   private readonly logger: Logger = LoggerProvider.getLogger(CoreMediaRichText.pluginName);
   private readonly delegate: HtmlDataProcessor;
@@ -24,7 +216,67 @@ export default class RichTextDataProcessor implements DataProcessor {
    *   Possibly look-up filter-behavior by CKEditor 4. I think, <null> as return value is meant to delete this node.
    */
 
-  // private readonly toDataFilterRules: Map<string, (k: string) => string | null>;
+  /**
+   * Coremedia Richtext Filter, that are applied before writing it back to the server. Some details about filter
+   * execution: (see especially <code>core/htmlparser/element.js</code>)
+   *
+   * <ul>
+   * <li>If an element name changes, filtering will be restarted at that node.</li>
+   * <li>If a new element is returned, it will replace the current one and processing will be restarted for that node.</li>
+   * <li>If false is returned, the element and all its children will be removed.</li>
+   * <li>If element name changes to empty, only the element itself will be removed and the children appended to the parent</li>
+   * <li>An element is processed first, then its attributes and afterwards its children (if any)</li>
+   * <li>Text nodes only support to be changed or removed... but not to be wrapped into some other element.</li>
+   * <li><code>$</code> is a so called generic element rule which is applied after element processing.
+   * The opposite handler is <code>'^'</code> which would be applied before all other element handlers.</li>
+   * </ul>
+   */
+  private readonly toDataFilterRules: FilterRuleSet = {
+    elements: {
+      h2: function (el: MutableElement): ElementFilterFunctionResult {
+        const attributes = el.attributes;
+
+        attributes["style"] = "great";
+
+        const classAttr = attributes["class"];
+        const styleAttr = attributes["style"];
+        console.log("mutable:", {
+          attributes: attributes,
+          classAttr: classAttr,
+          styleAttr: styleAttr,
+        });
+        return false;
+      },
+      a: function (el: MutableElement): ElementFilterFunctionResult {
+        const attributes = el.attributes;
+
+        attributes["style"] = "great";
+        delete attributes["href"];
+
+        const hrefAttr = attributes["href"];
+        const styleAttr = attributes["style"];
+
+        // TODO[cke] Now respect modified attributes and adapt the element...
+
+        console.log("mutable:", {
+          attributes: attributes,
+          hrefAttr: hrefAttr,
+          styleAttr: styleAttr,
+        });
+        let k: string;
+        for (k in attributes) {
+          console.log("loop: ", {
+            k: k,
+            val: attributes[k],
+          });
+        }
+        return true;
+      },
+    },
+    text: function (text: string | null, textNode: Text): TextFilterFunctionResult {
+      return false;
+    },
+  };
 
   constructor(document: ViewDocument) {
     this.delegate = new HtmlDataProcessor(document);
@@ -38,28 +290,44 @@ export default class RichTextDataProcessor implements DataProcessor {
   }
 
   toData(viewFragment: ViewDocumentFragment): string {
-    // As we insert a DocumentFragment, we know, that we will receive a
-    // DocumentFragment in return.
-    // TODO: Nach Debugging: Entgegen der Doku kommt hier ein "normales" DocumentFragment zurück: https://developer.mozilla.org/de/docs/Web/API/Document/createDocumentFragment
-    //   Nein, falsch. Es kommt tatsächlich ein normaler Node oder DocumentFragment zurück. Fixen!!!
     const domFragment: Node | DocumentFragment = this.domConverter.viewToDom(viewFragment, document);
-    // TODO: Das Ergebnis ist eher ein normaler DOM Node (also nicht CK)...
-    function getAllFuncs(toCheck: unknown) {
-      let props: any[] = [];
-      let obj = toCheck;
-      do {
-        props = props.concat(Object.getOwnPropertyNames(obj));
-      } while ((obj = Object.getPrototypeOf(obj)));
-
-      return props.sort();
-    }
 
     this.logger.debug("toData: ViewFragment converted to DOM.", {
       view: viewFragment,
       dom: domFragment,
-      props: getAllFuncs(domFragment),
-      type: typeof domFragment,
     });
+
+    const nodeIterator: NodeIterator = document.createNodeIterator(
+      domFragment,
+      NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT
+    );
+
+    let currentNode: Node | null;
+    while ((currentNode = nodeIterator.nextNode())) {
+      console.log("currentNode", currentNode);
+      if (currentNode instanceof Element && this.toDataFilterRules.elements) {
+        const filterRule: ElementFilterFunction = this.toDataFilterRules.elements[currentNode.nodeName.toLowerCase()];
+        if (!!filterRule) {
+          const result: ElementFilterFunctionResult = filterRule(new MutableElement(currentNode));
+          if (!result) {
+            const parentNode: (Node & ParentNode) | null = currentNode.parentNode;
+            if (!!parentNode) {
+              parentNode.removeChild(currentNode);
+            }
+          }
+        }
+      } else if (currentNode instanceof Text && this.toDataFilterRules.text) {
+        const filterRule: TextFilterFunction = this.toDataFilterRules.text;
+        const result: TextFilterFunctionResult = filterRule(currentNode.textContent, currentNode);
+        if (result === false) {
+          const parentNode: (Node & ParentNode) | null = currentNode.parentNode;
+          if (!!parentNode) {
+            parentNode.removeChild(currentNode);
+          }
+        }
+      }
+    }
+
     const html: string = this.htmlWriter.getHtml(domFragment);
     return this.htmlToRichText(html);
   }
