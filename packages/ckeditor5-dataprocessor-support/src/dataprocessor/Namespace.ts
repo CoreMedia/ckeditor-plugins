d658c16ceb5121b506a2fd3fc405b62301cbb136
Mark Michaelis <mark.michaelis@coremedia.com>
2021-02-23 16:36:22 +0100

    Namespace: Added Some Standard Namespaces
    Added some standard namespaces for convenience:
    
    * xlink
    * xml
    * xmlns

--- packages/ckeditor5-dataprocessor-support/src/dataprocessor/Namespace.ts
+++ packages/ckeditor5-dataprocessor-support/src/dataprocessor/Namespace.ts
@@ -7,3 +7,15 @@ export default interface Namespace {
 export interface Namespaces {
   [prefix: string]: Namespace;
 }
+
+export const DEFAULT_NAMESPACES: Namespaces = {
+  "xlink": {
+    uri: "http://www.w3.org/1999/xlink",
+  },
+  "xml": {
+    uri: "http://www.w3.org/XML/1998/namespace",
+  },
+  "xmlns": {
+    uri: "http://www.w3.org/2000/xmlns/",
+  },
+}
