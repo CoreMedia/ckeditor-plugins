825379ffcb8a8c384d31e7edb2cc88bd087e75df
Mark Michaelis <mark.michaelis@coremedia.com>
2021-04-28 15:11:09 +0200

    LinkTarget: Adjust Upcast Mapping
    Adjust upcast mapping to be similar to `linkediting.js`
    provided by CKEditor 5 Link Plugin.

--- packages/ckeditor5-link/src/LinkTarget.ts
+++ packages/ckeditor5-link/src/LinkTarget.ts
@@ -1,9 +1,11 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import Editor from "@ckeditor/ckeditor5-core/src/editor/editor";
+import { Logger, LoggerProvider } from "@coremedia/coremedia-utils/index";
 
 export const PLUGIN_NAME = "CoreMediaLinkTarget";
-export const LINK_TARGET = "linkTarget";
+export const LINK_TARGET_MODEL = "linkTarget";
 
+const LINK_TARGET_VIEW = "target";
 /**
  * Adds an attribute `linkTarget` to the model, which will be represented
  * as `target` attribute in view.
@@ -12,18 +14,23 @@ export const LINK_TARGET = "linkTarget";
  */
 export default class LinkTarget extends Plugin {
   static readonly pluginName: string = PLUGIN_NAME;
+  private readonly logger: Logger = LoggerProvider.getLogger(PLUGIN_NAME);
   private readonly TEXT_NAME = "$text";
 
   init(): Promise<void> | null {
+    const startTimestamp = performance.now();
+
+    this.logger.info(`Initializing ${LinkTarget.pluginName}...`);
+
     const editor: Editor = this.editor;
 
     // Allow link attribute on all inline nodes.
-    editor.model.schema.extend(this.TEXT_NAME, { allowAttributes: LINK_TARGET });
+    editor.model.schema.extend(this.TEXT_NAME, { allowAttributes: LINK_TARGET_MODEL });
 
     // Create element with target attribute. Element will be merged with
     // a-Element created by Link plugin.
     editor.conversion.for("downcast").attributeToElement({
-      model: LINK_TARGET,
+      model: LINK_TARGET_MODEL,
       view: (modelAttributeValue, { writer }) => {
         return writer.createAttributeElement("a", {
           target: modelAttributeValue,
@@ -34,14 +41,21 @@ export default class LinkTarget extends Plugin {
 
     // Take the view target-attribute of a-element and transform it to
     // linkTarget attribute in model.
-    editor.conversion.for("upcast").attributeToAttribute({
-      model: LINK_TARGET,
+    editor.conversion.for("upcast").elementToAttribute({
       view: {
         name: "a",
-        key: "target",
+        attributes: {
+          [LINK_TARGET_VIEW]: true,
+        },
+      },
+      model: {
+        key: LINK_TARGET_MODEL,
+        value: (viewElement) => viewElement.getAttribute(LINK_TARGET_VIEW) || null,
       },
       converterPriority: "low",
     });
+
+    this.logger.info(`Initialized ${LinkTarget.pluginName} within ${performance.now() - startTimestamp} ms.`);
 
     return null;
   }
