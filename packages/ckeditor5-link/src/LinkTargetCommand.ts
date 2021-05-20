938382a5515e7de80dfbc1400b648d42963a5f89
Mark Michaelis <mark.michaelis@coremedia.com>
2021-05-20 13:22:20 +0200

    Copy & Paste LinkCommand for LinkTargetCommand
    We now behave very similar to the LinkCommand on execute. Nevertheless,
    as expected, this state creates two entries in undo history instead of
    one, if we for example create a new link with a collapsed selection.
    
    This is a clear vote, that setting/adjusting the attributes should
    go into the post-fixer while on execution we just will store the
    parameters (ranges et al.) we want to set.

--- packages/ckeditor5-link/src/LinkTargetCommand.ts
+++ packages/ckeditor5-link/src/LinkTargetCommand.ts
@@ -2,12 +2,14 @@ import Command from "@ckeditor/ckeditor5-core/src/command";
 import Editor from "@ckeditor/ckeditor5-core/src/editor/editor";
 import first from "@ckeditor/ckeditor5-utils/src/first";
 import { isImageAllowed } from "@ckeditor/ckeditor5-link/src/utils";
-import { LINK_TARGET_MODEL } from "./Constants";
+import { LINK_COMMAND_NAME, LINK_TARGET_MODEL } from "./Constants";
 import { DiffItem, DiffItemAttribute, DiffItemInsert } from "@ckeditor/ckeditor5-engine/src/model/differ";
 import Writer from "@ckeditor/ckeditor5-engine/src/model/writer";
 import Range from "@ckeditor/ckeditor5-engine/src/model/range";
 import { Logger, LoggerProvider } from "@coremedia/coremedia-utils/index";
 import Selection from "@ckeditor/ckeditor5-engine/src/model/selection";
+import findAttributeRange from "@ckeditor/ckeditor5-typing/src/utils/findattributerange";
+import Position from "@ckeditor/ckeditor5-engine/src/model/position";
 
 /**
  * Extension to `LinkCommand` which takes care of setting the `linkTarget`
@@ -19,7 +21,17 @@ import Selection from "@ckeditor/ckeditor5-engine/src/model/selection";
  */
 export default class LinkTargetCommand extends Command {
   private readonly logger: Logger = LoggerProvider.getLogger("LinkTargetCommand");
+  /**
+   * Toggles, if post-fix should be applied or not.
+   * @private
+   */
   private postFixEnabled = false;
+  /**
+   * Toggles, if writer.removeSelectionAttribute should be called once
+   * LinkCommand is done.
+   * @private
+   */
+  private removeSelectionAttribute = false;
   private nextTarget: string | "" | null = null;
 
   constructor(editor: Editor) {
@@ -27,7 +39,7 @@ export default class LinkTargetCommand extends Command {
 
     const model = editor.model;
     const document = model.document;
-    document.registerPostFixer((writer) => this.updateTargetOnLinkHref(writer));
+    document.registerPostFixer((writer) => this._updateTargetOnLinkHref(writer));
   }
 
   refresh(): void {
@@ -56,46 +68,184 @@ export default class LinkTargetCommand extends Command {
    */
   execute(target: string | "" | null, href: string | "" | null): void {
     const editor = this.editor;
-    const linkCommand = editor.commands.get("link");
+    const linkCommand = editor.commands.get(LINK_COMMAND_NAME);
 
-    if (!linkCommand) {
-      this.logger.warn("Cannot execute: Required LinkCommand not available.");
-      return;
-    }
-
-    /*
-     * TODO[cke] Special case: HREF Emptied... We may even clear our href
-     *    attribute. In this case, the anchor remains, but href is removed.
-     *    Testing with current code will run into deadlock.
-     *    General idea how to continue: Deal with the last special case, that
-     *    href did not change, but see if target got changed. And of course
-     *    fix the deadlock.
-     */
     const model = editor.model;
-    /*
-     * TODO[cke] New approach. Instead of relying on the differ within the
-     *    postfixer, we are going to store the original selection state
-     *    to apply changes. We cannot currently guess, why for an empty
-     *    href and having target set, we don't get a full range on changes
-     *    but only each character of the current link as range, which again
-     *    currently causes an infinite loop.
-     *    The challenge: The selection is reset to a new position by
-     *    LinkCommand, so that we cannot rely on the selection once we
-     *    run _after_ LinkCommand (as post-fixer).
-     */
     const selection = model.document.selection;
+
+    model.change((writer) => {
+      if (selection.isCollapsed) {
+        this._handleCollapsedSelection(writer, target, href, linkCommand);
+      } else {
+        this._handleExpandedSelection(writer, target);
+      }
+    });
 
     this.nextTarget = target;
     /*
      * We must listen to several events triggered by `LinkCommand` on model
      * change, so that we have to keep up listening until the command is done.
+     * But we only need to do that, if the linkCommand is available.
      */
-    this.postFixEnabled = true;
+    this.postFixEnabled = !!linkCommand;
 
-    linkCommand.once("execute", () => {
+    linkCommand?.once("execute", () => {
       // LinkCommand execution is done. Let's stop applying post-fixes.
       this.postFixEnabled = false;
     });
+  }
+
+  /**
+   * If the selection is expanded (i.e. text is actually selected), we need to
+   * update all valid ranges with the target attribute.
+   *
+   * Copy & Paste from LinkCommand:
+   *
+   * Note, that this code is very similar to the `LinkCommand` execution
+   * as the command does not provide appropriate extension points yet
+   * (see https://github.com/ckeditor/ckeditor5/issues/9730). On each update
+   * it should be checked, if the corresponding implementation changed in
+   * a way, that requires to adapt the following code.
+   */
+  private _handleExpandedSelection(writer: Writer, target: string | "" | null) {
+    const editor = this.editor;
+    const model = editor.model;
+    const selection = model.document.selection;
+    const ranges = model.schema.getValidRanges(selection.getRanges(), LINK_TARGET_MODEL);
+    const allowedRanges = [];
+    for (const element of selection.getSelectedBlocks()) {
+      if (model.schema.checkAttribute(element, LINK_TARGET_MODEL)) {
+        allowedRanges.push(writer.createRangeOn(element));
+      }
+    }
+    const rangesToUpdate = allowedRanges.slice();
+    for (const range of ranges) {
+      if (LinkTargetCommand._isRangeToUpdate(range, allowedRanges)) {
+        rangesToUpdate.push(range);
+      }
+    }
+    for (const range of rangesToUpdate) {
+      this._setOrRemoveAttribute(writer, target, range);
+    }
+  }
+
+  /**
+   * If the selection is collapsed (i.e. we are at cursor-position having
+   * nothing selected), we are possibly going to modify an already existing
+   * `linkTarget`.
+   *
+   * Copy & Paste from LinkCommand:
+   *
+   * Note, that this code is very similar to the `LinkCommand` execution
+   * as the command does not provide appropriate extension points yet
+   * (see https://github.com/ckeditor/ckeditor5/issues/9730). On each update
+   * it should be checked, if the corresponding implementation changed in
+   * a way, that requires to adapt the following code.
+   * @private
+   */
+  private _handleCollapsedSelection(
+    writer: Writer,
+    target: string | "" | null,
+    href: string | "" | null,
+    linkCommand: Command | undefined
+  ) {
+    const editor = this.editor;
+    const model = editor.model;
+    const selection = model.document.selection;
+    // LinkCommand does not check for possible null value, so don't we.
+    const position: Position = <Position>selection.getFirstPosition();
+
+    const hasLinkTarget = selection.hasAttribute(LINK_TARGET_MODEL);
+    const hasLinkHref = selection.hasAttribute("linkHref");
+
+    if (hasLinkTarget || hasLinkHref) {
+      const referenceAttribute = hasLinkTarget ? LINK_TARGET_MODEL : "linkHref";
+      /*
+       * We have to determine the range based on either linkTarget or
+       * linkHref. If linkTarget has not been set yet, we assume, that
+       * the target shall be applied to the same range as we will apply
+       * linkHref to.
+       */
+      const linkRange = findAttributeRange(
+        position,
+        referenceAttribute,
+        selection.getAttribute(referenceAttribute),
+        model
+      );
+      this._setOrRemoveAttribute(writer, target, linkRange);
+    } else if (!!target) {
+      /*
+       * Only set the target, if it is not empty. We already know at this
+       * point, that the current selection has no `linkHref` attribute
+       * set, which leaves two possible states we need to handle:
+       *
+       *
+       * TL;DR: Always call `writer.removeSelectionAttribute` after
+       * execution of link-command, but only set the attribute if
+       * linkHref is not empty.
+       *
+       * linkHref !== ''
+       *
+       *     This will cause `LinkCommand` to add a text containing linkHref.
+       *     As `LinkCommand` link command will take over the attributes from
+       *     the selection, we can add the target to the selection (because
+       *     this will/must run before `LinkCommand`). Doing so, requires
+       *     to manually trigger `writer.removeSelectionAttribute` after
+       *     `LinkCommand` got executed.
+       *
+       * linkHref == ''
+       *
+       *     `LinkCommand` won't create any text and won't apply any
+       *     attributes. Thus, we must not add `linkTarget` attribute,
+       *     as there would be no UI to editor this orphaned `LinkTarget`.
+       *     But just as `LinkCommand` we should ensure to call
+       *     `writer.removeSelectionAttribute`.
+       */
+      if (!!href && !!linkCommand) {
+        // This will tell link-command that it should take these
+        // attributes when creating the text.
+        writer.setSelectionAttribute(LINK_TARGET_MODEL, target);
+        this.removeSelectionAttribute = true;
+      }
+    }
+  }
+
+  /**
+   * Depending if the target is empty or not, the attribute is either removed
+   * from the given range or set.
+   * @private
+   */
+  private _setOrRemoveAttribute(writer: Writer, target: string | "" | null, range: Range) {
+    // If we empty the target, we just want to remove it.
+    if (!target) {
+      // May remove `linkTarget` attribute, where there isn't any. But
+      // this does no harm. The following command will just do nothing.
+      writer.removeAttribute(LINK_TARGET_MODEL, range);
+    } else {
+      // May set the very same value as already set. Just as for
+      // removal: Won't do any action, if the target attribute did
+      // not change.
+      writer.setAttribute(LINK_TARGET_MODEL, target, range);
+    }
+  }
+
+  /**
+   * Checks whether specified `range` is inside an element that accepts the `linkTarget` attribute.
+   *
+   * @private
+   * @param range A range to check.
+   * @param allowedRanges An array of ranges created on elements where the attribute is accepted.
+   * @returns {Boolean}
+   */
+  private static _isRangeToUpdate(range: Range, allowedRanges: Range[]): boolean {
+    for (const allowedRange of allowedRanges) {
+      // A range is inside an element that will have the `linkTarget` attribute. Do not modify its nodes.
+      if (allowedRange.containsRange(range)) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   destroy(): void {
@@ -116,7 +266,7 @@ export default class LinkTargetCommand extends Command {
    * @private
    * @return `true`, if changes got applied; `false` otherwise
    */
-  private updateTargetOnLinkHref(writer: Writer): boolean {
+  private _updateTargetOnLinkHref(writer: Writer): boolean {
     // Note on parallel changes: You may observe infinite iterations when this
     // post-fixer clashes with the post-fixer for `linkHref` removal in
     // `LinkTargetModelView`. So, be aware, that both post-fixers may run on
@@ -127,6 +277,20 @@ export default class LinkTargetCommand extends Command {
       return false;
     }
 
+    if (this.removeSelectionAttribute) {
+      writer.removeSelectionAttribute(LINK_TARGET_MODEL);
+      // We only want to do this once, in case we get called multiple times
+      // during post-fixing.
+      this.removeSelectionAttribute = false;
+    }
+
+    if (true) {
+      // TODO[cke] Currently nothing more todo on post-fixing despite removing
+      //   the selection attribute. If we experience, that the current state
+      //   produces two undo-steps, we need to activate post-processing again
+      //   and apply attributes at all in here.
+      return false;
+    }
     const target = this.nextTarget;
     const model = writer.model;
     const document = model.document;
@@ -172,15 +336,6 @@ export default class LinkTargetCommand extends Command {
       // we continue typing.
       writer.removeSelectionAttribute(LINK_TARGET_MODEL);
     }
-
-    console.log("updateTargetOnLinkHref", {
-      target: target,
-      linkHrefRangeCandidates: linkHrefRangeCandidates,
-      linkHrefRanges: linkHrefRanges,
-      validRanges: validRanges,
-      operationsBefore: operationsBefore,
-      operationsAfter: operationsAfter,
-    });
 
     return operationsBefore !== operationsAfter;
   }
