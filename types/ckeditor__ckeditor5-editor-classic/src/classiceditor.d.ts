897c36066760763a0dc502167cb3a0cab1f345f5
Mark Michaelis <mark.michaelis@coremedia.com>
2021-06-09 13:17:27 +0200

    feat(typings): Enhance Typing for ClassicEditor

--- types/ckeditor__ckeditor5-editor-classic/src/classiceditor.d.ts
+++ types/ckeditor__ckeditor5-editor-classic/src/classiceditor.d.ts
@@ -1,6 +1,32 @@
 import Editor from "@ckeditor/ckeditor5-core/src/editor/editor";
 
-export default class ClassicEditor extends Editor {
+// TODO[typing]
+interface DataApi {
+}
 
-  static create (sourceElementOrData:any, config:object):Promise<Editor>;
+// TODO[typing]
+interface EditorWithUI {
+}
+
+// TODO[typing]
+interface ElementApi {
+}
+
+// TODO[typing]
+type ClassicEditorUI = any;
+// TODO[typing]
+type EditorConfig = any;
+
+export default class ClassicEditor extends Editor implements DataApi, EditorWithUI, ElementApi {
+  ui: ClassicEditorUI;
+
+  static create(sourceElementOrData: HTMLElement | string, config?: EditorConfig): Promise<ClassicEditor>;
+
+  setData(data: string): void;
+
+  getData(options?: { rootName?: string; trim?: "empty" | "none" }): string;
+
+  sourceElement?: HTMLElement;
+
+  updateSourceElement(): void;
 }
