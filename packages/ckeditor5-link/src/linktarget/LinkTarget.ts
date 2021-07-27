34a3f847ea47a83ac97bab0dc5401ad1e2f8f4be
Mark Michaelis <mark.michaelis@coremedia.com>
2021-07-27 14:48:37 +0200

    docs(content-links): Fix Left-Over from Refactoring
    ContentLinks is now a separate plugin.

--- packages/ckeditor5-link/src/linktarget/LinkTarget.ts
+++ packages/ckeditor5-link/src/linktarget/LinkTarget.ts
@@ -8,8 +8,6 @@ import Editor from "@ckeditor/ckeditor5-core/src/editor/editor";
  * Adds an attribute `linkTarget` to the model, which will be represented
  * as `target` attribute in view.
  *
- * Also adds the ContentLinks plugin, which allows content objects to be dropped into the link dialog.
- *
  * @see <a href="https://stackoverflow.com/questions/51303892/how-to-add-target-attribute-to-a-tag-in-ckeditor5">How to add "target" attribute to `a` tag in ckeditor5? - Stack Overflow</a>
  */
 export default class LinkTarget extends Plugin {
