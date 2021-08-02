6936d4f88148fde62c47d46d82862966483ec473
pkliesch <pkliesch@coremedia.com>
2021-08-02 17:16:48 +0200

    change mousedown listener (do not listen on draggables)

--- packages/ckeditor5-link/src/contentlink/ContentLinks.ts
+++ packages/ckeditor5-link/src/contentlink/ContentLinks.ts
@@ -4,6 +4,7 @@ import LinkUI from "@ckeditor/ckeditor5-link/src/linkui";
 import LinkEditing from "@ckeditor/ckeditor5-link/src/linkediting";
 import ContentLinkActionsViewExtension from "./ui/ContentLinkActionsViewExtension";
 import ContentLinkFormViewExtension from "./ui/ContentLinkFormViewExtension";
+import Emitter from "@ckeditor/ckeditor5-utils/src/emittermixin";
 
 /**
  * This plugin allows content objects to be dropped into the link dialog.
@@ -15,5 +16,61 @@ export default class ContentLinks extends Plugin {
 
   static get requires(): Array<new (editor: Editor) => Plugin> {
     return [LinkUI, LinkEditing, ContentLinkActionsViewExtension, ContentLinkFormViewExtension];
+  }
+
+  init(): Promise<void> | null {
+    const editor = this.editor;
+    const linkUI: LinkUI = <LinkUI>editor.plugins.get(LinkUI);
+
+    this.#removeInitialMouseDownListener(linkUI);
+    this.#addMouseDownListenerForNotDraggables(linkUI);
+    return null;
+  }
+
+  #removeInitialMouseDownListener(linkUI: LinkUI) {
+    linkUI.formView.stopListening(<Emitter>(<unknown>document), "mousedown");
+  }
+
+  #addCustomClickOutsideHandler({
+    emitter,
+    activator,
+    callback,
+    contextElements,
+  }: {
+    emitter: Emitter;
+    activator: () => boolean;
+    callback: () => void;
+    contextElements: HTMLElement[];
+  }) {
+    emitter.listenTo(
+      <Emitter>(<unknown>document),
+      "mousedown",
+      (evt: any, domEvt: { composedPath: () => any; target: any }) => {
+        if (!activator()) {
+          return;
+        }
+
+        // Check if `composedPath` is `undefined` in case the browser does not support native shadow DOM.
+        // Can be removed when all supported browsers support native shadow DOM.
+        const path = typeof domEvt.composedPath == "function" ? domEvt.composedPath() : [];
+
+        for (const contextElement of contextElements) {
+          if (domEvt.target.draggable || contextElement.contains(domEvt.target) || path.includes(contextElement)) {
+            return;
+          }
+        }
+
+        callback();
+      }
+    );
+  }
+
+  #addMouseDownListenerForNotDraggables(linkUI: LinkUI) {
+    this.#addCustomClickOutsideHandler({
+      emitter: <Emitter>(<unknown>linkUI.formView),
+      activator: () => linkUI._isUIInPanel,
+      contextElements: [linkUI._balloon.view.element],
+      callback: () => linkUI._hideUI(),
+    });
   }
 }
