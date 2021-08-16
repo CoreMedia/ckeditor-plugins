756410316c4c58d63a7bb92e5cd106d0df29e894
Mark Michaelis <mark.michaelis@coremedia.com>
2021-08-16 15:30:14 +0200

    fix(content-links): Fixes Enabled State for Save-Button
    Save-Button did not get disabled as intended, when the
    content name is not yet available. Fixed it by adding
    listener to formView properties.
    
    Extracted to extra method for better overview in init
    method.

--- packages/ckeditor5-link/src/contentlink/ui/ContentLinkFormViewExtension.ts
+++ packages/ckeditor5-link/src/contentlink/ui/ContentLinkFormViewExtension.ts
@@ -15,6 +15,8 @@ import { showContentLinkField } from "../ContentLinkViewUtils";
 import ContentLinkView from "./ContentLinkView";
 import DragDropAsyncSupport from "@coremedia/coremedia-studio-integration/content/DragDropAsyncSupport";
 import ContentLinkCommandHook from "../ContentLinkCommandHook";
+import LinkFormView from "@ckeditor/ckeditor5-link/src/ui/linkformview";
+import Command from "@ckeditor/ckeditor5-core/src/command";
 
 /**
  * Extends the form view for Content link display. This includes:
@@ -39,47 +41,33 @@ class ContentLinkFormViewExtension extends Plugin {
 
     const editor = this.editor;
     const linkUI: LinkUI = <LinkUI>editor.plugins.get(LinkUI);
+    const formView = linkUI.formView;
     const contentLinkCommandHook: ContentLinkCommandHook = <ContentLinkCommandHook>(
       editor.plugins.get(ContentLinkCommandHook)
     );
-    const linkCommand = editor.commands.get("link");
+    const linkCommand = <Command>editor.commands.get("link");
 
-    linkUI.formView.set({
+    formView.set({
       contentUriPath: undefined,
       contentName: undefined,
     });
 
-    linkUI.formView.bind("contentUriPath").to(linkCommand, "value", (value: string) => {
+    formView.bind("contentUriPath").to(linkCommand, "value", (value: string) => {
       return CONTENT_CKE_MODEL_URI_REGEXP.test(value) ? value : undefined;
     });
 
-    // We have to extend the algorithm of LinkUI to calculate the enabled state of
-    // the save button. This is because, we must not submit a content-link when
-    // we don't know its name yet.
-    const saveButtonView = linkUI.formView.saveButtonView;
-    saveButtonView.unbind("isEnabled");
-    saveButtonView.bind("isEnabled").to(
-      linkCommand,
-      "isEnabled",
-      this,
-      "contentName",
-      this,
-      "contentUriPath",
-      (isEnabled: boolean, contentName: string | undefined, contentUriPath: string | undefined): boolean =>
-        // Either contentUriPath must be unset or contentName must be set.
-        isEnabled && (!contentUriPath || !!contentName)
-    );
+    this.#rebindSaveEnabled(linkCommand, formView);
 
     // We need to propagate the content name prior to the LinkCommand being executed.
     // This is required for collapsed selections, where the LinkCommand wants to
     // write the URL into the text. For content-links this must be the content name
     // instead.
     this.listenTo(
-      linkUI.formView,
+      formView,
       "submit",
       () => {
         // @ts-ignore
-        const { contentUriPath, contentName } = linkUI.formView;
+        const { contentUriPath, contentName } = formView;
         if (!!contentUriPath && typeof contentName === "string") {
           contentLinkCommandHook.registerContentName(contentUriPath, contentName);
         }
@@ -97,6 +85,34 @@ class ContentLinkFormViewExtension extends Plugin {
     );
 
     return null;
+  }
+
+  /**
+   * When in Content-Link mode, we need to respect additional states to decide on
+   * enabled/disabled state of Save-Button. Which is: We must only activate the
+   * save-button when we know the content-name to write for collapsed selections.
+   *
+   * @param linkCommand command which is originally bound to enabled state
+   * @param formView formView to rebind enabled state of saveButtonView for
+   * @private
+   */
+  #rebindSaveEnabled(linkCommand: Command, formView: LinkFormView): void {
+    // We have to extend the algorithm of LinkUI to calculate the enabled state of
+    // the save button. This is because, we must not submit a content-link when
+    // we don't know its name yet.
+    const saveButtonView = formView.saveButtonView;
+    const enabledProperties = [linkCommand, "isEnabled", formView, "contentName", formView, "contentUriPath"];
+    const enabledHandler = (
+      isEnabled: boolean,
+      contentName: string | undefined,
+      contentUriPath: string | undefined
+    ): boolean => {
+      // Either contentUriPath must be unset or contentName must be set.
+      return isEnabled && (!contentUriPath || !!contentName);
+    };
+
+    saveButtonView.unbind("isEnabled");
+    saveButtonView.bind("isEnabled").to(...enabledProperties, enabledHandler);
   }
 
   #extendView(linkUI: LinkUI): void {
