6efc336e19660d60e4c7e2b68a1026472e65a6ff
jdallman <Jens.Dallmann@coremedia.com>
2021-08-20 11:33:53 +0200

    Use shorter syntax

--- packages/ckeditor5-link/src/linkclipboardextension/ContentLinkData.ts
+++ packages/ckeditor5-link/src/linkclipboardextension/ContentLinkData.ts
@@ -1,21 +1,9 @@
 export class ContentLinkData {
-  isFirstInsertedLink: boolean;
-  isLastInsertedLink: boolean;
-  text: string;
-  contentUri: string;
-  href: string;
-
   constructor(
-    isFirstInsertedLink: boolean,
-    isLastInsertedLink: boolean,
-    text: string,
-    contentUri: string,
-    href: string
-  ) {
-    this.isFirstInsertedLink = isFirstInsertedLink;
-    this.isLastInsertedLink = isLastInsertedLink;
-    this.text = text;
-    this.contentUri = contentUri;
-    this.href = href;
-  }
+    readonly isFirstInsertedLink: boolean,
+    readonly isLastInsertedLink: boolean,
+    readonly text: string,
+    readonly contentUri: string,
+    readonly href: string
+  ) {}
 }
