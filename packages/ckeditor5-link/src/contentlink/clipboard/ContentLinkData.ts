ccc3b7da314e9a4ff9a3b449892798933f9d2f32
Mark Michaelis <mark.michaelis@coremedia.com>
2021-09-02 14:51:23 +0200

    docs(content-links): Document ContentLinkData

--- packages/ckeditor5-link/src/contentlink/clipboard/ContentLinkData.ts
+++ packages/ckeditor5-link/src/contentlink/clipboard/ContentLinkData.ts
@@ -1,9 +1,27 @@
+/**
+ * Meta-data of a content dropped to CKEditor.
+ */
 export class ContentLinkData {
   constructor(
+    /**
+     * Signals, if the given content is the first in a list of dropped contents.
+     */
     readonly isFirstInsertedLink: boolean,
+    /**
+     * Signals, if the given content is the last in a list of dropped contents.
+     */
     readonly isLastInsertedLink: boolean,
+    /**
+     * The text to write to CKEditor (the name of the content).
+     */
     readonly text: string,
+    /**
+     * The URI-Path of the content, e.g., `content/42`.
+     */
     readonly contentUri: string,
+    /**
+     * The respective `linkHref` value to set in model, e.g., `content:42`.
+     */
     readonly href: string
   ) {}
 }
