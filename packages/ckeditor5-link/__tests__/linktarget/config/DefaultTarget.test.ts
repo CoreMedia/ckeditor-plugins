dc2d93e826be9f18a440ad35f05d47f57824f375
Mark Michaelis <mark.michaelis@coremedia.com>
2021-09-03 11:15:49 +0200

    test(link-target): Test for getDefaultTargetDefinition

--- packages/ckeditor5-link/__tests__/linktarget/config/DefaultTarget.test.ts
+++ packages/ckeditor5-link/__tests__/linktarget/config/DefaultTarget.test.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_TARGETS_ARRAY } from "../../../dist/linktarget/config/DefaultTarget";
+import { DEFAULT_TARGETS_ARRAY, getDefaultTargetDefinition } from "../../../src/linktarget/config/DefaultTarget";
 
 describe("DefaultTarget", () => {
 
@@ -12,8 +12,8 @@ describe("DefaultTarget", () => {
     });
 
     test.each`
-       name | title
-       ${"_self"} | ${"Open in Current Tab"}
+       name        | title
+       ${"_self"}  | ${"Open in Current Tab"}
        ${"_blank"} | ${"Open in New Tab"}
        ${"_embed"} | ${"Show Embedded"}
        ${"_other"} | ${"Open in Frame"}
@@ -21,6 +21,47 @@ describe("DefaultTarget", () => {
       const options = DEFAULT_TARGETS_ARRAY.find((definition) => name === definition.name);
       // Precondition-check
       expect(options).toBeTruthy();
+      const { title: actualTitle } = options!;
+      expect(actualTitle).toStrictEqual(expectedTitle);
+    });
+
+  });
+
+  describe("getDefaultTargetDefinition", () => {
+    const defTarget = "Default Standard Target";
+    const xLinkBehavior = "While used for xlink:show, not part of our standard definitions";
+    const htmlBehavior = "While standard HTML target, not part of our standard definitions";
+
+    test.each`
+      name         | exists   | comment
+      ${"_self"}   | ${true}  | ${defTarget}
+      ${"_blank"}  | ${true}  | ${defTarget}
+      ${"_embed"}  | ${true}  | ${defTarget}
+      ${"_other"}  | ${true}  | ${defTarget}
+      ${"_other"}  | ${true}  | ${defTarget}
+      ${"_none"}   | ${false} | ${xLinkBehavior}
+      ${"_parent"} | ${false} | ${htmlBehavior}
+      ${"_top"}    | ${false} | ${htmlBehavior}
+      ${"custom"}  | ${false} | ${"Any custom target is not expected to be defined by default"}
+    `("[$#] Expecting default definition for $name? $exists ($comment)", ({ name, exists }) => {
+      const definition = getDefaultTargetDefinition(name);
+      if (exists) {
+        expect(definition).toBeDefined();
+      } else {
+        expect(definition).toBeUndefined();
+      }
+    });
+
+    test.each`
+       name        | title
+       ${"_self"}  | ${"Open in Current Tab"}
+       ${"_blank"} | ${"Open in New Tab"}
+       ${"_embed"} | ${"Show Embedded"}
+       ${"_other"} | ${"Open in Frame"}
+    `("[$#] Should provide expected title for $name: $title", ({ name, title: expectedTitle }) => {
+      const options = getDefaultTargetDefinition(name);
+      // Precondition-check
+      expect(options).toBeDefined();
       const { title: actualTitle } = options!;
       expect(actualTitle).toStrictEqual(expectedTitle);
     });
