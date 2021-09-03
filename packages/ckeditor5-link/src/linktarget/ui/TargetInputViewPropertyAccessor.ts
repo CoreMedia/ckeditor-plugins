2c952601bbe6ceea0b9277b9fdbabf160f8a6847
jdallman <Jens.Dallmann@coremedia.com>
2021-09-03 09:54:50 +0200

    Execute lint

--- packages/ckeditor5-link/src/linktarget/ui/TargetInputViewPropertyAccessor.ts
+++ packages/ckeditor5-link/src/linktarget/ui/TargetInputViewPropertyAccessor.ts
@@ -5,6 +5,6 @@ import DropdownView from "@ckeditor/ckeditor5-ui/src/dropdown/dropdownview";
  * An interface to cast the targetInputView of LinkFormViewExtension to, to access the properties in a typed way
  * without issues of eslint.
  */
-export default interface TargetInputViewPropertyAccessor extends LabeledFieldView<DropdownView>{
+export default interface TargetInputViewPropertyAccessor extends LabeledFieldView<DropdownView> {
   hiddenTarget: string;
 }
