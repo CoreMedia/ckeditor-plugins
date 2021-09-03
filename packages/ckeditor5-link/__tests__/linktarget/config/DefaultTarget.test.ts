d81b0115d6dc8e4c93bf5e9172564ad217570038
Mark Michaelis <mark.michaelis@coremedia.com>
2021-09-03 10:28:23 +0200

    test(link-target): Test for Default Titles
    The titles should be available as expected.

--- packages/ckeditor5-link/__tests__/linktarget/config/DefaultTarget.test.ts
+++ packages/ckeditor5-link/__tests__/linktarget/config/DefaultTarget.test.ts
@@ -1,12 +1,30 @@
 import { DEFAULT_TARGETS_ARRAY } from "../../../dist/linktarget/config/DefaultTarget";
 
 describe("DefaultTarget", () => {
+
   describe("DEFAULT_TARGETS_ARRAY", () => {
+
     test("should contain entries in expected order", () => {
       const names = DEFAULT_TARGETS_ARRAY.map((definition) => definition.name);
       // These are the buttons we want to see in the given order by default,
       // if no other configuration got provided.
       expect(names).toEqual(["_self", "_blank", "_embed", "_other"]);
     });
+
+    test.each`
+       name | title
+       ${"_self"} | ${"Open in Current Tab"}
+       ${"_blank"} | ${"Open in New Tab"}
+       ${"_embed"} | ${"Show Embedded"}
+       ${"_other"} | ${"Open in Frame"}
+    `("[$#] Should provide expected title for $name: $title", ({ name, title: expectedTitle }) => {
+      const options = DEFAULT_TARGETS_ARRAY.find((definition) => name === definition.name);
+      // Precondition-check
+      expect(options).toBeTruthy();
+      const { title: actualTitle } = options!;
+      expect(actualTitle).toStrictEqual(expectedTitle);
+    });
+
   });
+
 });
