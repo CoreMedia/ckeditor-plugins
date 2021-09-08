74e5e9890a5c4e0fc6804b6ccba16f314a731fce
Mark Michaelis <mark.michaelis@coremedia.com>
2021-09-08 12:04:33 +0200

    test(link-target): Extend Tests for DefaultTarget
    * Testing for icon property in config.
    * Adding test for requireDefaultTargetDefinition

--- packages/ckeditor5-link/__tests__/linktarget/config/DefaultTarget.test.ts
+++ packages/ckeditor5-link/__tests__/linktarget/config/DefaultTarget.test.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_TARGETS_ARRAY, getDefaultTargetDefinition } from "../../../src/linktarget/config/DefaultTarget";
+import { DEFAULT_TARGETS_ARRAY, getDefaultTargetDefinition, requireDefaultTargetDefinition } from "../../../src/linktarget/config/DefaultTarget";
 
 describe("DefaultTarget", () => {
 
@@ -20,9 +20,23 @@ describe("DefaultTarget", () => {
     `("[$#] Should provide expected title for $name: $title", ({ name, title: expectedTitle }) => {
       const options = DEFAULT_TARGETS_ARRAY.find((definition) => name === definition.name);
       // Precondition-check
-      expect(options).toBeTruthy();
+      expect(options).toBeDefined();
       const { title: actualTitle } = options!;
       expect(actualTitle).toStrictEqual(expectedTitle);
+    });
+
+    test.each`
+       name
+       ${"_self"}
+       ${"_blank"}
+       ${"_embed"}
+       ${"_other"}
+    `("[$#] Should provide an icon $name", ({ name }) => {
+      const options = DEFAULT_TARGETS_ARRAY.find((definition) => name === definition.name);
+      // Precondition-check
+      expect(options).toBeDefined();
+      const { icon: actualIcon } = options!;
+      expect(actualIcon).toBeTruthy();
     });
 
   });
@@ -66,6 +80,32 @@ describe("DefaultTarget", () => {
       expect(actualTitle).toStrictEqual(expectedTitle);
     });
 
+  });
+
+  describe("requireDefaultTargetDefinition", () => {
+    const defTarget = "Default Standard Target";
+    const xLinkBehavior = "While used for xlink:show, not part of our standard definitions";
+    const htmlBehavior = "While standard HTML target, not part of our standard definitions";
+
+    test.each`
+      name         | exists   | comment
+      ${"_self"}   | ${true}  | ${defTarget}
+      ${"_blank"}  | ${true}  | ${defTarget}
+      ${"_embed"}  | ${true}  | ${defTarget}
+      ${"_other"}  | ${true}  | ${defTarget}
+      ${"_other"}  | ${true}  | ${defTarget}
+      ${"_none"}   | ${false} | ${xLinkBehavior}
+      ${"_parent"} | ${false} | ${htmlBehavior}
+      ${"_top"}    | ${false} | ${htmlBehavior}
+      ${"custom"}  | ${false} | ${"Any custom target is not expected to be defined by default"}
+    `("[$#] Expecting default definition for $name? $exists ($comment)", ({ name, exists }) => {
+      const definitionCallback = () => requireDefaultTargetDefinition(name);
+      if (exists) {
+        expect(definitionCallback).not.toThrow();
+      } else {
+        expect(definitionCallback).toThrow();
+      }
+    });
   });
 
 });
