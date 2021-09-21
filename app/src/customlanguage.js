cd907e7f473038a4ffc21bbc0f2b49349e513b65
jdallman <Jens.Dallmann@coremedia.com>
2021-09-21 10:18:13 +0200

    Override existing ones

--- app/src/customlanguage.js
+++ app/src/customlanguage.js
@@ -1,11 +1,24 @@
-// Make sure that the global object is defined. If not, define it.
 window.CKEDITOR_TRANSLATIONS = window.CKEDITOR_TRANSLATIONS || {};
 
-// Make sure that the dictionary for Polish translations exist.
 window.CKEDITOR_TRANSLATIONS[ 'es' ] = window.CKEDITOR_TRANSLATIONS[ 'es' ] || {};
 window.CKEDITOR_TRANSLATIONS[ 'es' ].dictionary =  window.CKEDITOR_TRANSLATIONS[ 'es' ].dictionary || {};
 
-// Extend the dictionary for Polish translations with your translations:
 Object.assign( window.CKEDITOR_TRANSLATIONS[ 'es' ].dictionary, {
   'Enter url or drag and drop content onto this area.': 'Introduzca la url o arrastre y suelte el contenido en esta área.'
+} );
+
+
+window.CKEDITOR_TRANSLATIONS[ 'de' ] = window.CKEDITOR_TRANSLATIONS[ 'de' ] || {};
+window.CKEDITOR_TRANSLATIONS[ 'de' ].dictionary =  window.CKEDITOR_TRANSLATIONS[ 'de' ].dictionary || {};
+
+Object.assign( window.CKEDITOR_TRANSLATIONS[ 'de' ].dictionary, {
+  'Enter url or drag and drop content onto this area.': 'URL hier rein!!!'
+} );
+
+
+window.CKEDITOR_TRANSLATIONS[ 'en' ] = window.CKEDITOR_TRANSLATIONS[ 'en' ] || {};
+window.CKEDITOR_TRANSLATIONS[ 'en' ].dictionary =  window.CKEDITOR_TRANSLATIONS[ 'en' ].dictionary || {};
+
+Object.assign( window.CKEDITOR_TRANSLATIONS[ 'en' ].dictionary, {
+  'Enter url or drag and drop content onto this area.': 'URL here!!!'
 } );
