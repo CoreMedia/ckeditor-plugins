0b4bc3502d613b12ee7606eea4c28442397f6a3f
jdallman <Jens.Dallmann@coremedia.com>
2021-09-27 14:35:11 +0200

    Switch from PO-File to a typescript definition
    Unfortunately the po files are preprocessed and variables can't be used. To use variables we have to use the add method instead.

---
+++ packages/ckeditor5-coremedia-link/src/lang/contentlink.ts
@@ -0,0 +1,6 @@
+import { add } from "@ckeditor/ckeditor5-utils/src/translation-service";
+
+add("de", {
+  Link: "Verknüpfung",
+  "Enter url or drag and drop content onto this area.": "URL angeben oder Inhalt hierher ziehen",
+});
