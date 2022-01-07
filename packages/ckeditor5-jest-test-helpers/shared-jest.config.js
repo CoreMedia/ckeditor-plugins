a46eb5c00a0289b8587b604bc3c7ec17830d58a2
Mark Michaelis <mark.michaelis@coremedia.com>
2022-01-07 08:42:28 +0100

    build(jest): Require `*.test.ts` as Test Name
    Don't detect utility files as tests, i.e. require `test` in name.

--- packages/ckeditor5-jest-test-helpers/shared-jest.config.js
+++ packages/ckeditor5-jest-test-helpers/shared-jest.config.js
@@ -4,6 +4,10 @@ module.exports = {
   // https://github.com/facebook/jest/issues/8896
   // passWithNoTests: true,
   testEnvironment: 'jsdom',
+  // Don't detect utility files as tests, i.e. require `test` in name.
+  testMatch: [
+    "**/?(*.)+(test).[jt]s?(x)",
+  ],
   moduleFileExtensions: ["js", "ts", "d.ts"],
   "moduleNameMapper": {
     // https://www.npmjs.com/package/jest-transform-stub
