816645e178d6349e7499104b243215641951cd20
Mark Michaelis <mark.michaelis@coremedia.com>
2022-01-10 16:49:02 +0100

    test(data-processor): Adding Tests for Attributes of Inline Elements

--- packages/ckeditor5-coremedia-richtext/__tests__/rules/Inline.test.ts
+++ packages/ckeditor5-coremedia-richtext/__tests__/rules/Inline.test.ts
@@ -23,6 +23,7 @@ describe("CoreMediaRichTextConfig: Miscellaneous Inline Tags", () => {
       },
     ].map(({ view, data, bijective }): DataProcessingTestCase[] => {
       const key = view.toUpperCase();
+      // noinspection HtmlUnknownAttribute
       return [
         {
           name: `${key}#1: View: <${view}> ${bijective ? "<" : ""}-> Data: <${data}>.`,
@@ -35,6 +36,42 @@ describe("CoreMediaRichTextConfig: Miscellaneous Inline Tags", () => {
           direction: Direction.toData,
           data: wrapContent(`<p><${data}>${text}</${data}></p>`),
           dataView: wrapContent(`<p><${data}>${text}</${data}></p>`),
+        },
+        {
+          name: `${key}#3: Keep class attribute — View: <${view}> ${bijective ? "<" : ""}-> Data: <${data}>.`,
+          direction: bijective ? Direction.both : Direction.toData,
+          data: wrapContent(`<p><${data} class="CLASS">${text}</${data}></p>`),
+          dataView: wrapContent(`<p><${view} class="CLASS">${text}</${view}></p>`),
+        },
+        {
+          name: `${key}#4: Should keep <${data}> when transformed along with class attribute.`,
+          direction: Direction.toData,
+          data: wrapContent(`<p><${data} class="CLASS">${text}</${data}></p>`),
+          dataView: wrapContent(`<p><${data} class="CLASS">${text}</${data}></p>`),
+        },
+        {
+          name: `${key}#5: Keep dir attribute — View: <${view}> ${bijective ? "<" : ""}-> Data: <${data}>.`,
+          direction: bijective ? Direction.both : Direction.toData,
+          data: wrapContent(`<p><${data} dir="rtl">${text}</${data}></p>`),
+          dataView: wrapContent(`<p><${view} dir="rtl">${text}</${view}></p>`),
+        },
+        {
+          name: `${key}#6: Should keep <${data}> when transformed along with dir attribute.`,
+          direction: Direction.toData,
+          data: wrapContent(`<p><${data} dir="rtl">${text}</${data}></p>`),
+          dataView: wrapContent(`<p><${data} dir="rtl">${text}</${data}></p>`),
+        },
+        {
+          name: `${key}#5: Keep lang/xml:lang attribute — View: <${view}> ${bijective ? "<" : ""}-> Data: <${data}>.`,
+          direction: bijective ? Direction.both : Direction.toData,
+          data: wrapContent(`<p><${data} xml:lang="en">${text}</${data}></p>`),
+          dataView: wrapContent(`<p><${view} lang="en">${text}</${view}></p>`),
+        },
+        {
+          name: `${key}#6: Should keep <${data}> when transformed along with lang/xml:lang attribute.`,
+          direction: Direction.toData,
+          data: wrapContent(`<p><${data} xml:lang="en">${text}</${data}></p>`),
+          dataView: wrapContent(`<p><${data} lang="en">${text}</${data}></p>`),
         },
       ];
     })
@@ -67,6 +104,7 @@ describe("CoreMediaRichTextConfig: Miscellaneous Inline Tags", () => {
       // The mapping, which corresponds to the default representation in
       // CKEditor should be bijective (i.e. = true).
       const key = view.toUpperCase();
+      // noinspection HtmlUnknownAttribute
       return [
         {
           name: `${key}#1: View: <${view}> ${bijective ? "<" : ""}-> Data: by <span class="${dataClass}">.`,
@@ -79,6 +117,30 @@ describe("CoreMediaRichTextConfig: Miscellaneous Inline Tags", () => {
           direction: Direction.toData,
           data: wrapContent(`<p><span class="${dataClass}">${text}</span></p>`),
           dataView: wrapContent(`<p><span class="${dataClass}">${text}</span></p>`),
+        },
+        {
+          name: `${key}#3: Keep class attribute — View: <${view}> ${
+            bijective ? "<" : ""
+          }-> Data: by <span class="${dataClass}">.`,
+          direction: bijective ? Direction.both : Direction.toData,
+          data: wrapContent(`<p><span class="CLASS ${dataClass}">${text}</span></p>`),
+          dataView: wrapContent(`<p><${view} class="CLASS">${text}</${view}></p>`),
+        },
+        {
+          name: `${key}#4: Keep dir attribute — View: <${view}> ${
+            bijective ? "<" : ""
+          }-> Data: by <span class="${dataClass}">.`,
+          direction: bijective ? Direction.both : Direction.toData,
+          data: wrapContent(`<p><span dir="rtl" class="${dataClass}">${text}</span></p>`),
+          dataView: wrapContent(`<p><${view} dir="rtl">${text}</${view}></p>`),
+        },
+        {
+          name: `${key}#4: Keep lang/xml:lang attribute — View: <${view}> ${
+            bijective ? "<" : ""
+          }-> Data: by <span class="${dataClass}">.`,
+          direction: bijective ? Direction.both : Direction.toData,
+          data: wrapContent(`<p><span class="${dataClass}" xml:lang="en">${text}</span></p>`),
+          dataView: wrapContent(`<p><${view} lang="en">${text}</${view}></p>`),
         },
       ];
     })
