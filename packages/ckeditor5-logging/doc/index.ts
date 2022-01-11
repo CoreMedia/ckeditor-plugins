88936c03e93751581cb09496c8f46c42e79a6df6
Mark Michaelis <mark.michaelis@coremedia.com>
2022-01-11 14:29:30 +0100

    build(doc): First Example for TypeDoc
    Introducing first example for generating TSDoc by TypeDoc.
    
    Some design decisions:
    
    * index.ts:
    
        We don't want to make it part of our default packaging. To do so,
        we can just add an extra path (here: `doc/`) within a module, which
        holds in `index.ts` just for TypeDoc.
    
    * docs/api:
    
        This folder shall hold our documentation, which will automatically
        become part of GitHub Pages site.
    
    This state works for generating TypeDoc for the Logging Module.
    
    See-also: <https://typedoc.org/>

---
+++ packages/ckeditor5-logging/doc/index.ts
@@ -0,0 +1,8 @@
+export * from "../src/logging/Logger";
+export { default as Logger } from "../src/logging/Logger";
+
+export * from "../src/logging/LogLevel";
+export { LogLevel } from "../src/logging/LogLevel";
+
+export * from "../src/logging/LoggerProvider";
+export { default as LoggerProvider } from "../src/logging/LoggerProvider";
