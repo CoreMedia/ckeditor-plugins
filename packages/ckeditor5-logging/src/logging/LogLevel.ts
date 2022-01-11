3c4884227f9d516392e8311d81b6a4d343b4d9e3
Mark Michaelis <mark.michaelis@coremedia.com>
2022-01-11 14:59:31 +0100

    fix(docs): Documentation Fixed: LogLevel

--- packages/ckeditor5-logging/src/logging/LogLevel.ts
+++ packages/ckeditor5-logging/src/logging/LogLevel.ts
@@ -1,7 +1,26 @@
+/**
+ * Log Levels to use. This includes the artificial level
+ * `none` to suppress output by a given logger.
+ */
 export enum LogLevel {
+  /**
+   * Level for debug and above output.
+   */
   DEBUG,
+  /**
+   * Level for info and above output.
+   */
   INFO,
+  /**
+   * Level for warn and above output.
+   */
   WARN,
+  /**
+   * Level for error output.
+   */
   ERROR,
+  /**
+   * Artificial level to disable logging.
+   */
   NONE,
 }
