c0e76933a69db136cbd351e826bbe46362d4ffbc
Mark Michaelis <mark.michaelis@coremedia.com>
2022-01-12 12:11:48 +0100

    docs(studio-integration): Add Module ckeditor5-coremedia-studio-integration
    New to TypeDoc Generation:
    
    * ckeditor5-coremedia-studio-integration

---
+++ packages/ckeditor5-coremedia-studio-integration/src/content/index-doc.ts
@@ -0,0 +1,37 @@
+/**
+ * Provides access to information on contents.
+ *
+ * @packageDocumentation
+ * @category Virtual
+ */
+export * as studioservices from "./studioservices/index-doc";
+
+export * from "./ContentAsLink";
+export { default as ContentAsLink } from "./ContentAsLink";
+
+export * from "./ContentDisplayService";
+export { default as ContentDisplayService } from "./ContentDisplayService";
+
+export * from "./ContentDisplayServiceDescriptor";
+export { default as ContentDisplayServiceDescriptor } from "./ContentDisplayServiceDescriptor";
+
+export * from "./DisplayHint";
+export { default as DisplayHint } from "./DisplayHint";
+
+export * from "./DragAndDropUtils";
+//export { default as DragAndDropUtils } from "./DragAndDropUtils";
+
+export * from "./DragDropAsyncSupport";
+export { default as DragDropAsyncSupport } from "./DragDropAsyncSupport";
+
+export * from "./RichtextConfigurationService";
+export { default as RichtextConfigurationService } from "./RichtextConfigurationService";
+
+export * from "./RichtextConfigurationServiceDescriptor";
+export { default as RichtextConfigurationServiceDescriptor } from "./RichtextConfigurationServiceDescriptor";
+
+export * from "./UriPath";
+//export { default as UriPath } from "./UriPath";
+
+export * from "./WorkAreaServiceDescriptor";
+export { default as WorkAreaServiceDescriptor } from "./WorkAreaServiceDescriptor";
---
+++ packages/ckeditor5-coremedia-studio-integration/src/content/studioservices/index-doc.ts
@@ -0,0 +1,9 @@
+/**
+ * @packageDocumentation
+ * @category Virtual
+ */
+export * from "./DragDropService";
+export { default as DragDropService } from "./DragDropService";
+
+export * from "./WorkAreaService";
+export { default as WorkAreaService } from "./WorkAreaService";
