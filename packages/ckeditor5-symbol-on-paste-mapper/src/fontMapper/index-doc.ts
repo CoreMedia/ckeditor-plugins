e8d9b109b4830ae97cada605f3f55e4938fa81d3
Mark Michaelis <mark.michaelis@coremedia.com>
2022-01-12 13:33:01 +0100

    docs(studio-integration): Add Module ckeditor5-symbol-on-paste-mapper
    New to TypeDoc Generation:
    
    * ckeditor5-symbol-on-paste-mapper

---
+++ packages/ckeditor5-symbol-on-paste-mapper/src/fontMapper/index-doc.ts
@@ -0,0 +1,12 @@
+/**
+ * @packageDocumentation
+ * @category Virtual
+ */
+export * from "./FontMapper";
+export { default as FontMapper } from "./FontMapper";
+
+export * from "./FontMapperProvider";
+export { default as FontMapperProvider } from "./FontMapperProvider";
+
+export * from "./SymbolFontMapper";
+export { default as SymbolFontMapper } from "./SymbolFontMapper";
