d316610ceca4e57063a3b5601abf71a8dd46871f
jdallman <Jens.Dallmann@coremedia.com>
2022-02-09 13:02:35 +0100

    Setup the conversion for the attributes from DataProcessing except class.

--- packages/ckeditor5-coremedia-images/README.md
+++ packages/ckeditor5-coremedia-images/README.md
@@ -5,16 +5,16 @@ CoreMedia CKEditor 5 Images
 
 Relevant attributes:
 
-| Data            | View                 | Model | Plugin  | Data Example                              |
-|-----------------|----------------------|-------|---------|-------------------------------------------|
-| `alt`           | `alt`                | `alt` | default | `alt="Some Alternative"`                  |
-| `dir`           | `dir`                | `dir` |         | `dir="ltr"`                               |
-| `xlink:actuate` | `data-xlink-actuate` | `?`   |         | `xlink:actuate="onLoad"`                  |
-| `xlink:href `   | `data-xlink-href`    | `src` | default | `xlink:href="content/42#properties.data"` | 
-| `xlink:role`    | `data-xlink-role`    | ``    |         | `xlink:role="https://example.org/"`       | 
-| `xlink:show`    | `data-xlink-show`    | ``    |         | `xlink:show="embed"`                      |
-| `xlink:title`   | `title`              | `?`   |         | `xlink:title="All Attributes"`            | 
-| `xlink:type`    | `data-xlink-type`    | ``    |         | `xlink:type="simple"`                     |
+| Data            | View                 | Model           | Plugin  | Data Example                              |
+|-----------------|----------------------|-----------------|---------|-------------------------------------------|
+| `alt`           | `alt`                | `alt`           | default | `alt="Some Alternative"`                  |
+| `dir`           | `dir`                | `dir`           |         | `dir="ltr"`                               |
+| `xlink:actuate` | `data-xlink-actuate` | `xlink-actuate` |         | `xlink:actuate="onLoad"`                  |
+| `xlink:href `   | `data-xlink-href`    | `xlink-href`    | default | `xlink:href="content/42#properties.data"` | 
+| `xlink:role`    | `data-xlink-role`    | `xlink-role`    |         | `xlink:role="https://example.org/"`       | 
+| `xlink:show`    | `data-xlink-show`    | `xlink-show`    |         | `xlink:show="embed"`                      |
+| `xlink:title`   | `title`              | `title`         |         | `xlink:title="All Attributes"`            | 
+| `xlink:type`    | `data-xlink-type`    | `xlink-type`    |         | `xlink:type="simple"`                     |
 
 Not that relevant attributes:
 
--- packages/ckeditor5-coremedia-images/src/ContentImageEditingPlugin.ts
+++ packages/ckeditor5-coremedia-images/src/ContentImageEditingPlugin.ts
@@ -1,11 +1,6 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import Editor from "@ckeditor/ckeditor5-core/src/editor/editor";
 import Image from "@ckeditor/ckeditor5-image/src/image";
-import UpcastDispatcher, { UpcastConversionApi } from "@ckeditor/ckeditor5-engine/src/conversion/upcastdispatcher";
-import EventInfo from "@ckeditor/ckeditor5-utils/src/eventinfo";
-import { Item } from "@ckeditor/ckeditor5-engine/src/view/item";
-import Position from "@ckeditor/ckeditor5-engine/src/model/position";
-import first from "@ckeditor/ckeditor5-utils/src/first";
 
 export default class ContentImageEditingPlugin extends Plugin {
   static readonly pluginName: string = "ContentImageEditingPlugin";
@@ -15,41 +10,26 @@ export default class ContentImageEditingPlugin extends Plugin {
   }
 
   init(): null {
-    this.editor.model.schema.extend("imageInline", {
-      allowAttributes: ["xlink:href", "xlink:show"],
-    });
-    //this.editor.conversion.attributeToAttribute({ model: "xlink:show", view: "data-xlink-show" });
-    this.editor.conversion.attributeToAttribute({ model: "xlink:href", view: "data-xlink-href" });
-    this.editor.conversion.for("upcast").add(
-      (() => {
-        return (dispatcher: UpcastDispatcher) => {
-          dispatcher.on("element:img", this.converterFunction);
-        };
-      })()
-    );
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-href", "data-xlink-href");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-role", "data-xlink-role");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-show", "data-xlink-show");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-actuate", "data-xlink-actuate");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-type", "data-xlink-type");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "title", "title");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "dir", "dir");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "lang", "lang");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "height", "height");
+    ContentImageEditingPlugin.#setupAttribute(this.editor, "width", "width");
     return null;
   }
 
-  converterFunction(
-    event: EventInfo,
-    data: { viewItem: Item; modelCursor: Position },
-    conversionApi: UpcastConversionApi
-  ): void {
-    if (!conversionApi.consumable.consume(data.viewItem, { attributes: "data-xlink-show" })) {
-      return;
-    }
-
-    const conversionResult = conversionApi.convertItem(data.viewItem, data.modelCursor);
-    const modelRange = conversionResult.modelRange;
-    if (!modelRange) {
-      conversionApi.consumable.revert(data.viewItem, { attributes: "data-xlink-show" });
-      return;
-    }
-    const modelImage = first(modelRange.getItems());
-    if (!modelImage) {
-      conversionApi.consumable.revert(data.viewItem, { attributes: "data-xlink-show" });
-      return;
-    }
-    conversionApi.writer.setAttribute("xlink:show", data.viewItem.getAttribute("data-xlink-show"), modelImage);
+  static #setupAttribute(editor: Editor, model: string, view: string): void {
+    editor.model.schema.extend("imageInline", {
+      allowAttributes: [model],
+    });
+    editor.conversion.attributeToAttribute({
+      model: { name: "imageInline", key: model },
+      view: { name: "img", key: view },
+    });
   }
 }
