25a3ba1156a92979a27e2a0341ee3d0099a0cf1a
jdallman <Jens.Dallmann@coremedia.com>
2022-02-23 07:29:41 +0100

    Remove conversion for attributes which are currently not supported to edit.

--- packages/ckeditor5-coremedia-images/src/ContentImageEditingPlugin.ts
+++ packages/ckeditor5-coremedia-images/src/ContentImageEditingPlugin.ts
@@ -1,12 +1,6 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import Editor from "@ckeditor/ckeditor5-core/src/editor/editor";
-import {
-  dataDowncastCustomClasses,
-  editingDowncastCustomClasses,
-  editingDowncastXlinkHref,
-  preventUpcastImageSrc,
-  upcastCustomClasses,
-} from "./converters";
+import { editingDowncastXlinkHref, preventUpcastImageSrc } from "./converters";
 import ImageUtils from "@ckeditor/ckeditor5-image/src/imageutils";
 import ModelBoundSubscriptionPlugin from "./ModelBoundSubscriptionPlugin";
 import { imageInlineElementToElementConversionPatch } from "./patches";
@@ -26,17 +20,7 @@ export default class ContentImageEditingPlugin extends Plugin {
     if (subscriptionPlugin) {
       subscriptionPlugin.registerModelElement("imageInline");
     }
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-role", "data-xlink-role");
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-show", "data-xlink-show");
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-actuate", "data-xlink-actuate");
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "xlink-type", "data-xlink-type");
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "title", "title");
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "dir", "dir");
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "lang", "lang");
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "height", "height");
-    ContentImageEditingPlugin.#setupAttribute(this.editor, "width", "width");
     ContentImageEditingPlugin.#setupXlinkHrefConversion(this.editor, "xlink-href", "data-xlink-href");
-    ContentImageEditingPlugin.#setupCustomClassConversion(this.editor, "img", "imageInline");
     imageInlineElementToElementConversionPatch(this.editor);
 
     // We have to prevent to write src-attribute to model because we fetch the src attribute for the editing view asynchronously.
@@ -46,36 +30,31 @@ export default class ContentImageEditingPlugin extends Plugin {
   }
 
   static #setupXlinkHrefConversion(editor: Editor, modelAttributeName: string, dataAttributeName: string): void {
-    editor.model.schema.extend("imageInline", {
-      allowAttributes: [modelAttributeName],
-    });
+    this.#setupXlinkHrefConversionDowncast(editor, "imageInline", modelAttributeName, dataAttributeName);
     editor.conversion.for("upcast").attributeToAttribute({
       model: modelAttributeName,
       view: { name: "img", key: dataAttributeName },
     });
+  }
+
+  static #setupXlinkHrefConversionDowncast(
+    editor: Editor,
+    modelElementName: "imageInline",
+    modelAttributeName: string,
+    dataAttributeName: string
+  ): void {
+    editor.model.schema.extend(modelElementName, {
+      allowAttributes: [modelAttributeName],
+    });
     editor.conversion.for("dataDowncast").attributeToAttribute({
-      model: modelAttributeName,
+      model: {
+        name: modelElementName,
+        key: modelAttributeName,
+      },
       view: dataAttributeName,
     });
-    editor.conversion.for("editingDowncast").add(editingDowncastXlinkHref(editor, "imageInline", modelAttributeName));
-  }
-
-  static #setupAttribute(editor: Editor, model: string, view: string): void {
-    editor.model.schema.extend("imageInline", {
-      allowAttributes: [model],
-    });
-    editor.conversion.attributeToAttribute({
-      model: { name: "imageInline", key: model },
-      view: { name: "img", key: view },
-    });
-  }
-
-  static #setupCustomClassConversion(editor: Editor, viewElementName: string, modelElementName: string): void {
-    editor.model.schema.extend(modelElementName, { allowAttributes: ["cmClass"] });
-    editor.conversion.for("upcast").add(upcastCustomClasses(viewElementName));
     editor.conversion
       .for("editingDowncast")
-      .add(editingDowncastCustomClasses(editor, viewElementName, modelElementName));
-    editor.conversion.for("dataDowncast").add(dataDowncastCustomClasses(viewElementName, modelElementName));
+      .add(editingDowncastXlinkHref(editor, modelElementName, modelAttributeName));
   }
 }
--- packages/ckeditor5-coremedia-images/src/converters.ts
+++ packages/ckeditor5-coremedia-images/src/converters.ts
@@ -1,19 +1,13 @@
 /**
  * Creates an upcast converter that will pass all classes from the view element to the model element.
  */
-import {
-  DowncastConversionHelperFunction,
-  UpcastConversionHelperFunction,
-} from "@ckeditor/ckeditor5-engine/src/conversion/conversionhelpers";
+import { DowncastConversionHelperFunction } from "@ckeditor/ckeditor5-engine/src/conversion/conversionhelpers";
 import UpcastDispatcher, {
   UpcastConversionApi,
   UpcastEventData,
 } from "@ckeditor/ckeditor5-engine/src/conversion/upcastdispatcher";
 import EventInfo from "@ckeditor/ckeditor5-utils/src/eventinfo";
-import DowncastDispatcher, {
-  DowncastConversionApi,
-  DowncastEventData,
-} from "@ckeditor/ckeditor5-engine/src/conversion/downcastdispatcher";
+import DowncastDispatcher, { DowncastEventData } from "@ckeditor/ckeditor5-engine/src/conversion/downcastdispatcher";
 import ViewElement from "@ckeditor/ckeditor5-engine/src/view/element";
 import { serviceAgent } from "@coremedia/service-agent";
 import BlobDisplayServiceDescriptor from "@coremedia/ckeditor5-coremedia-studio-integration/content/BlobDisplayServiceDescriptor";
@@ -42,72 +36,6 @@ export const preventUpcastImageSrc = () => {
       { priority: "highest" }
     );
   };
-};
-
-export const upcastCustomClasses = (viewElementName: string): UpcastConversionHelperFunction => {
-  return (dispatcher: UpcastDispatcher): void =>
-    dispatcher.on(`element:${viewElementName}`, onImgTagUpcast, { priority: "low" });
-};
-
-const onImgTagUpcast = (evt: EventInfo, data: UpcastEventData, conversionApi: UpcastConversionApi): void => {
-  const viewItem = data.viewItem;
-  const modelRange = data.modelRange;
-
-  const modelElement = modelRange && modelRange.start.nodeAfter;
-  if (!modelElement) {
-    return;
-  }
-
-  const currentAttributeValue = modelElement.getAttribute("cmClass") || [];
-
-  currentAttributeValue.push(...viewItem.getClassNames());
-  conversionApi.writer.setAttribute("cmClass", currentAttributeValue, modelElement);
-};
-
-export const dataDowncastCustomClasses = (
-  viewElementName: string,
-  modelElementName: string
-): DowncastConversionHelperFunction => {
-  return (dispatcher: DowncastDispatcher) =>
-    dispatcher.on(
-      `insert:${modelElementName}`,
-      (evt: EventInfo, data: DowncastEventData, conversionApi: DowncastConversionApi) => {
-        onImageInlineDataDowncast(viewElementName, data, conversionApi);
-      },
-      { priority: "low" }
-    );
-};
-
-const onImageInlineDataDowncast = (
-  viewElementName: string,
-  data: DowncastEventData,
-  conversionApi: DowncastConversionApi
-): void => {
-  const modelElement = data.item;
-  const viewImage = conversionApi.mapper.toViewElement(modelElement);
-  if (!viewImage) {
-    return;
-  }
-
-  if (!viewImage.is("element", viewElementName)) {
-    return;
-  }
-  conversionApi.writer.addClass(modelElement.getAttribute("cmClass"), viewImage);
-};
-
-export const editingDowncastCustomClasses = (
-  editor: Editor,
-  viewElementName: string,
-  modelElementName: string
-): DowncastConversionHelperFunction => {
-  return (dispatcher: DowncastDispatcher) =>
-    dispatcher.on(
-      `insert:${modelElementName}`,
-      (evt: EventInfo, data: DowncastEventData, conversionApi: DowncastConversionApi) => {
-        onImageInlineEditingDowncast(editor, viewElementName, data, conversionApi);
-      },
-      { priority: "low" }
-    );
 };
 
 export const editingDowncastXlinkHref = (
@@ -202,29 +130,6 @@ const toUriPath = (xlinkHref: string): string => {
 
 const toProperty = (xlinkHref: string): string => {
   return xlinkHref.split("#")[1];
-};
-
-const onImageInlineEditingDowncast = (
-  editor: Editor,
-  viewElementName: string,
-  data: DowncastEventData,
-  conversionApi: DowncastConversionApi
-): void => {
-  const modelElement = data.item;
-  const viewSpan = conversionApi.mapper.toViewElement(modelElement);
-  if (!viewSpan) {
-    return;
-  }
-
-  if (!viewSpan.is("element", "span")) {
-    return;
-  }
-
-  const viewImageElement = findViewChild(editor, viewSpan, viewElementName);
-  if (!viewImageElement) {
-    return;
-  }
-  conversionApi.writer.addClass(modelElement.getAttribute("cmClass"), viewImageElement);
 };
 
 const findViewChild = (editor: Editor, viewElement: ViewElement, viewElementName: string): ViewElement | null => {
