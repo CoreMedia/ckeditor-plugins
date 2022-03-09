5cc726c8d2888d2a78f82748958971865bd6eea4
Mark Michaelis <mark.michaelis@coremedia.com>
2022-03-09 14:10:19 +0100

    refactor(studio-integration): Adjustments to BlobDisplayService
    * Fix some TSDoc issues.
    * Migrate `InlinePreview` to interface and add some documentation.

--- packages/ckeditor5-coremedia-studio-integration/src/content/BlobDisplayService.ts
+++ packages/ckeditor5-coremedia-studio-integration/src/content/BlobDisplayService.ts
@@ -2,20 +2,41 @@ import { ServiceObject } from "@coremedia/service-agent";
 import { UriPath } from "./UriPath";
 import { Observable } from "rxjs";
 
-export type InlinePreview = { thumbnailSrc: string; thumbnailTitle: string; isPlaceholder: boolean };
+/**
+ * Represents data for creating an inline representation of the referenced
+ * image.
+ */
+export interface InlinePreview {
+  /**
+   * Where to read the blob data from.
+   */
+  thumbnailSrc: string;
+  /**
+   * A title to apply to the thumbnail representation.
+   */
+  thumbnailTitle: string;
+  /**
+   * `false` for normal images. `true` if the `thumbnailSrc` refers
+   * to a placeholder image instead. May trigger different rendering.
+   */
+  isPlaceholder: boolean;
+}
 
-interface BlobDisplayService extends ServiceObject {
+/**
+ * Service to receive data from blobs like images, audio data, etc.
+ */
+export default interface BlobDisplayService extends ServiceObject {
   /**
    * Provides information to render a preview of the given blob.
    *
-   * For unreadable or not existing content or not existing data, it is expected, that the response contains
-   * some kind of visualization for authors about the state (e.g. unreadable - some image which visualizes that the content is unreadable).
+   * For unreadable or not existing content or not existing data, it is
+   * expected, that the response contains some kind of visualization for editors
+   * about the state (e.g., unreadable — some image, which visualizes that the
+   * content is unreadable).
    *
    * @param uriPath - URI path of the content, such as `content/120`
-   * @param property - the property which contains the blob data
+   * @param property - the property, which contains the blob data
    * @returns Observable which resolves to the InlinePreview.
    */
   observe_asInlinePreview(uriPath: UriPath, property: string): Observable<InlinePreview>;
 }
-
-export default BlobDisplayService;
