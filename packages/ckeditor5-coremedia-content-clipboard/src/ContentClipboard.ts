61f7b61988e6d340f4891ea5fa61992ea4413f0e
Mark Michaelis <mark.michaelis@coremedia.com>
2022-03-16 07:42:06 +0100

    refactor(studio-integration): Reformat comments
    ... and reword some of them.

--- packages/ckeditor5-coremedia-content-clipboard/src/ContentClipboard.ts
+++ packages/ckeditor5-coremedia-content-clipboard/src/ContentClipboard.ts
@@ -42,7 +42,8 @@ export default class ContentClipboard extends Plugin {
   }
 
   /**
-   * Adds a listener to `dragover` and `clipboardInput` to process possibly dragged contents.
+   * Adds a listener to `dragover` and `clipboardInput` to process possibly
+   * dragged contents.
    */
   #initEventListeners(): void {
     const editor = this.editor;
@@ -81,8 +82,8 @@ export default class ContentClipboard extends Plugin {
    * @param data - clipboard data
    */
   static #dragOverHandler(evt: EventInfo, data: ClipboardEventData): void {
-    // The clipboard content was already processed by the listener on the higher priority
-    // (for example while pasting into the code block).
+    // The clipboard content was already processed by the listener on the
+    // higher priority (for example, while pasting into the code block).
     if (data.content) {
       return;
     }
@@ -93,10 +94,10 @@ export default class ContentClipboard extends Plugin {
 
     data.preventDefault();
     const containsDisplayableContents = DragDropAsyncSupport.containsDisplayableContents(cmDataUris);
-    // Applying dropEffects required to be run *after* CKEditor's normal listeners,
-    // which almost always enforce `move` as dropEffect. We also must not `stop`
-    // processing (at least at normal priority), as otherwise the range indicator
-    // won't be updated.
+    // Applying dropEffects required to be run *after* CKEditor's normal
+    // listeners, which almost always enforce `move` as dropEffect. We also must
+    // not `stop` processing (at least at normal priority), as otherwise the
+    // range indicator won't be updated.
     if (containsDisplayableContents) {
       data.dataTransfer.dropEffect = "link";
     } else {
@@ -110,58 +111,71 @@ export default class ContentClipboard extends Plugin {
    * an item is dropped or pasted into the editor.
    *
    * See: https://github.com/ckeditor/ckeditor5/blob/6eab4831ef4432152069c457c8921395315c1b33/packages/ckeditor5-clipboard/src/clipboardpipeline.js#L59-L121
-   * The goal here is to hook into the clipboardPipeline and use our inputTransform method if the clipboard data is a CoreMedia content.
+   * The goal here is to hook into the clipboardPipeline and use our
+   * inputTransform method if the clipboard data is a CoreMedia content.
    *
    * @param evt - event information
    * @param data - clipboard data
    */
   #clipboardInputHandler = (evt: EventInfo, data: ClipboardEventData): void => {
-    // return if this is no CoreMedia content drop
+    // Return if this is no CoreMedia content drop.
     if ((getUriListValues(data) ?? []).length === 0) {
       return;
     }
 
-    // this is kinda hacky, we need to set content to skip the default clipboardInputHandler
-    // by setting content, we mark this event as "already resolved"
+    // This is kinda hacky, we need to set content to skip the default
+    // clipboardInputHandler by setting content, we mark this event as
+    // "already resolved".
     data.content = new ViewDocumentFragment();
   };
 
   /**
    * Event listener callback that gets hooked into CKEditor's clipboardPipeline.
-   * If the retrieved data is a CoreMedia content, the event will be stopped. This also stops the default clipboardPipeline.
-   * After adding all needed markers (and UIElements), we trigger the last step of the pipeline manually by firing an event with an empty document fragment.
+   * If the retrieved data is a CoreMedia content, the event will be stopped.
+   * This also stops the default clipboardPipeline.
    *
-   * Important: This function also disabled the undo command. Be sure to enable it again after the content has been written to the model.
+   * After adding all needed markers (and UIElements), we trigger the last step
+   * of the pipeline manually by firing an event with an empty document
+   * fragment.
+   *
+   * **Important:** This function also disabled the undo command. Be sure to
+   * enable it again after the content has been written to the model.
    *
    * @param evt - event information
    * @param data - clipboard data
    */
   #inputTransformation = (evt: EventInfo, data: ClipboardEventData): void => {
     const cmDataUris: string[] = getUriListValues(data) ?? [];
-    // return if this is no CoreMedia content drop
+    // Return if this is no CoreMedia content drop.
     if (cmDataUris.length === 0) {
       return;
     }
 
     const editor = this.editor;
 
-    // return if no range has been set (usually indicated by a blue cursor during the drag)
+    // Return if no range has been set (usually indicated by a blue cursor
+    // during the drag)
     const targetRange = ContentClipboard.#evaluateTargetRange(editor, data);
     if (!targetRange) {
       return;
     }
 
-    // do not trigger the default inputTransformation event listener to avoid rendering the text of the input data
+    // Do not trigger the default inputTransformation event listener to avoid
+    // rendering the text of the input data.
     evt.stop();
 
     const containsOnlyDisplayableContents = DragDropAsyncSupport.containsDisplayableContents(cmDataUris);
     if (!containsOnlyDisplayableContents) {
       return;
     }
-    // we might run into trouble during complex input scenarios
-    // e.g., a drop with multiple items will result in different requests that might differ in response time
-    // an undo/redo when only a part of the input has already been resolved, will cause an unsynchronized state between content and placeholder elements
-    // the best solution for this seems to disable the undo command before the input and enable it again afterwards
+    // We might run into trouble during complex input scenarios.
+    // A drop with multiple items will result in different requests that might
+    // differ in response time, for example.
+    // Triggering undo/redo while only a part of the input has already been
+    // resolved, will cause an unsynchronized state between content and
+    // placeholder elements.
+    // The best solution for this seems to disable the undo command before the
+    // input and enable it again afterwards.
     CommandUtils.disableCommand(editor, "undo");
     editor.model.enqueueChange({ isUndoable: false }, (writer: Writer) => {
       writer.setSelection(targetRange);
@@ -169,11 +183,13 @@ export default class ContentClipboard extends Plugin {
 
     const batch = editor.model.createBatch();
 
-    // save the attributes of the current selection to apply them later on the input element
+    // Save the attributes of the current selection to apply them later on the
+    // input element.
     const attributes = Array.from(editor.model.document.selection.getAttributes());
 
-    // use the current timestamp as the dropId to have increasing drop indexes. needed to keep the order when multiple inputs happen simultaneously
-    // on the same position
+    // Use the current timestamp as the dropId to have increasing drop indexes.
+    // Needed to keep the order when multiple inputs happen simultaneously
+    // on the same position.
     const dropId = Date.now();
     const multipleItemsDropped = cmDataUris.length > 1;
     const dropContext: DropContext = {
@@ -182,7 +198,7 @@ export default class ContentClipboard extends Plugin {
       selectedAttributes: attributes,
     };
 
-    // add a drop marker for each item
+    // Add a drop marker for each item.
     cmDataUris.forEach((contentUri: string, index: number): void => {
       const isEmbeddableContent = DragDropAsyncSupport.isEmbeddable(contentUri, true);
       const contentDropData = ContentClipboard.#createContentDropData(
@@ -194,8 +210,9 @@ export default class ContentClipboard extends Plugin {
       ContentClipboard.#addContentDropMarker(editor, targetRange, contentDropData);
     });
 
-    // Fire content insertion event in a single change block to allow other handlers to run in the same block
-    // without post-fixers called in between (i.e., the selection post-fixer).
+    // Fire content insertion event in a single change block to allow other
+    // handlers to run in the same block without post-fixers called in between
+    // (i.e., the selection post-fixer).
     this.editor.model.change(() => {
       this.fire("contentInsertion", {
         content: new ModelDocumentFragment(),
@@ -208,8 +225,11 @@ export default class ContentClipboard extends Plugin {
 
   /**
    * Adds a marker to the editors model.
-   * A marker indicates the position of an input item, which can then be displayed in the editing view, but will not be written into the data view.
-   * This function also stores data for the dropped item (contentDropData) to the ContentDropDataCache.
+   *
+   * A marker indicates the position of an input item, which can then be
+   * displayed in the editing view, but will not be written into the data view.
+   * This function also stores data for the dropped item (contentDropData) to
+   * the ContentDropDataCache.
    *
    * @param editor - the editor
    * @param markerRange - the marker range
@@ -252,7 +272,8 @@ export default class ContentClipboard extends Plugin {
    *
    * @param dropContext - dropContext
    * @param contentUri - the content-URI of the input item
-   * @param isInline - determines whether the item will be displayed inline or as new paragraph
+   * @param isInline - determines whether the item will be displayed inline or
+   * as new paragraph
    * @param itemIndex - the position of the item inside the drop
    * @returns ContentDropData
    */
