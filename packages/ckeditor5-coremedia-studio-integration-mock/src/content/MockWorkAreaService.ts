de0d1ef78e0d40956bb5bdece70b106342b57346
Mark Michaelis <mark.michaelis@coremedia.com>
2022-04-29 10:04:14 +0200

    Promise to Async for: MockWorkAreaService.openEntitiesInTabs
    Use `async` rather than explicitly creating promise.

--- packages/ckeditor5-coremedia-studio-integration-mock/src/content/MockWorkAreaService.ts
+++ packages/ckeditor5-coremedia-studio-integration-mock/src/content/MockWorkAreaService.ts
@@ -1,24 +1,22 @@
 import WorkAreaService from "@coremedia/ckeditor5-coremedia-studio-integration/content/studioservices/WorkAreaService";
 
 class MockWorkAreaService implements WorkAreaService {
-  openEntitiesInTabs(entities: Array<unknown>): Promise<unknown> {
-    return new Promise((resolve: (resolve: { success: boolean }) => void): void => {
-      entities = entities.map((entity: unknown): unknown => {
-        const node: Element = document.createElement("DIV");
-        node.classList.add("notification");
-        const textnode: Text = document.createTextNode(`Open Content ${entity} in Studio Tab`);
-        node.appendChild(textnode);
-        document.getElementById("notifications")?.appendChild(node);
-        setTimeout(() => {
-          document.getElementById("notifications")?.removeChild(node);
-        }, 4000);
-        return entity;
-      });
-
-      resolve({ success: true });
+  async openEntitiesInTabs(entities: Array<unknown>): Promise<unknown> {
+    entities.forEach((entity: unknown): void => {
+      const node: Element = document.createElement("DIV");
+      node.classList.add("notification");
+      const textnode: Text = document.createTextNode(`Open Content ${entity} in Studio Tab`);
+      node.appendChild(textnode);
+      document.getElementById("notifications")?.appendChild(node);
+      setTimeout(() => {
+        document.getElementById("notifications")?.removeChild(node);
+      }, 4000);
     });
+
+    return { success: true };
   }
-  canBeOpenedInTab(): Promise<unknown> {
+
+  async canBeOpenedInTab(): Promise<unknown> {
     throw new Error("Method not implemented.");
   }
   getName(): string {
