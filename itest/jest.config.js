33bf403c7400f2582e28a1eec9c1308d855ef48b
Mark Michaelis <mark.michaelis@coremedia.com>
2022-05-19 11:48:47 +0200

    fix(Playwright): Timeout of 30 s may not be enough
    One some tests I saw, that they may take close to 30 seconds. It is
    better to provide some more grace.

--- itest/jest.config.js
+++ itest/jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
   // The default timeout is 5000. This may be not enough for Jest Playwright
   // tests. If the test fails due to test-timeout, we will only get unspecific
   // failures `Exceeded timeout`.
-  testTimeout: 30000,
+  testTimeout: 60000,
   preset: "jest-playwright-preset",
   testEnvironment: "./playwright.environment.js",
   setupFilesAfterEnv: ["expect-playwright"],
