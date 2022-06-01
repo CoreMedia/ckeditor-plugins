a7311e202c6142c42b2b40c169b94a5fd614fe2e
Mark Michaelis <mark.michaelis@coremedia.com>
2022-06-01 09:47:47 +0200

    test(itest): Fix Jest Config (Roots)
    We should not run tests from `dist/` folder, thus, explicitly setting
    it to `src` folder. Previously, tests were executed twice: Once as
    TypeScript from `src/` then as JavaScript from `dist/`. It may be
    caused by not using the default setup adding tests to `__tests__`
    folder.

--- itest/jest.config.js
+++ itest/jest.config.js
@@ -13,6 +13,7 @@ const enhancedResolveJest27 = {
 
 module.exports = {
   ...jestConfig,
+  roots: ["<rootDir>/src/"],
   // The default timeout is 5000. This may be not enough for Jest Playwright
   // tests. If the test fails due to test-timeout, we will only get unspecific
   // failures `Exceeded timeout`.
