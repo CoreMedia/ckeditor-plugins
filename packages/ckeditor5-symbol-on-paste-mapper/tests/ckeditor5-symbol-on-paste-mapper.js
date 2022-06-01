18db7246eb24e5ff87461e6c43dbec070fcad039
Mark Michaelis <mark.michaelis@coremedia.com>
2022-06-01 16:46:33 +0200

    chore: Fix Issues after Main-Merge
    * Removing obsolete dependencies in symbol-on-paste
    
      We don't use the CKEditor Integration Tests. Removing
      Files and corresponding dependencies.
    
    * We experience some issues with Jest after Main
      Merge. First steps trying to address these issues.

--- packages/ckeditor5-symbol-on-paste-mapper/tests/ckeditor5-symbol-on-paste-mapper.js
+++
@@ -1,8 +0,0 @@
-import SymbolOnPasteMapper from '../dist/SymbolOnPasteMapper';
-import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
-
-describe( 'SymbolOnPasteMapper Plugin', () => {
-  it( 'requires Clipboard from @ckeditor', () => {
-    expect( SymbolOnPasteMapper.requires ).to.deep.equal( [ Clipboard ] );
-  } );
-} );
