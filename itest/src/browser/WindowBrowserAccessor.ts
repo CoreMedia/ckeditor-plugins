9a9a1aa59def04641a1f0338a22bdd898119acfe
jdallman <Jens.Dallmann@coremedia.com>
2022-06-23 09:54:12 +0200

    doc(font-mapping): Provide documentation

--- itest/src/browser/WindowBrowserAccessor.ts
+++ itest/src/browser/WindowBrowserAccessor.ts
@@ -1,4 +1,10 @@
+/**
+ * Accesses the window object inside the currently running browser instance.
+ */
 export default class WindowBrowserAccessor {
+  /**
+   * Gets the <i>userAgent</i> from the browser.
+   */
   static async getUserAgent(): Promise<string> {
     return page.evaluate(() => {
       return window.navigator.userAgent;
