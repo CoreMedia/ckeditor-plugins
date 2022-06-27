64551534fc6dcac6e711f3ee1c4243dbc5e42313
Mark Michaelis <mark.michaelis@coremedia.com>
2022-06-27 08:44:20 +0200

    test(typings): Provide Use-Case Test for isRaw
    Demonstrates the benefit of this method and provides
    a rough idea how to use it, for example in Type Guards.

--- packages/ckeditor5-common/__tests__/AdvancedTypes.test.ts
+++ packages/ckeditor5-common/__tests__/AdvancedTypes.test.ts
@@ -2,6 +2,30 @@ import { isRaw } from "../src/AdvancedTypes";
 
 describe("AdvancedTypes", () => {
   describe("isRaw", () => {
+    it("Demonstrate Use Case", () => {
+      const value = "some value";
+      const obj: unknown = { value };
+
+      let actual1: string | undefined = undefined;
+      let actual2: string | undefined = undefined;
+
+      if (typeof obj === "object" && !!obj) {
+        if ("value" in obj) {
+          // @ts-expect-error Due to microsoft/TypeScript#21732 this access will
+          // be marked as error. If this shows an error, the issue seems to be
+          // fixed, and the workaround can be removed.
+          actual1 = obj.value;
+        }
+      }
+      if (isRaw(obj, "value")) {
+        // This would fail without isRaw having microsoft/TypeScript#21732
+        if (typeof obj.value === "string") {
+          actual2 = obj.value;
+        }
+      }
+      expect(actual1).toStrictEqual(actual2);
+    });
+
     it.each`
       value
       ${"string"}
