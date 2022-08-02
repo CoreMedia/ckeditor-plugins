ed4da5efe9bddca0d136daff3d0afa0d0dbe90df
Mark Michaelis <mark.michaelis@coremedia.com>
2022-08-02 09:00:36 +0200

    feat(differencing): Simplify Element/Attribute Registration
    We decided, to propagate differences also back to data view again,
    just as we did for CKEditor 4. It will be the responsibility of
    the underlying architecture to prevent such augmented data to
    be written back to server.

--- packages/ckeditor5-coremedia-differencing/src/Differencing.ts
+++ packages/ckeditor5-coremedia-differencing/src/Differencing.ts
@@ -2,15 +2,57 @@ import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import { ImageElementSupport } from "./integrations/Image";
 import { HtmlImageElementSupport } from "./integrations/HtmlSupportImage";
 
+/**
+ * Attributes used by server-side differencing.
+ *
+ * * **Key:** Name of attributes in data and editing view.
+ * * **Value:** Name of attribute in model.
+ */
 export const XDIFF_ATTRIBUTES = {
-  "xdiff:changetype": "changeType",
-  "xdiff:changes": "changes",
-  "xdiff:class": "class",
-  "xdiff:id": "id",
-  "xdiff:next": "next",
-  "xdiff:previous": "previous",
+  /**
+   * Attribute, applied to images for example.
+   */
+  "xdiff:changetype": "xdiff-change-type",
+  /**
+   * Verbose description of changes. Unused yet.
+   */
+  "xdiff:changes": "xdiff-changes",
+  /**
+   * UI-Class that categorizes the change type.
+   */
+  "xdiff:class": "xdiff-class",
+  /**
+   * ID of the difference, which may be used, to jump between
+   * changes. Unused yet.
+   */
+  "xdiff:id": "xdiff-id",
+  /**
+   * Reference to next diff for diff-navigation. Unused yet.
+   */
+  "xdiff:next": "xdiff-next",
+  /**
+   * Reference to previous diff for diff-navigation. Unused yet.
+   */
+  "xdiff:previous": "xdiff-previous",
 };
 
+/**
+ * Extra element, server-side differencing augments the original data with.
+ */
+export const XDIFF_SPAN_ELEMENT_CONFIG: {
+  view: string;
+  model: string;
+} = {
+  view: "xdiff:span",
+  model: "xdiff-span",
+};
+
+/**
+ * Plugin, which adds support for server-side data augmentation for
+ * showing differences in data. This plugin is meant to be used in
+ * read-only CKEditors that receive data from the server, which got augmented
+ * by differencing hints.
+ */
 export class Differencing extends Plugin {
   static readonly pluginName: string = "Differencing";
   static readonly requires = [HtmlImageElementSupport, ImageElementSupport];
@@ -27,33 +69,17 @@ export class Differencing extends Plugin {
       isInline: true,
     });
 
-    conversion.for("upcast").elementToElement({
-      view: "xdiff:span",
-      model: `xdiff-span`,
-    });
-    conversion.for("editingDowncast").elementToElement({
-      view: "xdiff:span",
-      model: `xdiff-span`,
-    });
-    // TODO: Preferred to remove instead. If not possible, merge with editingDowncat
-    conversion.for("dataDowncast").elementToElement({
-      view: "xdiff:span",
-      model: `xdiff-span`,
-    });
+    conversion.for("upcast").elementToElement(XDIFF_SPAN_ELEMENT_CONFIG);
+    // dataDowncast: We also downcast back to data view, relying on other mechanisms
+    // (like data-processing), that these changes are not written back to the
+    // server, as these don't represent "real" data but augmented data, which are
+    // not meant to be stored on server again.
+    conversion.for("downcast").elementToElement(XDIFF_SPAN_ELEMENT_CONFIG);
+
     Object.entries(XDIFF_ATTRIBUTES).forEach(([view, model]) => {
-      conversion.for("upcast").attributeToAttribute({
-        view,
-        model,
-      });
-      conversion.for("editingDowncast").attributeToAttribute({
-        view,
-        model,
-      });
-      // TODO: Preferred to remove instead. If not possible, merge with editingDowncat
-      conversion.for("dataDowncast").attributeToAttribute({
-        view,
-        model,
-      });
+      const config = { view, model };
+      conversion.for("upcast").attributeToAttribute(config);
+      conversion.for("downcast").attributeToAttribute(config);
     });
   }
 }
