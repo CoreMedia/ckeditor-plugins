3ec5415f3075c9ba16c483a91fdb8b30f504b830
Mark Michaelis <mark.michaelis@coremedia.com>
2022-08-02 10:00:13 +0200

    feat(differencing): Provide Plugin Integration Hook
    For integrating into optionally available plugins, we need to wait
    for their schema elements to be registered, prior to extending
    these registrations.
    
    Added a similar approach as for GHS, to workaround any other
    better approach, as suggested in ckeditor/ckeditor5#12199.

--- packages/ckeditor5-coremedia-differencing/src/integrations/Image.ts
+++ packages/ckeditor5-coremedia-differencing/src/integrations/Image.ts
@@ -1,28 +1,53 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import { PluginIntegrationHook } from "../PluginIntegrationHook";
+import Model from "@ckeditor/ckeditor5-engine/src/model/model";
+import Logger from "@coremedia/ckeditor5-logging/logging/Logger";
+import LoggerProvider from "@coremedia/ckeditor5-logging/logging/LoggerProvider";
 
+/**
+ * Hooks into `ImageInline` and `ImageBlock` plugin, if available and
+ * registered additional differencing attributes applied to `<img>` elements.
+ */
 export class ImageElementSupport extends Plugin {
   static readonly pluginName: string = "DifferencingImageElementSupport";
-  static readonly requires = [];
+  static readonly requires = [PluginIntegrationHook];
+
+  static readonly #logger: Logger = LoggerProvider.getLogger(ImageElementSupport.pluginName);
 
   init(): void {
     const { editor } = this;
-    const { model } = editor;
-    const { schema } = model;
+    const { model, plugins } = editor;
+    const { schema }: Model = model;
+    const logger = ImageElementSupport.#logger;
 
     // At least one image plugin should be loaded for the integration to work properly.
     if (!editor.plugins.has("ImageInlineEditing") && !editor.plugins.has("ImageBlockEditing")) {
+      logger.debug("Skipping Image Element Integration. Corresponding plugins unavailable.");
       return;
     }
-    if (schema.isRegistered("imageBlock")) {
-      schema.extend("imageBlock", {
-        allowAttributes: ["changeType"],
-      });
-    }
 
-    if (schema.isRegistered("imageInline")) {
-      schema.extend("imageInline", {
-        allowAttributes: ["changeType"],
-      });
-    }
+    const pluginIntegrationHook = plugins.get(PluginIntegrationHook);
+
+    logger.debug("Waiting for plugin-integration hook to be ready.");
+
+    pluginIntegrationHook.on("plugin-integration:ready", () => {
+      if (schema.isRegistered("imageBlock")) {
+        logger.debug(`Extending "imageBlock" by difference augmentation attribute.`);
+        schema.extend("imageBlock", {
+          allowAttributes: ["changeType"],
+        });
+      } else {
+        logger.debug(`Not integrating with "imageBlock" element, as it is unavailable.`);
+      }
+
+      if (schema.isRegistered("imageInline")) {
+        logger.debug(`Extending "imageInline" by difference augmentation attribute.`);
+        schema.extend("imageInline", {
+          allowAttributes: ["changeType"],
+        });
+      } else {
+        logger.debug(`Not integrating with "imageBlock" element, as it is unavailable.`);
+      }
+    });
   }
 }
