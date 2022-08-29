fa7efbae85c31848389c79f866ff0232fafdb226
Mark Michaelis <mark.michaelis@coremedia.com>
2022-08-29 13:26:10 +0200

    fix(playwright): No DOMParser available
    For Playwright tests, we don't have jsdom at hand, and thus, we
    cannot create a DOMParser instance.
    
    Moved corresponding code to extra file `RichTextDOM` which can
    now be used by tests or for example-data creation where JSDOM
    is available.

---
+++ packages/ckeditor5-coremedia-example-data/src/RichTextDOM.ts
@@ -0,0 +1,13 @@
+import { Content, richtext } from "./RichTextBase";
+
+const domParser = new DOMParser();
+
+export const parseRichTextFromString = (
+  innerXml: Content = "",
+  enforcedNamespaces: ReadonlyArray<string> = []
+): Document => {
+  const xml = richtext(innerXml, true, enforcedNamespaces);
+  return domParser.parseFromString(xml, "text/xml");
+};
+
+export const richTextDocument = parseRichTextFromString("", ["xlink"]);
