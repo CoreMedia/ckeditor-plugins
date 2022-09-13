9119e9e023478a7beb5489a9e21d0a0efec75db4
Mark Michaelis <mark.michaelis@coremedia.com>
2022-09-13 13:16:20 +0200

    docs(coremedia-richtext-support): Adjust Documentation

--- packages/ckeditor5-coremedia-richtext-support/src/GeneralRichTextSupport.ts
+++ packages/ckeditor5-coremedia-richtext-support/src/GeneralRichTextSupport.ts
@@ -1,9 +1,7 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
-import Logger from "@coremedia/ckeditor5-logging/logging/Logger";
-import LoggerProvider from "@coremedia/ckeditor5-logging/logging/LoggerProvider";
-import Editor from "@ckeditor/ckeditor5-core/src/editor/editor";
 import GeneralHtmlSupport from "@ckeditor/ckeditor5-html-support/src/generalhtmlsupport";
 import RichTextDataFilter from "./RichTextDataFilter";
+import { reportInitEnd, reportInitStart } from "@coremedia/ckeditor5-core-common/Plugins";
 
 /**
  * Wrapper plugin to enable GeneralRichTextSupport based on CKEditor's
@@ -11,18 +9,12 @@ import RichTextDataFilter from "./RichTextDataFilter";
  */
 class GeneralRichTextSupport extends Plugin {
   static readonly pluginName: string = "GeneralRichTextSupport";
-  static readonly #logger: Logger = LoggerProvider.getLogger(GeneralRichTextSupport.pluginName);
 
   static readonly requires = [GeneralHtmlSupport, RichTextDataFilter];
 
-  constructor(editor: Editor) {
-    super(editor);
-  }
-
   init(): Promise<void> | void {
-    const logger = GeneralRichTextSupport.#logger;
-
-    logger.info(`Initialized ${GeneralRichTextSupport.pluginName}.`);
+    const initInformation = reportInitStart(this);
+    reportInitEnd(initInformation);
   }
 }
 
