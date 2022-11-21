81d3279f1ebbaa6d1fb5c21dd2460a17eed6b5e1
Mark Michaelis <mark.michaelis@coremedia.com>
2022-11-21 06:49:50 +0100

    refactor(dom-converter): Prepare Package Structure

---
+++ packages/ckeditor5-dom-converter/src/index-doc.ts
@@ -0,0 +1,11 @@
+/**
+ * This module is meant as support for developing custom data-processors for
+ * CKEditor 5. Its task is converting the data view to corresponding data,
+ * assuming that the data view is provided as HTML source as it is by default
+ * in CKEditor 5.
+ *
+ * The overall concept is inspired by the `DOMConverter` as it is available in
+ * CKEditor 5 for upcast and downcast between model and view layers.
+ *
+ * @module ckeditor5-dom-converter
+ */
