df8eaf36259f4368790f02944633b8281c86c24d
Mark Michaelis <mark.michaelis@coremedia.com>
2023-01-02 14:41:06 +0100

    refactor(dom-support): Extend DocumentFragments and provide Tests
    From previous PoC we know, that we will need to deal with
    DocumentFragments. Adding corresponding type-guard and a
    utility method, which is at least helpful for testing.

--- packages/ckeditor5-dom-support/src/DocumentFragments.ts
+++ packages/ckeditor5-dom-support/src/DocumentFragments.ts
@@ -4,3 +4,15 @@
  * @param value - value to guard
  */
 export const isDocumentFragment = (value: unknown): value is DocumentFragment => value instanceof DocumentFragment;
+
+/**
+ * Extracts `DocumentFragment` from contents of given node.
+ *
+ * @param value - node to extract `DocumentFragment` from
+ */
+export const fragmentFromNodeContents = (value: Node): DocumentFragment => {
+  const { ownerDocument } = value;
+  const range = ownerDocument?.createRange() ?? new Range();
+  range.selectNodeContents(value);
+  return range.extractContents();
+};
