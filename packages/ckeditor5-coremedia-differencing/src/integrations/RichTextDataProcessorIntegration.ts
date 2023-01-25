19c237788ea816b3bf8465810cfcd126f4bd327a
Mark Michaelis <mark.michaelis@coremedia.com>
2023-01-25 14:44:19 +0100

    docs(data-processor): Document `afterInit`
    As Differencing is one of our live examples, how to adapt
    data-processing from plugins, we add some more documentation
    to the `afterInit` implementation. It clarifies, that such
    adaptations must be done in earlier phases.

--- packages/ckeditor5-coremedia-differencing/src/integrations/RichTextDataProcessorIntegration.ts
+++ packages/ckeditor5-coremedia-differencing/src/integrations/RichTextDataProcessorIntegration.ts
@@ -9,6 +9,17 @@ import { xDiffElements } from "./XDiffElements";
 export class RichTextDataProcessorIntegration extends Plugin {
   static readonly pluginName: string = "RichTextDataProcessorIntegration";
 
+  /**
+   * Runs as late initialization to add mapping rules for `xdiff:span`
+   * elements. The corresponding rules mainly take care, that no such data
+   * are written back to server. Along with that, maps `<xdiff:span/>` (thus,
+   * empty span) to `<xdiff:br/>`, as this provides more robustness detecting
+   * added or removed line-breaks via CSS selectors in editing view for proper
+   * highlighting of differences.
+   *
+   * Note, that it is important to run this later than `init` phase, as
+   * the data-processor itself is applied in `init` phase of plugins.
+   */
   afterInit(): void {
     const { editor } = this;
     const { processor } = editor.data;
