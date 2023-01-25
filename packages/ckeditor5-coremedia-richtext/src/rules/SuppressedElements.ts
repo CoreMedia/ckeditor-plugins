872885d740dd22d962203252225c59ac270c97d8
Mark Michaelis <mark.michaelis@coremedia.com>
2023-01-25 10:07:55 +0100

    test(data-processor): Testing SuppressedElements
    Along with tests, provided more documentation and ensured, that
    filter is only applied for `toData` direction, as `toView` mapping
    is irrelevant.

--- packages/ckeditor5-coremedia-richtext/src/rules/SuppressedElements.ts
+++ packages/ckeditor5-coremedia-richtext/src/rules/SuppressedElements.ts
@@ -3,7 +3,22 @@ import { replaceByChildren } from "./ReplaceByChildren";
 /**
  * CKEditor surrounds tables, for example, by `<figure>` elements. We want to
  * remove them to ensure, that we get no complaints on sanitation.
+ *
+ * Note, that this approach ignores any properties possibly set at figure.
+ * To deal with these elements, you either have to apply some early processing
+ * or remove this rule.
+ *
+ * @example For table from data view
+ * ```html
+ * <figure class="table"><table><tbody><tr><td>&nbsp;</td></tr></tbody></table></figure>
+ * ```
  */
-export const suppressedFigure = replaceByChildren({ localName: "figure" });
+export const suppressedFigure = replaceByChildren({ localName: "figure", direction: "toData" });
 
+/**
+ * Rules for removing elements, we want to ignore in data. While this is
+ * also done as part of sanitation for _unknown CoreMedia Rich Text 1.0
+ * elements_, these extra rules are meant to prevent possible warnings
+ * triggered during sanitation process.
+ */
 export const suppressedElements = [suppressedFigure];
