c549e3f844764f81ec87467cba57e9218c86a67e
jdallman <Jens.Dallmann@coremedia.com>
2023-01-26 15:17:37 +0100

    test(data-processor): Adapt order, as the data processor now keeps the order like it is in the input

--- itest/src/DragDrop.test.ts
+++ itest/src/DragDrop.test.ts
@@ -362,7 +362,7 @@ describe("Drag and Drop", () => {
           const data = await application.editor.getData();
           for (const contentMock of contentMocks) {
             // noinspection HtmlUnknownAttribute
-            await expect(data).toContain(`<img alt="" xlink:href="content/${contentMock.id}#properties.data"/>`);
+            await expect(data).toContain(`<img xlink:href="content/${contentMock.id}#properties.data" alt=""/>`);
           }
         });
 
