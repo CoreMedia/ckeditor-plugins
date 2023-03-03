c23810afd9442be166a74718424a94625044f178
Mark Michaelis <mark.michaelis@coremedia.com>
2023-03-03 13:12:15 +0100

    docs(link): LinkAttributesConfig

--- packages/ckeditor5-link-common/src/LinkAttributesConfig.ts
+++ packages/ckeditor5-link-common/src/LinkAttributesConfig.ts
@@ -1,14 +1,51 @@
 import { isRegisterAttributeConfig, RegisterAttributeConfig } from "./RegisterAttributeConfig";
 import Config from "@ckeditor/ckeditor5-utils/src/config";
 
+/**
+ * Configuration, that is expected as part of the CKEditor 5
+ * link-feature configuration.
+ *
+ * ```typescript
+ * const linkAttributesConfig: LinkAttributesConfig = {
+ *   attributes: [
+ *     { view: "title", model: "linkTitle" },
+ *     { view: "data-xlink-actuate", model: "linkActuate" },
+ *   ],
+ * };
+ *
+ * ClassicEditor.create(sourceElement, {
+ *   plugins: [
+ *     LinkAttributes,
+ *     Link,
+ *     // ...
+ *   ],
+ *   link: {
+ *     defaultProtocol: "https://",
+ *     ...linkAttributesConfig,
+ *   },
+ * };
+ * ```
+ */
 export interface LinkAttributesConfig {
+  /**
+   * Configuration of attributes, which are to be handled as being
+   * part of a link, similar to `href` (`linkHref` in model).
+   */
   attributes: RegisterAttributeConfig[];
 }
 
+/**
+ * Provides an empty configuration by default.
+ */
 const emptyConfig = (): LinkAttributesConfig => ({
   attributes: [],
 });
 
+/**
+ * Configuration parsing for link attributes.
+ *
+ * @param config editor configuration to parse
+ */
 export const parseAttributesConfig = (config: Config): LinkAttributesConfig => {
   const fromConfig: unknown = config.get("link.attributes");
   if (!fromConfig) {
