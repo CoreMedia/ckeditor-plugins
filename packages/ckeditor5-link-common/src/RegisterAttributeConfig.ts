cd7337c05c91345ec6a6203852107f9b1d3d4692
Mark Michaelis <mark.michaelis@coremedia.com>
2023-03-03 13:14:51 +0100

    docs(link): RegisterAttributeConfig

--- packages/ckeditor5-link-common/src/RegisterAttributeConfig.ts
+++ packages/ckeditor5-link-common/src/RegisterAttributeConfig.ts
@@ -15,12 +15,20 @@ export interface RegisterAttributeConfig {
    */
   model: LinkAttributeName;
   /**
-   * Name of attribute in view (assumed to be identical in editing and
-   * data view.
+   * Name of attribute in view.
+   *
+   * Assumed to be identical in editing and data view.
    */
   view: string;
 }
 
+/**
+ * Validate if value contains required attributes.
+ *
+ * @param value - value to guard
+ * @param value.model - attribute name in model
+ * @param value.view - attribute name in view (data and editing view)
+ */
 const hasModelAndView = (value: { model?: unknown; view?: unknown }): value is RegisterAttributeConfig => {
   const { model, view } = value;
 
@@ -32,6 +40,11 @@ const hasModelAndView = (value: { model?: unknown; view?: unknown }): value is R
   return false;
 };
 
+/**
+ * Type guard for `RegisterAttributeConfig`.
+ *
+ * @param value - value to guard
+ */
 export const isRegisterAttributeConfig = (value: unknown): value is RegisterAttributeConfig => {
   if (!value || typeof value !== "object") {
     return false;
