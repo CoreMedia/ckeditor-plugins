5ad0e87cc9a1fc82e1779bf1f2bc65b674e50508
Mark Michaelis <mark.michaelis@coremedia.com>
2023-04-12 16:36:10 +0200

    fix(third-party): Fix Error Prototype Chain

--- packages/ckeditor5-coremedia-link/src/contentlink/LazyLinkUIPropertiesNotInitializedYetError.ts
+++ packages/ckeditor5-coremedia-link/src/contentlink/LazyLinkUIPropertiesNotInitializedYetError.ts
@@ -1,5 +1,7 @@
 export class LazyLinkUIPropertiesNotInitializedYetError extends Error {
   constructor() {
     super("Lazy LinkUI properties (LinkUI.formView and LinkUI.actionsView) are not initialized yet.");
+    // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
