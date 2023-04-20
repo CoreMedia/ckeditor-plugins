ac13367a028995ad25e1e883e98a8f7a0c4fbde4
Mark Michaelis <mark.michaelis@coremedia.com>
2023-04-20 11:35:45 +0200

    refactor(dom-support): Introduce `index.ts`

---
+++ packages/ckeditor5-dom-support/src/index.ts
@@ -0,0 +1,48 @@
+/**
+ * @module ckeditor5-dom-support
+ */
+
+export { copyAttributesFrom } from "./Attrs";
+export { fragmentFromNodeContents, fragmentToString } from "./DocumentFragments";
+export { documentFromHtml, documentFromXml } from "./Documents";
+export {
+  type ElementDefinition,
+  type ElementDefinitionType,
+  compileElementDefinition,
+  createElement,
+  renameElement,
+  removeClass,
+} from "./Elements";
+export {
+  wrapIfHTMLElement,
+  defaultDataAttributeClassNamePrefix,
+  defaultDataAttributeElementName,
+  type HTMLElementWrapper,
+} from "./HTMLElements";
+export { wrapIfTableElement, type HTMLTableElementWrapper } from "./HTMLTableElements";
+export { registerNamespacePrefixes, xmlnsNamespaceUri } from "./Namespaces";
+export {
+  appendNodeContents,
+  extractNodeContents,
+  lookupDocumentDefaultNamespaceURI,
+  lookupNamespaceURI,
+  prependNodeContents,
+  serializeToXmlString,
+} from "./Nodes";
+export { querySelectorAllDirectChildren, querySelectorDirectChild } from "./ParentNodes";
+export {
+  isAttr,
+  isCharacterData,
+  isComment,
+  isDocument,
+  isDocumentFragment,
+  isElement,
+  isHTMLAnchorElement,
+  isHTMLElement,
+  isHTMLImageElement,
+  isHTMLTableElement,
+  isHasNamespaceUri,
+  isParentNode,
+  isText,
+} from "./TypeGuards";
+export { type HasChildren, type HasNamespaceUri } from "./Types";
