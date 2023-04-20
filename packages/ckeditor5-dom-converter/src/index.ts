965672922c77465fd69eee0a6ca073e0d104157f
Mark Michaelis <mark.michaelis@coremedia.com>
2023-04-20 12:13:04 +0200

    refactor(dom-converter): Introduce `index.ts`

---
+++ packages/ckeditor5-dom-converter/src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * @module ckeditor5-dom-converter
+ */
+
+export type { ConversionApi } from "./ConversionApi";
+export type { ConversionContext } from "./ConversionContext";
+export type { ConversionListener } from "./ConversionListener";
+export type {
+  AppendedFunction,
+  ImportedFunction,
+  ImportedWithChildrenFunction,
+  PrepareFunction,
+} from "./DomConverterStages";
+export { HtmlDomConverter } from "./HtmlDomConverter";
+export type { RuleConfig, RuleSectionConfig } from "./Rule";
+export { RuleBasedConversionListener } from "./RuleBasedConversionListener";
+export { skip, type Skip } from "./Signals";
