7c082247967c0b1464b84742bf40a92e76a12d3a
Mark Michaelis <mark.michaelis@coremedia.com>
2023-04-27 15:55:22 +0200

    refactor(logging): Fix ESLint Config
    Need to update reference to tsconfig.json.

--- packages/ckeditor5-logging/.eslintrc.js
+++ packages/ckeditor5-logging/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
       files: ["**/*.ts", "**/*.tsx"],
       parserOptions: {
         tsconfigRootDir: __dirname,
-        project: ["./__tests__/tsconfig.json", "./src/tsconfig.json"],
+        project: ["./tsconfig.json"],
       },
     },
   ],
