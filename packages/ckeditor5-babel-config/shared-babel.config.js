364cefcb66fc3e081d9e409f150c81c6e605df08
Mark Michaelis <mark.michaelis@coremedia.com>
2023-05-02 12:36:25 +0200

    chore(babel): Add Missing Babel Plugins

--- packages/ckeditor5-babel-config/shared-babel.config.js
+++ packages/ckeditor5-babel-config/shared-babel.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   plugins: [
-    [require.resolve("@babel/plugin-transform-typescript"), { allowDeclareFields: true, allowNamespaces: true }],
-    [require.resolve("@babel/plugin-transform-parameters"), { loose: true }],
-    [require.resolve("@babel/plugin-transform-computed-properties"), { loose: true }],
-    [require.resolve("@babel/plugin-transform-shorthand-properties"), { loose: true }],
-    [require.resolve("@babel/plugin-transform-arrow-functions"), { spec: false }],
-    [require.resolve("@babel/plugin-transform-for-of"), { assumeArray: true }],
     [require.resolve("@babel/plugin-proposal-class-properties"), {}],
+    [require.resolve("@babel/plugin-proposal-private-methods"), {}],
+    [require.resolve("@babel/plugin-transform-arrow-functions"), { spec: false }],
+    [require.resolve("@babel/plugin-transform-classes"), {}],
+    [require.resolve("@babel/plugin-transform-computed-properties"), { loose: true }],
+    [require.resolve("@babel/plugin-transform-for-of"), { assumeArray: true }],
+    [require.resolve("@babel/plugin-transform-parameters"), { loose: true }],
+    [require.resolve("@babel/plugin-transform-shorthand-properties"), { loose: true }],
+    [require.resolve("@babel/plugin-transform-typescript"), { allowDeclareFields: true, allowNamespaces: true }],
   ],
   presets: [
     [
