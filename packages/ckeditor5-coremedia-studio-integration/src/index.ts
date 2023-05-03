a69ff5aaf6695e30a0f24b44b8ab65b8b1c6c888
jdallman <Jens.Dallmann@coremedia.com>
2023-05-03 15:39:32 +0200

    fix(public-api): Use an index.ts for coremedia-studio-integration

---
+++ packages/ckeditor5-coremedia-studio-integration/src/index.ts
@@ -0,0 +1,9 @@
+export { default as BlobDisplayService } from "./content/BlobDisplayService";
+export { InlinePreview } from "./content/BlobDisplayService";
+export { createBlobDisplayServiceDescriptor } from "./content/BlobDisplayServiceDescriptor";
+export { UriPath } from "./content/UriPath";
+export { default as ContentAsLink } from "./content/ContentAsLink";
+export { default as ContentDisplayService } from "./content/ContentDisplayService";
+export { createContentDisplayServiceDescriptor } from "./content/ContentDisplayServiceDescriptor";
+export { default as RichtextConfigurationService } from "./content/RichtextConfigurationService";
+export { createRichtextConfigurationServiceDescriptor } from "./content/RichtextConfigurationServiceDescriptor";
