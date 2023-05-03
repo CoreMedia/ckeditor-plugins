35572b7ecc0787b78a69010d97ef869ec99f626d
jdallman <Jens.Dallmann@coremedia.com>
2023-05-03 16:50:42 +0200

    chore(public-api): Improve exports

--- packages/ckeditor5-coremedia-studio-integration/src/index.ts
+++ packages/ckeditor5-coremedia-studio-integration/src/index.ts
@@ -1,9 +1,13 @@
-export { default as BlobDisplayService } from "./content/BlobDisplayService";
-export { InlinePreview } from "./content/BlobDisplayService";
+// Services
+export { default as BlobDisplayService, InlinePreview } from "./content/BlobDisplayService";
 export { createBlobDisplayServiceDescriptor } from "./content/BlobDisplayServiceDescriptor";
-export { UriPath } from "./content/UriPath";
-export { default as ContentAsLink } from "./content/ContentAsLink";
+
 export { default as ContentDisplayService } from "./content/ContentDisplayService";
 export { createContentDisplayServiceDescriptor } from "./content/ContentDisplayServiceDescriptor";
+
 export { default as RichtextConfigurationService } from "./content/RichtextConfigurationService";
 export { createRichtextConfigurationServiceDescriptor } from "./content/RichtextConfigurationServiceDescriptor";
+
+// Helpers
+export { UriPath } from "./content/UriPath";
+export { default as ContentAsLink } from "./content/ContentAsLink";
