2f9f70476d59631784de5890f3b02735d1ddef3c
pkliesch <pkliesch@coremedia.com>
2023-06-22 11:00:48 +0200

    feature(blocklist): add missing typedoc entries

--- packages/ckeditor5-blocklist/src/index-doc.ts
+++ packages/ckeditor5-blocklist/src/index-doc.ts
@@ -7,3 +7,13 @@
 
 export * as ui from "./ui/index-doc";
 export * as lang from "./lang/index-doc";
+
+export { default as BlockList } from "./blocklist";
+export * from "./blocklistChangesUtils";
+
+export { default as BlocklistCommand } from "./blocklistCommand";
+export * from "./blocklistCommand";
+
+export { default as BlocklistEditing } from "./blocklistediting";
+
+export { default as Blocklistui } from "./blocklistui";
