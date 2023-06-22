21de09dcfcdb4551c5f3cdc98a0a8bf5edcc99c2
pkliesch <pkliesch@coremedia.com>
2023-06-22 10:46:04 +0200

    feature(blocklist): typedoc files added to package

---
+++ packages/ckeditor5-blocklist/src/index-doc.ts
@@ -0,0 +1,9 @@
+/**
+ * A plugin, which retrieves words from a Studio service, that are highlighted in the editor's
+ * content when present.
+ *
+ * @module ckeditor5-blocklist
+ */
+
+export * as ui from "./ui/index-doc";
+export * as lang from "./lang/index-doc";
---
+++ packages/ckeditor5-blocklist/src/lang/index-doc.ts
@@ -0,0 +1,8 @@
+/**
+ * Localizations.
+ *
+ * @packageDocumentation
+ * @category Virtual
+ */
+
+export * as blocklist from "./blocklist";
