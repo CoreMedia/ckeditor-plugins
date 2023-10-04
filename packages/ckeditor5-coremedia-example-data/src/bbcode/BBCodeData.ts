abe2eb4bb3f2f47fd99806566a45f732e3fc4c3f
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-04 08:41:40 +0200

    test: Security Challenge Data

--- packages/ckeditor5-coremedia-example-data/src/bbcode/BBCodeData.ts
+++ packages/ckeditor5-coremedia-example-data/src/bbcode/BBCodeData.ts
@@ -2,9 +2,11 @@ import { ExampleData } from "../ExampleData";
 import { inlineFormatData } from "./InlineFormatData";
 import { welcomeTextData } from "./WelcomeTextData";
 import { challengingData } from "./ChallengingData";
+import { securityChallengeData } from "./SecurityChallengeData";
 
 export const bbCodeData: ExampleData = {
   ...challengingData,
   ...inlineFormatData,
+  ...securityChallengeData,
   ...welcomeTextData,
 };
---
+++ packages/ckeditor5-coremedia-example-data/src/bbcode/SecurityChallengeData.ts
@@ -0,0 +1,67 @@
+import { ExampleData } from "../ExampleData";
+import { bbCode } from "./BBCode";
+
+const lines = (...texts: string[]): string => texts.join("");
+
+const htmlEntity = {
+  lsqb: {
+    raw: `[`,
+    dec: `&#91;`,
+    hex: `&#x5B;`,
+    named: `&lsqb;`,
+  },
+  rsqb: {
+    raw: `]`,
+    dec: `&#93;`,
+    hex: `&#x5D;`,
+    named: `&rsqb;`,
+  },
+  gt: {
+    raw: `>`,
+    dec: `&#62;`,
+    hex: `&#x3E;`,
+    named: `&gt;`,
+  },
+  lt: {
+    raw: `<`,
+    dec: `&#60;`,
+    hex: `&#x3C;`,
+    named: `&lt;`,
+  },
+};
+
+/**
+ * Same possible challenges to BBCode to HTML and vice versa processing.
+ *
+ * @see [XSS Filter Evasion – OWASP Cheat Sheet Series](https://cheatsheetseries.owasp.org/cheatsheets/XSS_Filter_Evasion_Cheat_Sheet.html)
+ */
+export const securityChallengeData: ExampleData = {
+  "Security: Inline HTML in BBCode": lines(
+    `${bbCode.h1("Security: Inline HTML in BBCode")}`,
+    `${bbCode.p(`Lorem <strong>ipsum</strong> dolor`)}`,
+  ),
+  "Security: Escaped Inline HTML in BBCode (Decimal)": lines(
+    `${bbCode.h1("Security: Escaped Inline HTML in BBCode (Decimal)")}`,
+    `${bbCode.p(`Lorem ${htmlEntity.lt.dec}strong${htmlEntity.gt.dec}ipsum${htmlEntity.lt.dec}/strong${htmlEntity.gt.dec} dolor`)}`,
+  ),
+  "Security: Escaped Inline HTML in BBCode (Hexadecimal)": lines(
+    `${bbCode.h1("Security: Escaped Inline HTML in BBCode (Hexadecimal)")}`,
+    `${bbCode.p(`Lorem ${htmlEntity.lt.hex}strong${htmlEntity.gt.hex}ipsum${htmlEntity.lt.hex}/strong${htmlEntity.gt.hex} dolor`)}`,
+  ),
+  "Security: Escaped Inline HTML in BBCode (Named)": lines(
+    `${bbCode.h1("Security: Escaped Inline HTML in BBCode (Named)")}`,
+    `${bbCode.p(`Lorem ${htmlEntity.lt.named}strong${htmlEntity.gt.named}ipsum${htmlEntity.lt.named}/strong${htmlEntity.gt.named} dolor`)}`,
+  ),
+  "Security: Square Bracket HTML Entity in BBCode (Decimal)": lines(
+    `${bbCode.h1("Security: Square Bracket HTML Entity in BBCode (Decimal)")}`,
+    `Lorem ${htmlEntity.lsqb.dec}b${htmlEntity.rsqb.dec}ipsum${htmlEntity.lsqb.dec}/b${htmlEntity.rsqb.dec} dolor`,
+  ),
+  "Security: Square Bracket HTML Entity in BBCode (Hexadecimal)": lines(
+    `${bbCode.h1("Security: Square Bracket HTML Entity in BBCode (Hexadecimal)")}`,
+    `Lorem ${htmlEntity.lsqb.hex}b${htmlEntity.rsqb.hex}ipsum${htmlEntity.lsqb.hex}/b${htmlEntity.rsqb.hex} dolor`,
+  ),
+  "Security: Square Bracket HTML Entity in BBCode (Named)": lines(
+    `${bbCode.h1("Security: Square Bracket HTML Entity in BBCode (Named)")}`,
+    `Lorem ${htmlEntity.lsqb.named}b${htmlEntity.rsqb.named}ipsum${htmlEntity.lsqb.named}/b${htmlEntity.rsqb.named} dolor`,
+  ),
+};
