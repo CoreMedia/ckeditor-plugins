b0b71a354fc8f17939370f6b377766d98c123d58
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-05 09:33:52 +0200

    feat: toData: Support language for `[code]`

--- packages/ckeditor5-coremedia-bbcode/src/rules/BBCodeCode.ts
+++ packages/ckeditor5-coremedia-bbcode/src/rules/BBCodeCode.ts
@@ -1,5 +1,7 @@
 import { BBCodeProcessingRule } from "./BBCodeProcessingRule";
 
+const escapeLanguage = (language: string): string => language.replace(/([\][])/g, "\\$1");
+
 /**
  * Maps `<pre>` to `[code]`.
  */
@@ -9,6 +11,13 @@ export class BBCodeCode implements BBCodeProcessingRule {
   toData(element: HTMLElement, content: string): undefined | string {
     if (!(element instanceof HTMLPreElement)) {
       return;
+    }
+    // CKEditor 5 encodes the language as `language` in dataset.
+    const {
+      dataset: { language },
+    } = element;
+    if (language) {
+      return `[code=${escapeLanguage(language)}]${content}[/code]\n`;
     }
     return `[code]${content}[/code]\n`;
   }
