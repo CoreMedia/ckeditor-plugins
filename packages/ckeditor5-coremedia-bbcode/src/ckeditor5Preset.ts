fcf72b600e41a1441fdb518932964a88a41842f3
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-09 15:21:03 +0200

    feat: Adapt Code-Mapping for CKEditor 5 Data View
    Replace "recursive mapping detection" by explicitly invoking
    `render` internally. And, while at it, added a behavior, that trims
    surrounding newlines, that may be a left-over from pretty-printing in
    HTML 2 BBCode processing.

--- packages/ckeditor5-coremedia-bbcode/src/ckeditor5Preset.ts
+++ packages/ckeditor5-coremedia-bbcode/src/ckeditor5Preset.ts
@@ -5,7 +5,6 @@ import html5Preset from "@bbob/preset-html5/es";
 type DefaultTags = Parameters<typeof createPreset>[0];
 type TagMappingFn = DefaultTags[string];
 type Core = Parameters<TagMappingFn>[1];
-type RenderFn = Core["render"];
 type Tag = TagNode["tag"];
 type Content = TagNode["content"];
 type Attrs = TagNode["attrs"];
@@ -16,6 +15,11 @@ const toNode = (tag: Tag, attrs: Attrs, content: Content): TagNode => ({
   content,
 });
 
+/**
+ * Extension of the HTML 5 Default Preset, that ships with BBob. It adapts
+ * the given presets, so that they align with the expectations by CKEditor 5
+ * regarding the representation in data view.
+ */
 export const ckeditor5Preset: typeof html5Preset = html5Preset.extend(
   (tags: DefaultTags): DefaultTags => ({
     ...tags,
@@ -23,14 +27,17 @@ export const ckeditor5Preset: typeof html5Preset = html5Preset.extend(
      * Adapted for CKEditor 5: CKEditor 5 requires a nested `<code>` element and
      * allows specifying the language via a corresponding class parameter.
      */
-    code: (node) => {
-      if ("class" in node.attrs) {
-        // Break recursion: Nothing to do, it is our node we created before
-        // by assigning a language attribute.
-        return node;
-      }
+    code: (node: TagNode, { render }: Core): TagNode => {
       const language = getUniqAttr(node.attrs) || "plaintext";
-      return toNode("pre", {}, [toNode("code", { class: `language-${language}` }, node.content)]);
+      const renderedCodeContent = render(node.content);
+      // Remove some surrounding space from possible pretty-print BBCode.
+      // If not doing so, you will see some extra lines above and below the
+      // actual code within the code-block.
+      const trimmedRenderedCodeContent = renderedCodeContent.trim();
+      // Must not nest mapping to code-Node, as this would trigger an endless
+      // recursion. Render the node directly instead.
+      const renderedCode = render(toNode("code", { class: `language-${language}` }, [trimmedRenderedCodeContent]));
+      return toNode("pre", {}, [renderedCode]);
     },
   }),
 );
