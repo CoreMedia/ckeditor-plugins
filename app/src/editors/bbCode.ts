c54ed2a814ea4bbebbb15b94dfddc42daa3b058f
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-10 10:59:37 +0200

    feat: Customize Language Tokens
    Add a demo for custom language tokens and add custom `bbcode`
    language to the configuration copied from the default config.

--- app/src/editors/bbCode.ts
+++ app/src/editors/bbCode.ts
@@ -91,6 +91,26 @@ export const createBBCodeEditor: CKEditorInstanceFactory = (
     autosave: {
       waitingTime: 1000, // in ms
     },
+    codeBlock: {
+      // Mostly defaults, despite new one: BBCode.
+      languages: [
+        { language: "plaintext", label: "Plain text" }, // The default language.
+        { language: "bbcode", label: "BBCode" },
+        { language: "c", label: "C" },
+        { language: "cs", label: "C#" },
+        { language: "cpp", label: "C++" },
+        { language: "css", label: "CSS" },
+        { language: "diff", label: "Diff" },
+        { language: "html", label: "HTML" },
+        { language: "java", label: "Java" },
+        { language: "javascript", label: "JavaScript" },
+        { language: "php", label: "PHP" },
+        { language: "python", label: "Python" },
+        { language: "ruby", label: "Ruby" },
+        { language: "typescript", label: "TypeScript" },
+        { language: "xml", label: "XML" },
+      ],
+    },
     dataFacade: {
       save(dataApi): Promise<void> {
         console.log("Save triggered...");
