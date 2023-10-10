6250e86a13571e4b3ba09d5930574cd6cf994389
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-10 11:01:45 +0200

    refactor: Defensive Language Matching (Code Block)
    Instead of trying to escape some weird matched language pattern using
    a defensive regular expression now. This feels much less error-prone,
    especially, as there are known issues with escaping not to work
    within attributes regarding BBCode parsing.

--- packages/ckeditor5-coremedia-bbcode/src/rules/BBCodeCode.ts
+++ packages/ckeditor5-coremedia-bbcode/src/rules/BBCodeCode.ts
@@ -1,65 +1,162 @@
 import { BBCodeProcessingRule } from "./BBCodeProcessingRule";
 import { removeLeadingAndTrailingNewlines } from "../BBCodeUtils";
 
-const escapeLanguage = (language: string): string => language.replace(/([\][])/g, "\\$1");
-const ckeditorCodeBlockLanguageClassPrefix = "language-";
+/**
+ * The default extracted language by `HTMLElement.classList` that is considered
+ * _plain text_, which, in return, will not show up as language token.
+ */
+export const defaultPlainTextToken = "plaintext";
 
 /**
- * Determines the language as it is set in CKEditor's data view for a given
- * code block. The language is attached as class to the nested `<code>`
- * element, like `language-css`. For plain text, the class added is
- * `language-plaintext`. Classes like this may be ignored by optional
- * parameter `forceUnset`.
+ * Used to extract a language from a given entry in `HTMLElement.classList`.
  *
- * Typical CKEditor Code Blocks:
+ * Note that matched language tokens must not contain reserved characters
+ * such as `[` or `]` that will break BBCode parsing.
+ *
+ * A falsy return value will skip any further language processing and
+ * especially will not add any language identifier to the resulting
+ * BBCode.
+ */
+export type LanguageByClass = (classListEntry: string) => string | undefined;
+
+/**
+ * Signals, if a given non-empty token extracted via `LanguageByClass` should be
+ * considered as _unset_ when generating the resulting BBCode.
+ */
+export type IsUnset = (languageToken: string) => boolean;
+
+/**
+ * Default language token pattern applied by CKEditor 5 Code Block Plugin,
+ * which is a class prefixed with `language-` and follows by some alphabetic
+ * (widened to alphanumeric, here) characters.
+ */
+const ckeditor5DefaultCodeBlockLanguagePattern = /^language-(\w+)$/;
+
+/**
+ * Default language detection suitable for the default CKEditor 5 Code
+ * Block configuration, using class tokens prefixed with `language-`.
+ *
+ * @param classListEntry - a single entry in the class list to match.
+ */
+export const defaultLanguageByClass: LanguageByClass = (classListEntry: string): string | undefined => {
+  if (!classListEntry) {
+    return;
+  }
+  const languageMatch = ckeditor5DefaultCodeBlockLanguagePattern.exec(classListEntry);
+  return languageMatch?.[1];
+};
+
+/**
+ * Default predicate to identify a given token as _unset language_ when
+ * generating BBCode. It considers `plaintext` as to-be-ignored language,
+ * as this is the default plain text token in CKEditor 5 Code Block
+ * configuration.
+ *
+ * @param languageToken - language token to validate
+ */
+export const defaultIsUnset: IsUnset = (languageToken: string): boolean => defaultPlainTextToken === languageToken;
+
+/**
+ * Configuration for `BBCodeCode` rule.
+ */
+export interface BBCodeCodeConfig {
+  fromClass?: LanguageByClass;
+  isUnset?: IsUnset;
+}
+
+/**
+ * Processing rule for transforming a code blocks represented in HTML
+ * to `[code]Text[/code]` in BBCode. The rule expects the representation of
+ * code blocks as it is the default for the CKEditor 5 Code Block plugin, thus,
+ * a structure like this:
  *
  * ```xml
  * <pre><code class="language-plaintext">TEXT</code></pre>
  * <pre><code class="language-css">CSS</code></pre>
  * ```
- *
- * @param element - element to try to determine language for
- * @param forceUnset - languages that enforce `undefined` as return value
- * @returns the detected language or `undefined` if it cannot be determined or
- * is enforced to signal _unset_.
- */
-const determineLanguage = (element: HTMLPreElement, forceUnset = ["plaintext"]): string | undefined => {
-  const { firstElementChild } = element;
-  if (!(firstElementChild instanceof HTMLElement)) {
-    return;
-  }
-
-  const languageCls = Array.from(firstElementChild.classList).find((cls) =>
-    cls.startsWith(ckeditorCodeBlockLanguageClassPrefix),
-  );
-  if (!languageCls) {
-    return;
-  }
-
-  const language = languageCls.replace(ckeditorCodeBlockLanguageClassPrefix, "");
-  if (!language || forceUnset.includes(language)) {
-    return;
-  }
-  return language;
-};
-
-/**
- * Maps `<pre>` to `[code]`.
  */
 export class BBCodeCode implements BBCodeProcessingRule {
   readonly id = "code";
   readonly tags = ["code"];
+  readonly config: Required<BBCodeCodeConfig>;
+
+  constructor(config: BBCodeCodeConfig = {}) {
+    const { fromClass = defaultLanguageByClass, isUnset = defaultIsUnset } = config;
+    this.config = {
+      fromClass,
+      isUnset,
+    };
+  }
+
+  /**
+   * Find the first token that denotes a language and returns its matched part.
+   *
+   * @param tokens - class list tokens to match
+   */
+  #findFirstLangauge(tokens: string[]):
+    | {
+        language: string;
+        languageCls: string;
+      }
+    | undefined {
+    const mappingPredicate = this.config.fromClass;
+    for (const token of tokens) {
+      const found = mappingPredicate(token);
+      if (found) {
+        return {
+          language: found,
+          languageCls: token,
+        };
+      }
+    }
+  }
+
+  /**
+   * Determines the language as it is set in CKEditor's data view for a given
+   * code block. The language is attached as class to the nested `<code>`
+   * element, like `language-css`. For plain text, the class added is
+   * `language-plaintext`. Classes like this may be ignored by optional
+   * parameter `forceUnset`.
+   *
+   * @param element - element to try to determine language for
+   */
+  #determineLanguage(element: HTMLPreElement): string | undefined {
+    const { firstElementChild } = element;
+    if (!(firstElementChild instanceof HTMLElement)) {
+      return;
+    }
+    const languageInformation = this.#findFirstLangauge(Array.from(firstElementChild.classList));
+    if (languageInformation) {
+      const { language, languageCls } = languageInformation;
+      // Mark as "consumed".
+      firstElementChild.classList.remove(languageCls);
+      if (!this.config.isUnset(language)) {
+        return language;
+      }
+    }
+  }
+
+  #startTag(element: HTMLPreElement): "[code]" | `[code=${string}]` {
+    const language = this.#determineLanguage(element);
+    if (language) {
+      return `[code=${language}]`;
+    }
+    return "[code]";
+  }
+
   toData(element: HTMLElement, content: string): undefined | string {
     if (!(element instanceof HTMLPreElement)) {
       return;
     }
-    const language = determineLanguage(element);
-    const trimmed = removeLeadingAndTrailingNewlines(content);
-    if (language) {
-      return `[code=${escapeLanguage(language)}]\n${trimmed}\n[/code]\n`;
-    }
-    return `[code]\n${trimmed}\n[/code]\n`;
+    const startTag = this.#startTag(element);
+    const endTag = "[/code]";
+    const trimmedContent = removeLeadingAndTrailingNewlines(content);
+    return `${startTag}\n${trimmedContent}\n${endTag}\n`;
   }
 }
 
+/**
+ * Processing rule instance for transforming a code blocks represented in HTML
+ * to `[code]Text[/code]` in BBCode.
+ */
 export const bbCodeCode = new BBCodeCode();
