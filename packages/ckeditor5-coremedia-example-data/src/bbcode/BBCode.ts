11b9b708723fdccdd18d9d7870e8f80c5cf8797b
Jens Dallmann <jens.dallmann@coremedia.com>
2023-10-11 10:03:53 +0200

    chore(bbcode-dataprocessor): Apply prettier suggestion

--- packages/ckeditor5-coremedia-example-data/src/bbcode/BBCode.ts
+++ packages/ckeditor5-coremedia-example-data/src/bbcode/BBCode.ts
@@ -42,7 +42,8 @@ export const bbCode = {
   img: (url: string) => `[img]${url}[/img]`,
   emoji: (emoji: string) => `[${emoji}]`,
   quote: (text: string, author?: string) => (author ? `[quote="${author}"]${text}[/quote]` : `[quote]${text}[/quote]`),
-  code: (text: string, language?: string) => (language ? `[code=${language}]\n${text}\n[/code]` : `[code]\n${text}\n[/code]`),
+  code: (text: string, language?: string) =>
+    language ? `[code=${language}]\n${text}\n[/code]` : `[code]\n${text}\n[/code]`,
   style: (text: string, style: RequireSelected<StyleOptions, "size"> | RequireSelected<StyleOptions, "color">) => {
     let styleOptions = ``;
     if (style.size) {
