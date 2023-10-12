98ffa3898e5be7ff55d4eecb28d21d7b97344f00
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-12 13:01:47 +0200

    refactor: Inline Format Example Data
    Only using it for basic inline formatting we support.

--- packages/ckeditor5-coremedia-example-data/src/bbcode/InlineFormatData.ts
+++ packages/ckeditor5-coremedia-example-data/src/bbcode/InlineFormatData.ts
@@ -3,19 +3,35 @@ import { bbCode } from "./BBCode";
 
 const paragraphs = (...texts: string[]): string => texts.join(`\n\n`);
 
-export const inlineFormatData: ExampleData = {
-  Bold: paragraphs(`${bbCode.h1("Bold Text")}`, `Lorem ${bbCode.bold("ipsum")} dolor`),
-  Italic: paragraphs(`${bbCode.h1("Italic Text")}`, `Lorem ${bbCode.italic("ipsum")} dolor`),
-  Underline: paragraphs(`${bbCode.h1("Underline Text")}`, `Lorem ${bbCode.underline("ipsum")} dolor`),
-  Strikethrough: paragraphs(`${bbCode.h1("Strikethrough Text")}`, `Lorem ${bbCode.strikethrough("ipsum")} dolor`),
-  Styles: paragraphs(
-    `${bbCode.h1("Text Using Styles")}`,
-    `Lorem ${bbCode.style("ipsum", { size: "1.5em" })} dolor`,
-    `Lorem ${bbCode.style("ipsum", { color: "fuchsia" })} dolor`,
-    `Lorem ${bbCode.style("ipsum", { color: "#ff0000" })} dolor`,
-    `Lorem ${bbCode.style("ipsum", {
-      size: "1.5em",
-      color: "fuchsia",
-    })} dolor`,
+const inlineExamples = [
+  {
+    tag: "b",
+    name: "bold",
+    title: "Bold",
+  },
+  {
+    tag: "i",
+    name: "italic",
+    title: "Italic",
+  },
+  {
+    tag: "s",
+    name: "strikethrough",
+    title: "Strikethrough",
+  },
+  {
+    tag: "u",
+    name: "underline",
+    title: "Underline",
+  },
+];
+
+const inlineFormatDataEntries = inlineExamples.map(({ tag, name, title }) => [
+  title,
+  paragraphs(
+    `${bbCode.h1(`\\[${tag}\\] CKEditor 5 ${title} Style`)}`,
+    `Text can be represented in [${tag}]${name}[/${tag}] style.`,
   ),
-};
+]);
+
+export const inlineFormatData = Object.fromEntries(inlineFormatDataEntries) as ExampleData;
