5f4f498501fd8643b76d98c277325933fc9ad604
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-12 09:25:11 +0200

    refactor: Simplify And Mature BBCodeColor
    Now that we have the `RgbColor` representation for colors and the
    `getColor` utility in `CSSStyleDeclarations`, we can simplify and
    mature `BBCodeColor`:
    
    * Parsing is moved to the utility.
    * Due to the new parsing, we are not limited to color-names and hex-code
      representations for colors anymore.

--- packages/ckeditor5-bbcode/__tests__/BBCodeColor.test.ts
+++ packages/ckeditor5-bbcode/__tests__/BBCodeColor.test.ts
@@ -6,12 +6,13 @@ describe("BBCodeColor", () => {
     const rule = bbCodeColor;
 
     it.each`
-      dataView                                              | expected                         | comment
-      ${`<span style="color: #ff0000;">TEXT</span>`}        | ${`[color=#ff0000]TEXT[/color]`} | ${`BBob HTML 5 Preset Result (toView)`}
-      ${`<span style="color: #FF0000;">TEXT</span>`}        | ${`[color=#FF0000]TEXT[/color]`} | ${`ignore case`}
-      ${`<span style="color: fuchsia;">TEXT</span>`}        | ${`[color=fuchsia]TEXT[/color]`} | ${`supported color names`}
-      ${`<span style="color: #ccc;">TEXT</span>`}           | ${`[color=#ccc]TEXT[/color]`}    | ${`support shortened color codes`}
-      ${`<span style="color: rgb(255, 0, 0);">TEXT</span>`} | ${undefined}                     | ${`don't handle color codes, we cannot handle (yet?)`}
+      dataView                                                     | expected                           | comment
+      ${`<span style="color: #ff0000;">TEXT</span>`}               | ${`[color=#ff0000]TEXT[/color]`}   | ${`BBob HTML 5 Preset Result (toView)`}
+      ${`<span style="color: #FF0000;">TEXT</span>`}               | ${`[color=#ff0000]TEXT[/color]`}   | ${`ignore case`}
+      ${`<span style="color: fuchsia;">TEXT</span>`}               | ${`[color=fuchsia]TEXT[/color]`}   | ${`supported color names`}
+      ${`<span style="color: #ccc;">TEXT</span>`}                  | ${`[color=#cccccc]TEXT[/color]`}   | ${`support shortened color codes`}
+      ${`<span style="color: rgb(255, 0, 0);">TEXT</span>`}        | ${`[color=#ff0000]TEXT[/color]`}   | ${`also support rgb() codes`}
+      ${`<span style="color: rgba(255, 0, 0, 0.63);">TEXT</span>`} | ${`[color=#ff0000a0]TEXT[/color]`} | ${`also support rgba() codes`}
     `(
       "$[$#] Should process '$dataView' to '$expected' ($comment)",
       ({ dataView, expected }: { dataView: string; expected: string | undefined }) => {
--- packages/ckeditor5-bbcode/src/rules/BBCodeColor.ts
+++ packages/ckeditor5-bbcode/src/rules/BBCodeColor.ts
@@ -1,40 +1,5 @@
 import { BBCodeProcessingRule } from "./BBCodeProcessingRule";
-import { bbCodeLogger } from "../BBCodeLogger";
-
-/**
- * The parsing pattern is meant to accept well-known color names as well as
- * typical hex codes denoting a color, such as `#ff0000` or `#ccc`. The only
- * existing matching group `$1` will contain the matched color code.
- *
- * The following style values will match and provide a color code in group 1:
- *
- * ```text
- * color: #ff0000;
- * color: #FF0000;
- * color: #ff0000
- * color: #ccc;
- * color: fuchsia;
- * font-weight: bold; color: red; font-style: italic;
- * ```
- */
-const colorStylePattern = /(?:^|.*;\s*)color:\s*(\w+|#[a-f0-9]{3}|#[a-f0-9]{6})(?:\s*;.*$|$)/i;
-
-/**
- * Parses a supported color representation for transforming it into BBCode.
- * The method is meant to receive the whole style attribute value as string,
- * as in higher level API (thus, using `CSSStyleDeclaration.color`) may or will
- * already contain a parsed color format as RGB, that would need extra
- * transformation for BBCode. Instead, the `style` attribute value itself
- * will contain the color code, as it got set within CKEditor 5 without
- * additional magic applied.
- */
-const parseStyleColor = (style: string): string | undefined => {
-  const match = colorStylePattern.exec(style);
-  if (!match) {
-    return undefined;
-  }
-  return match[1];
-};
+import { getColor } from "@coremedia/ckeditor5-dom-support";
 
 /**
  * Processing rule for transforming a color style represented in HTML
@@ -47,25 +12,18 @@ export class BBCodeColor implements BBCodeProcessingRule {
   readonly id = "color";
   readonly tags = ["color"];
   toData(element: HTMLElement, content: string): undefined | string {
-    const logger = bbCodeLogger;
     const { style } = element;
-    // This is the already parsed color rgb() by default. We only support
-    // color names and hex-codes. For convenience, we use the parsed string to
-    // see if there is any color at all.
-    const { color } = style;
+    const color = getColor(style);
 
     if (color) {
-      const unparsedStyle = element.getAttribute("style") ?? "";
-      const parsedColor = parseStyleColor(unparsedStyle);
-      if (parsedColor) {
-        // Mark as parsed (thus, remove it)
-        style.removeProperty("color");
-        return `[color=${parsedColor}]${content}[/color]`;
+      style.removeProperty("color");
+      let colorRepresentation: string;
+      if (typeof color === "string") {
+        colorRepresentation = color;
       } else {
-        logger.debug(
-          `Skipped unsupported format for color in style attribute: ${unparsedStyle}. Color must be color name or valid hex-color string.`,
-        );
+        colorRepresentation = color.toHex();
       }
+      return `[color=${colorRepresentation}]${content}[/color]`;
     }
   }
 }
