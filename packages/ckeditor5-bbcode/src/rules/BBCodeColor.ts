fd3eec4125382085950dbb073a945dd8e9ca3fb9
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-12 12:20:29 +0200

    feat: Prefer Named Colors (Even More)
    While prior to this change, we only preferred color names if given
    explicitly in BBCode, we will now also prefer them (by default)
    when a color code can be mapped to some well-known W3C color name.
    
    This change also introduces a configuration option for the color
    mapper, so that customizations may veto this new mapping behavior.

--- packages/ckeditor5-bbcode/src/rules/BBCodeColor.ts
+++ packages/ckeditor5-bbcode/src/rules/BBCodeColor.ts
@@ -1,29 +1,76 @@
 import { BBCodeProcessingRule } from "./BBCodeProcessingRule";
-import { getColor } from "@coremedia/ckeditor5-dom-support";
+import { getColor, RgbColor } from "@coremedia/ckeditor5-dom-support";
+
+/**
+ * Maps a color to a representation suitable as unique argument to the
+ * `[color]` tag.
+ *
+ * If the mapper returns `undefined` (or empty string), no `[color]` tag will be
+ * created.
+ *
+ * One possible example for such a color mapper is mapping hex color codes
+ * to color names or even vice versa.
+ */
+export type ColorMapper = (color: string | RgbColor) => string | undefined;
+
+/**
+ * Color-mapper that prefers color names over hex- (or hex-alpha)
+ * representation. Thus, strings are returned as is, while `RgbColor`
+ * is transformed via `toColorNameOrHex()`.
+ *
+ * @param color - color to map to string
+ */
+export const defaultColorMapper: ColorMapper = (color: string | RgbColor): string => {
+  if (typeof color === "string") {
+    return color;
+  }
+  return color.toColorNameOrHex();
+};
+
+/**
+ * Configuration for `BBCodeColor`.
+ */
+export interface BBCodeColorConfig {
+  /**
+   * Used for mapping detected colors to a representation suitable for
+   * use in `[color]` tag.
+   */
+  mapper?: ColorMapper;
+}
 
 /**
  * Processing rule for transforming a color style represented in HTML
  * (by `color` style) to `[color=#ff0000]Text[/color]` in BBCode.
  *
- * The rule only accepts colors as color codes or as color names and ignores
- * all others.
+ * Due to the behavior of `CSSStyleDeclaration` color formats such as
+ * `rgb()`, `rgba()` or `hsl()` are supported as well and will all be
+ * transformed to hex or hex-alpha representation.
  */
 export class BBCodeColor implements BBCodeProcessingRule {
   readonly id = "color";
   readonly tags = ["color"];
+
+  readonly #mapper: ColorMapper;
+
+  /**
+   * Constructor with possible configuration options.
+   * @param config - configuration options
+   */
+  constructor(config: BBCodeColorConfig = {}) {
+    const { mapper = defaultColorMapper } = config;
+    this.#mapper = mapper;
+  }
+
   toData(element: HTMLElement, content: string): undefined | string {
     const { style } = element;
     const color = getColor(style);
 
     if (color) {
       style.removeProperty("color");
-      let colorRepresentation: string;
-      if (typeof color === "string") {
-        colorRepresentation = color;
-      } else {
-        colorRepresentation = color.toHex();
+      const mappedColor = this.#mapper(color);
+      if (mappedColor) {
+        return `[color=${mappedColor}]${content}[/color]`;
       }
-      return `[color=${colorRepresentation}]${content}[/color]`;
     }
   }
 }
@@ -32,7 +79,8 @@ export class BBCodeColor implements BBCodeProcessingRule {
  * Processing rule instance for transforming a color style represented in HTML
  * (by `color` style) to `[color=#ff0000]Text[/color]` in BBCode.
  *
- * The rule only accepts colors as color codes or as color names and ignores
- * all others.
+ * Due to the behavior of `CSSStyleDeclaration` color formats such as
+ * `rgb()`, `rgba()` or `hsl()` are supported as well and will all be
+ * transformed to hex or hex-alpha representation.
  */
 export const bbCodeColor = new BBCodeColor();
