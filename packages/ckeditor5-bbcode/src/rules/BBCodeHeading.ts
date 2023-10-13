9e7edc6b552eaac73507b419453d26b4ddc85fc2
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-13 10:44:01 +0200

    docs: BBCodeHeading

--- packages/ckeditor5-bbcode/src/rules/BBCodeHeading.ts
+++ packages/ckeditor5-bbcode/src/rules/BBCodeHeading.ts
@@ -1,7 +1,27 @@
 import { BBCodeProcessingRule } from "./BBCodeProcessingRule";
 
+/**
+ * Regular expression to match heading levels 1 to 6.
+ *
+ * Examples, that match:
+ *
+ * ```text
+ * h1
+ * h2
+ * ```
+ *
+ * **Named Groups:**
+ *
+ * * `level` (1): The matched heading level.
+ *
+ */
 const headingRegEx = /^h(?<level>[1-6])$/;
 
+/**
+ * General rule for any headings to map. Thus, supports BBCode tags
+ * `[h1]` to `[h6]`. While not supported by all vendors, it integrates
+ * nicely with the Heading Feature by CKEditor 5.
+ */
 export class BBCodeHeading implements BBCodeProcessingRule {
   readonly id = "heading";
   readonly tags = ["h1", "h2", "h3", "h4", "h5", "h6"];
@@ -15,6 +35,7 @@ export class BBCodeHeading implements BBCodeProcessingRule {
       // @ts-expect-error: https://github.com/microsoft/TypeScript/issues/32098
       const { level }: { level: string } = match.groups;
       const tag = `h${level}`;
+      // Adding newlines for minor pretty-printing.
       return `[${tag}]${content.trim()}[/${tag}]\n\n`;
     }
   }
