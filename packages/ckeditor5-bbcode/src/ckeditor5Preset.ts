38a6387a0c4fdb5ec12aea3fb5d591d395240610
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-18 12:25:37 +0200

    refactor: Dive into BBob API
    To address https://github.com/JiLiZART/BBob/issues/125 diving somewhat
    deeper into the BBob API. Assumption is, that the best place to
    respect paragraphs is the `process` method, as we require dedicated
    handling of text nodes and must not take them as is.
    
    The current state is more of "inlining the HTML5-Preset", so that we
    may start with experiments.

--- packages/ckeditor5-bbcode/src/ckeditor5Preset.ts
+++ packages/ckeditor5-bbcode/src/ckeditor5Preset.ts
@@ -1,10 +1,12 @@
-import { getUniqAttr, TagNode } from "@bbob/plugin-helper/es";
+import { getUniqAttr, isTagNode, TagNode } from "@bbob/plugin-helper/es";
 import { createPreset } from "@bbob/preset/es";
-import html5Preset from "@bbob/preset-html5/es";
+import html5DefaultTags from "@bbob/preset-html5/es/defaultTags";
+import { CoreTree } from "@bbob/core/es";
 
 type DefaultTags = Parameters<typeof createPreset>[0];
 type TagMappingFn = DefaultTags[string];
 type Core = Parameters<TagMappingFn>[1];
+type Options = Parameters<TagMappingFn>[2];
 type Tag = TagNode["tag"];
 type Content = TagNode["content"];
 type Attrs = TagNode["attrs"];
@@ -16,11 +18,29 @@ const toNode = (tag: Tag, attrs: Attrs, content: Content): TagNode => ({
 });
 
 /**
+ * Copy of default processor with adaptations to incorporate workaround for
+ * https://github.com/JiLiZART/BBob/issues/125.
+ */
+const process = (tags: DefaultTags, tree: CoreTree, core: Core, options: Options) => {
+  tree.walk((node) => (isTagNode(node) && tags[node.tag] ? tags[node.tag](node, core, options) : node));
+};
+
+/**
+ * Base Preset to modify parser, too. Possibly to be replaced once
+ * https://github.com/JiLiZART/BBob/issues/125 got resolved (newline handling).
+ *
+ * Note that if we stick to this custom preset, we could just modify the tags
+ * here (and export it as `ckeditor5Preset` directly). We keep this split-up
+ * state to possibly revert to extending the `html5Preset` instead.
+ */
+const basePreset: ReturnType<typeof createPreset> = createPreset(html5DefaultTags, process);
+
+/**
  * Extension of the HTML 5 Default Preset, that ships with BBob. It adapts
  * the given presets, so that they align with the expectations by CKEditor 5
  * regarding the representation in data view.
  */
-export const ckeditor5Preset: typeof html5Preset = html5Preset.extend(
+export const ckeditor5Preset: ReturnType<typeof createPreset> = basePreset.extend(
   (tags: DefaultTags): DefaultTags => ({
     ...tags,
     /**
--- packages/ckeditor5-bbcode/types/@bbob/core/es/index.d.ts
+++ packages/ckeditor5-bbcode/types/@bbob/core/es/index.d.ts
@@ -1,30 +1,40 @@
+export type CoreWalk = (callback: (node: unknown) => void) => void;
+
+export interface CoreOptions {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  parser?: Function;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  render?: Function;
+  skipParse?: boolean;
+  data?: null | unknown;
+  onlyAllowTags?: string[];
+  contextFreeTags?: string[];
+  enableEscapeTags?: boolean;
+  openTag?: string;
+  closeTag?: string;
+  onError?: (info: { message: string; tagName: string; lineNumber: number; columnNumber: number }) => void;
+}
+
+export interface CoreTree {
+  messages: string[];
+  options: CoreOptions;
+  walk: CoreWalk;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  match: Function;
+  [idx: number]: unknown;
+}
+
 export default function bbob(
   // eslint-disable-next-line @typescript-eslint/ban-types
   plugs?: Function | Function[],
 ): {
   process: (
     input: string | undefined,
-    opts?: {
-      // eslint-disable-next-line @typescript-eslint/ban-types
-      parser?: Function;
-      // eslint-disable-next-line @typescript-eslint/ban-types
-      render?: Function;
-      skipParse?: boolean;
-      data?: null | unknown;
-      onlyAllowTags?: string[];
-      contextFreeTags?: string[];
-      enableEscapeTags?: boolean;
-    },
+    opts?: CoreOptions,
   ) => {
     readonly html: string;
-    tree: {
-      messages: unknown[];
-      options: object;
-      walk: unknown;
-      // eslint-disable-next-line @typescript-eslint/ban-types
-      match: Function;
-    };
+    tree: CoreTree;
     raw: unknown;
-    messages: unknown[];
+    messages: CoreTree["messages"];
   };
 };
