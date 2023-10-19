b663cee14419d95f2296438a0fa243f293e73a23
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-19 16:11:56 +0200

    feat: Enrich Typings for BBob plugin-helper

--- packages/ckeditor5-bbcode/types/@bbob/plugin-helper/es/index.d.ts
+++ packages/ckeditor5-bbcode/types/@bbob/plugin-helper/es/index.d.ts
@@ -1,19 +1,19 @@
 type TagAttrs = Record<string, string>;
 
-declare module "@bbob/plugin-helper/es" {
-  declare class TagNode {
-    readonly tag: string;
-    attrs: TagAttrs;
-    content: (string | TagNode)[] | null;
-    append?: (value: string) => void;
-    static create: (tag: string, attrs: TagAttrs = {}, content: (string | TagNode)[] = []) => TagNode;
-    static isOf: (node: TagNode, type: string) => node is TagNode & { tag: typeof type };
-  }
-
-  declare function isTagNode(el: unknown): el is TagNode;
-  declare function isStringNode(el: unknown): el is string;
-
-  declare function getUniqAttr(attrs: Record<string, string>): string;
-
-  export { TagNode, getUniqAttr, isTagNode, isStringNode };
+declare class TagNode {
+  readonly tag: string;
+  attrs: TagAttrs;
+  content: (string | TagNode)[] | null;
+  append?: (value: string) => void;
+  static create: (tag: string, attrs: TagAttrs = {}, content: (string | TagNode)[] = []) => TagNode;
+  static isOf: (node: TagNode, type: string) => node is TagNode & { tag: typeof type };
 }
+
+declare function isTagNode(el: unknown): el is TagNode;
+declare function isStringNode(el: unknown): el is string;
+declare function isEOL(el: unknown): el is "\n";
+declare function escapeHTML(value: string): string;
+
+declare function getUniqAttr(attrs: Record<string, string>): string;
+
+export { TagNode, getUniqAttr, isEOL, isTagNode, isStringNode };
