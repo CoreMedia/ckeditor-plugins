f1362990407cea180ca92201c676efdda6c55203
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-19 16:14:08 +0200

    feat: Intermediate "Paragraph Support" Preset
    This is an intermediate commit to show the working paragraph support
    at least for nested block-elements. Still missing is support on root-
    level and of course a lot of clean-up.

--- packages/ckeditor5-bbcode/src/ckeditor5Preset.ts
+++ packages/ckeditor5-bbcode/src/ckeditor5Preset.ts
@@ -1,7 +1,8 @@
-import { getUniqAttr, isTagNode, TagNode } from "@bbob/plugin-helper/es";
+import { getUniqAttr, isEOL, isTagNode, TagNode } from "@bbob/plugin-helper/es";
 import { createPreset } from "@bbob/preset/es";
 import html5DefaultTags from "@bbob/preset-html5/es/defaultTags";
 import { CoreTree } from "@bbob/core/es";
+import { paragraphAwareContent } from "./bbob/Paragraphs";
 
 type DefaultTags = Parameters<typeof createPreset>[0];
 type TagMappingFn = DefaultTags[string];
@@ -11,7 +12,8 @@ type Tag = TagNode["tag"];
 type Content = TagNode["content"];
 type Attrs = TagNode["attrs"];
 
-const toNode = (tag: Tag, attrs: Attrs, content: Content): TagNode => ({
+// eslint-disable-next-line no-null/no-null
+const toNode = (tag: Tag, attrs: Attrs = {}, content: Content = null): TagNode => ({
   tag,
   attrs,
   content,
@@ -22,6 +24,42 @@ const toNode = (tag: Tag, attrs: Attrs, content: Content): TagNode => ({
  * https://github.com/JiLiZART/BBob/issues/125.
  */
 const process = (tags: DefaultTags, tree: CoreTree, core: Core, options: Options) => {
+  /*
+   * Brainstorming
+   *
+   * * On top-level it is clear, that each set of consecutive newlines
+   *   describes a paragraph.
+   *
+   * * Within inline-tags any paragraph is forbidden and should better be
+   *   ignored, possibly replaced by some BR? The only alternative here is
+   *   to ignore the newline(s).
+   *
+   * * We must not adapt anything, if we are inside a code-block. Newlines must
+   *   be kept as is.
+   *
+   * * We may leave it to explicit DefaultTags configuration, how newlines should
+   *   be handled within.
+   *
+   * * If we don't provide a "global handling", it requires any tag that should
+   *   handle doubled newlines, to be explicitly defined and not implicitly
+   *   converted. Such as `[h1]` is converted to `<h1>` without any further do.
+   *
+   * * Tags that should add special handling (thus, add paragraphs on demand):
+   *   [th], [td], [quote], [*] and "top level".
+   *
+   * * Regarding [*] Preset HTML transforms them to "li" TagNodes, which means
+   *   as an assumption that we can handle them with an extra [li] rule. Tests
+   *   show, that this is true, as the parents get processed prior to its
+   *   children. Thus, the rules will never process "*", as it already got
+   *   transformed to "li" when processing "list".
+   *
+   * * Having this, we could trick the processing, to create some intermediate
+   *   artificial child like "respect-paragraphs". This could then add
+   *   paragraphs where required. Only for root we need to find some other
+   *   solution, possibly introducing a virtual "root" even before handing
+   *   over to processing. The other option would be tracking the "depth"
+   *   within the process function and respond to "top-level" nodes.
+   */
   tree.walk((node) => (isTagNode(node) && tags[node.tag] ? tags[node.tag](node, core, options) : node));
 };
 
@@ -43,12 +81,41 @@ const basePreset: ReturnType<typeof createPreset> = createPreset(html5DefaultTag
 export const ckeditor5Preset: ReturnType<typeof createPreset> = basePreset.extend(
   (tags: DefaultTags): DefaultTags => ({
     ...tags,
+    td: (node) => {
+      // Must enable extra processing for `td` to handle nested paragraphs
+      // correctly.
+      node.content = paragraphAwareContent(node.content);
+      return node;
+    },
+    th: (node) => {
+      // Must enable extra processing for `th` to handle nested paragraphs
+      // correctly.
+      node.content = paragraphAwareContent(node.content);
+      return node;
+    },
+    li: (node) => {
+      // Processing the transformed result from BBob Preset HTML5. We must not
+      // return a node with the same name here, but instead we directly modify
+      // the content.
+      node.content = paragraphAwareContent(node.content);
+      return node;
+    },
+    quote: (node) => {
+      console.log("quote", {
+        node,
+        content: node.content,
+      });
+      return toNode("blockquote", {}, paragraphAwareContent(node.content, { requireParagraph: true }));
+    },
     /**
      * Adapted for CKEditor 5: CKEditor 5 requires a nested `<code>` element and
      * allows specifying the language via a corresponding class parameter.
      */
     code: (node: TagNode, { render }: Core): TagNode => {
       const language = getUniqAttr(node.attrs) || "plaintext";
+      // TODO: Possibly pass stripTags=true? We must prevent any plain script tag to appear.
+      // TODO: We may also need to prevent "onclick" etc. handlers, thus, strip any
+      //   unhandled attributes.
       const renderedCodeContent = render(node.content);
       // Remove some surrounding space from possible pretty-print BBCode.
       // If not doing so, you will see some extra lines above and below the
