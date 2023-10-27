e0f7c0a5d9e4736a411cbc78bce2f1a91cbbec95
Mark Michaelis <mark.michaelis@coremedia.com>
2023-10-27 09:30:27 +0200

    refactor: Extract "AttributeGuard"
    Extracting logic to secure attributes to extra module.

--- packages/ckeditor5-bbcode/src/bbob/renderHtmlDom.ts
+++ packages/ckeditor5-bbcode/src/bbob/renderHtmlDom.ts
@@ -3,21 +3,20 @@ import { CoreRenderable, CoreRenderer, CoreRenderNode } from "@bbob/core/es";
 import { isStringNode, isTagNode, TagAttrs } from "@bbob/plugin-helper/es";
 import { bbCodeLogger } from "../BBCodeLogger";
 import { RequireSelected } from "@coremedia/ckeditor5-common";
+import { AttributeGuard, defaultAttributeGuard } from "./AttributeGuard";
 
 const setAttributes = (
   element: HTMLElement,
   attrs: TagAttrs,
-  options: RequireSelected<HtmlDomRendererOptions, "isAttributeAllowed">,
+  tag: string,
+  options: RequireSelected<HtmlDomRendererOptions, "attributeGuard">,
 ): void => {
-  const { isAttributeAllowed } = options;
-  Object.entries(attrs)
-    .filter(([name, value]) =>
-      isAttributeAllowed(name, {
-        tag: element.localName,
-        owner: element,
-        value,
-      }),
-    )
+  const { attributeGuard } = options;
+  attributeGuard
+    .filteredEntries(attrs, {
+      owner: element,
+      tag,
+    })
     .forEach(([name, value]) => {
       try {
         element.setAttribute(name, value);
@@ -55,7 +54,7 @@ const renderDomNode = (node: CoreRenderable, options: Required<HtmlDomRendererOp
   }
 
   const element = document.createElement(tag);
-  setAttributes(element, attrs, options);
+  setAttributes(element, attrs, tag, options);
 
   element.append(...renderedContents);
 
@@ -82,14 +81,7 @@ export interface HtmlDomRendererOptions {
    * @param context.tag - node tag from BBCode processing
    * @param context.value - value, the attribute shall get
    */
-  isAttributeAllowed?: (
-    attrName: string,
-    context: {
-      owner: HTMLElement;
-      tag: string;
-      value: TagAttrs[string];
-    },
-  ) => boolean;
+  attributeGuard?: AttributeGuard;
   /**
    * If to strip any tags. If set on global level, only text-content will be
    * returned.
@@ -97,108 +89,8 @@ export interface HtmlDomRendererOptions {
   stripTags?: boolean;
 }
 
-/**
- * Rather broad set of protocols/URI-schemes, we respect as secure.
- * Note that blocking dedicated protocols is considered not enough.
- */
-const allowedProtocols = [
-  "about:",
-  "callto:",
-  "chrome:",
-  "facetime:",
-  "fax:",
-  "feed:",
-  "ftp:",
-  "geo:",
-  "git:",
-  "gopher:",
-  "gtalk:",
-  "http:",
-  "https:",
-  "im:",
-  "irc:",
-  "ircs:",
-  "jabber:",
-  "mailto:",
-  "news:",
-  "nntp:",
-  "rtmfp:",
-  "sftp:",
-  "skype:",
-  "soap:",
-  "spotify:",
-  "steam:",
-  "svn:",
-  "tel:",
-  "telnet:",
-  "xmpp:",
-];
-
-/**
- * Allows any attributes despite those starting with `on*`.
- * @param attrName - attribute name to validate.
- * @param context - context of attribute check
- * @param context.owner - owner element to set the attribute to
- * @param context.value - the value to set for the given attribute
- */
-const isAttributeAllowed: NonNullable<HtmlDomRendererOptions["isAttributeAllowed"]> = (
-  attrName: string,
-  { owner, value },
-): boolean => {
-  switch (attrName) {
-    case "href":
-      if (owner instanceof HTMLAnchorElement) {
-        return isAllowedUrl(value);
-      }
-      break;
-    case "src":
-      if (owner instanceof HTMLImageElement) {
-        return isAllowedUrl(value);
-      }
-      break;
-    default:
-      return !attrName.toLowerCase().startsWith("on");
-  }
-  return true;
-};
-
-/**
- * Validates if a URL is allowed. Validation focuses on the given protocol.
- * This function also URLs that are assumed to be relative.
- */
-const isAllowedUrl = (url: string): boolean => {
-  if (!url) {
-    // We allow empty URLs by default. They may be a result of named anchors
-    // to create. While not described to be supported by the `[url]` tag, we
-    // may end up with customizations that allow also [a id="top"] or similar.
-    return true;
-  }
-  let result: boolean;
-  let reason: string;
-  try {
-    // Will also fail with TypeError for relative URL references. Still, we
-    // do not check in advance, but validate this case later.
-    const parsedUrl = new URL(url);
-    const { protocol } = parsedUrl;
-    result = allowedProtocols.includes(protocol.toLowerCase());
-    reason = `validated protocol '${protocol}'`;
-  } catch (e) {
-    // On exception, there is some chance, that we deal with a relative URL.
-    // TODO: Do we want to allow relative URLs? While this may make sense
-    //   directly on the website, it poses some risk, that relative URLs
-    //   within Studio may trigger malicious behavior.
-    //   Example: A reviewer may add the following malicious BBCode to trick
-    //   an editor to do unwanted actions:
-    //     [url=/apps/workflow-app/]😈[/url]
-    result = !url.includes(":");
-    reason = `checked for assumed relative URL`;
-  }
-  bbCodeLogger.debug(`URL validation for '${url}: isValid=${result} (${reason})'`);
-  return result;
-};
-
 const defaultHtmlDomRendererOptions: Required<HtmlDomRendererOptions> = {
-  isAttributeAllowed,
+  attributeGuard: defaultAttributeGuard,
   stripTags: false,
 };
 
