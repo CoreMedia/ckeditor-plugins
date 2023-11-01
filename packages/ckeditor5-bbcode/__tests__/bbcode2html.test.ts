1c50f966e89bf86558c2fe45bd619a0b85996604
Mark Michaelis <mark.michaelis@coremedia.com>
2023-11-01 13:28:44 +0100

    test: Remove "on*" Handler Tests
    We don't promise filtering these attributes. It is up to the editing
    layers to robustly deal with corresponding data.

--- packages/ckeditor5-bbcode/__tests__/bbcode2html.test.ts
+++ packages/ckeditor5-bbcode/__tests__/bbcode2html.test.ts
@@ -107,25 +107,6 @@ describe("bbcode2html", () => {
   });
 
   describe("Security", () => {
-    describe("tainted on* handlers", () => {
-      it.each`
-        tainted
-        ${`[b onclick=">8-<~"]T[/b]`}
-        ${`[code onclick=">8-<~"]T[/code]`}
-        ${`[color=red onclick=">8-<~"]T[/color]`}
-        ${`[h1 onclick=">8-<~"]T[/h1]`}
-        ${`[i onclick=">8-<~"]T[/i]`}
-        ${`[list onclick=">8-<~"][* onclick=">8-<~"]T[/list]`}
-        ${`[quote onclick=">8-<~"]T[/quote]`}
-        ${`[s onclick=">8-<~"]T[/s]`}
-        ${`[table onclick=">8-<~"][tr onclick=">8-<~"][td onclick=">8-<~"]T[/td][/tr][/table]`}
-        ${`[u onclick=">8-<~"]T[/u]`}
-        ${`[url=https://example.org/ onclick=">8-<~"]T[/url]`}
-      `("[$#] Should prevent onclick-attack for: $tainted", ({ tainted }: { tainted: string }) => {
-        expect(aut.bbcode2html(tainted)).not.toContain("onclick");
-      });
-    });
-
     describe("Raw HTML Injections", () => {
       it.each`
         data            | expectedDataView            | comment
