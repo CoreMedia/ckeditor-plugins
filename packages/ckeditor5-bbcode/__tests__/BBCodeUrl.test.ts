5733e60a16f6ba7da8f07e3f3f0cb1d03fb54219
Mark Michaelis <mark.michaelis@coremedia.com>
2023-11-07 08:33:44 +0100

    test: Test toData Mapping of `[url]`

---
+++ packages/ckeditor5-bbcode/__tests__/BBCodeUrl.test.ts
@@ -0,0 +1,30 @@
+import { requireHTMLElement } from "./DOMUtils";
+import { bbCodeUrl } from "../src";
+
+describe("BBCodeUrl", () => {
+  describe("Default Configuration", () => {
+    const rule = bbCodeUrl;
+    const someUrl = "https://example.org/";
+
+    // noinspection HtmlUnknownAnchorTarget
+    it.each`
+      dataView                                                             | expected                                                              | comment
+      ${`<a href="${someUrl}">TEXT</a>`}                                   | ${`[url=${someUrl}]TEXT[/url]`}                                       | ${`Default Mapping Use-Case`}
+      ${`<a href="${someUrl}">${someUrl}</a>`}                             | ${`[url]${someUrl}[/url]`}                                            | ${`Pretty-Print: Shorten, if applicable`}
+      ${`<a href="${someUrl}?openBracket=[">TEXT</a>`}                     | ${`[url=${someUrl}?openBracket=%5B]TEXT[/url]`}                       | ${`Escape Open-Bracket [`}
+      ${`<a href="${someUrl}?closeBracket=]">TEXT</a>`}                    | ${`[url=${someUrl}?closeBracket=%5D]TEXT[/url]`}                      | ${`Escape Close-Bracket [`}
+      ${`<a href="${someUrl}?brackets=][">${someUrl}?brackets=\\]\\[</a>`} | ${`[url=${someUrl}?brackets=%5D%5B]${someUrl}?brackets=\\]\\[[/url]`} | ${`Escaping of text-content done by previous (outside) processing`}
+      ${`<a href="">TEXT</a>`}                                             | ${undefined}                                                          | ${`As there is no representation for "empty URLs" in BBCode, not mapping to [url]`}
+      ${`<a href="/relative">TEXT</a>`}                                    | ${`[url=/relative]TEXT[/url]`}                                        | ${`Keep relative URLs (1)`}
+      ${`<a href="?search=param">TEXT</a>`}                                | ${`[url=?search=param]TEXT[/url]`}                                    | ${`Keep relative URLs, search-param only (2)`}
+      ${`<a href="#hash">TEXT</a>`}                                        | ${`[url=#hash]TEXT[/url]`}                                            | ${`Keep relative URLs, hash-param only (3)`}
+    `(
+      "$[$#] Should process '$dataView' to '$expected' ($comment)",
+      ({ dataView, expected }: { dataView: string; expected: string | undefined }) => {
+        const element = requireHTMLElement(dataView);
+        const bbCode = rule.toData(element, element.textContent ?? "");
+        expect(bbCode).toEqual(expected);
+      },
+    );
+  });
+});
