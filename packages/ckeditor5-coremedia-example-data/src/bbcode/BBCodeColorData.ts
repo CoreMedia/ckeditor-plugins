cf7e7b568cfbdadbcb8de04a9ab5240b59609ab9
Mark Michaelis <mark.michaelis@coremedia.com>
2023-11-16 10:02:24 +0100

    refactor: [color] Examples
    Matured and extended [color] examples (and got rid of this weird
    BBCode generation utility in here).

--- packages/ckeditor5-coremedia-example-data/src/bbcode/BBCodeColorData.ts
+++ packages/ckeditor5-coremedia-example-data/src/bbcode/BBCodeColorData.ts
@@ -1,4 +1,3 @@
-import { bbCode } from "./BBCode";
 import { ExampleData } from "../ExampleData";
 
 const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
@@ -6,12 +5,19 @@ const bbCodeRainbow = Array.from("rainbow")
   .map((c, idx) => `[color=${rainbow[idx]}]${c}[/color]`)
   .join("");
 
-const text = `${bbCode.h1(`\\[color\\]: CKEditor 5 Font Colors in BBCode`)}\
-${bbCode.p(`\
-The CKEditor 5 BBCode Plugin supports the \\[color\\] tag with colors either \
-given in hex or as color names.`)}\
-${bbCode.h2("The Colors of the Rainbow")}
-${bbCode.p(`A ${bbCodeRainbow} has very nice colors.`)}
+const text = `\
+[h1]\\[color\\]: CKEditor 5 Font Colors in BBCode[/h1]
+
+The CKEditor 5 BBCode Plugin supports the \\[color\\] tag with colors either
+given in hex or as color names.
+
+[h2]The Colors of the Rainbow[/h2]
+
+A ${bbCodeRainbow} has very nice colors:
+
+[list]
+${rainbow.map((color) => `[*] [color=${color}]${color}[/color]`).join("\n")}
+[/list]
 `;
 
 export const colorData: ExampleData = {
