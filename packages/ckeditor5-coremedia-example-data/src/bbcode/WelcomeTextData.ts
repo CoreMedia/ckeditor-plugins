af1e2ac122f040a9301252a6664510614ae362e5
Mark Michaelis <mark.michaelis@coremedia.com>
2023-11-16 10:22:08 +0100

    refactor: BBCode Example Welcome Text

--- packages/ckeditor5-coremedia-example-data/src/bbcode/WelcomeTextData.ts
+++ packages/ckeditor5-coremedia-example-data/src/bbcode/WelcomeTextData.ts
@@ -1,18 +1,18 @@
 import { ExampleData } from "../ExampleData";
-import { bbCode } from "./BBCode";
 
-export const welcomeText = `${bbCode.h1("CKEditor 5: CoreMedia Plugin Showcase")}\
-${bbCode.p(`\
-This example instance of CKEditor 5 serves as a showcase for plugins provided \
-by CoreMedia. Most of these plugins are mandatory to use CKEditor 5 as editor \
-within CoreMedia Studio. Others are required to edit CoreMedia RichText, and \
-then again others provide more additional functionality. For details see \
-corresponding documentation.`)}\
-${bbCode.h2("Example Data")}\
-${bbCode.p(`
+export const welcomeText = `\
+[h1]CKEditor 5: CoreMedia BBCode Plugin Showcase[/h1]
+
+This example instance of CKEditor 5 serves as a showcase for the BBCode
+Plugin provided by CoreMedia (along with a so-called [i]Data Facade[/i]
+plugin).
+
+[h2]Example Data[/h2]
+
 For testing several use-cases you will see buttons at the top, which load
 different data-sets for testing and for experimenting with this CKEditor
-instance and its plugins.`)}`;
+instance and its plugins.
+`;
 
 export const welcomeTextData: ExampleData = {
   Welcome: welcomeText,
