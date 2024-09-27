a5d3c7d991bb354e5be854806a2414d7133d68c7
kliesch <pkliesch@coremedia.com>
2024-09-27 09:30:16 +0200

    delete tests

--- packages/ckeditor5-coremedia-richtext/__tests__/rules/TestDirection.ts
+++
@@ -1,9 +0,0 @@
-export const bijective = "<=>";
-export const toView = "=>";
-export const toData = "<=";
-type Bijective = typeof bijective;
-type ToView = typeof toView;
-type ToData = typeof toData;
-export type TestDirection = Bijective | ToView | ToData;
-export const isToView = (direction: TestDirection): boolean => [bijective, toView].includes(direction);
-export const isToData = (direction: TestDirection): boolean => [bijective, toData].includes(direction);
