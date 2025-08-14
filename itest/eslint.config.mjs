248f1a23be3bf7998dabafd5b1ca17568b117fae
Jens Meisner <jmeisner@coremedia.com>
2025-08-14 14:31:26 +0200

    upgrade eslint to 9.33.0 and update to new config and add lint script for each package

---
+++ itest/eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "eslint/config";
+import tsParser from "@typescript-eslint/parser";
+import { configs as workspaceConfig } from "../eslint.config.mjs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+export default defineConfig([
+  ...workspaceConfig,
+  {
+    languageOptions: {
+      parser: tsParser
+    }
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: dirname(fileURLToPath(import.meta.url));,
+      project: "./tsconfig.json"
+    }
+  }
+}
+]);
