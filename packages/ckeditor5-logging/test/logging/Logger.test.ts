6eac5e49d52966b4fab5c4e0e3902e351d51dd51
Jens Meisner <jmeisner@coremedia.com>
2025-10-13 09:13:33 +0200

    fix tests in logging

--- packages/ckeditor5-logging/test/logging/Logger.test.ts
+++ packages/ckeditor5-logging/test/logging/Logger.test.ts
@@ -1,156 +1,185 @@
+import type { TestContext } from "node:test";
+import test from "node:test";
+import expect from "expect";
 import LoggerProvider from "../../src/logging/LoggerProvider";
-// TODO[cke] Works in IDE, but not from console build. What's wrong here?
-//import "@types/jest";
 
-const mockFunctionName = "MOCKCONSTRUCTOR";
+const mockFunctionName = "ORIGINAL";
 const loggerName = "loggerName";
 const someMessage = "I'm logging stuff";
 const contextPrefix = `[${mockFunctionName}] ${loggerName}:`;
 
-beforeEach(() => {
-  setupConsoleMocks();
-});
+type Mocked<T> = T & { mock: { calls: { arguments: unknown[] }[] } };
 
-test("should log nothing when log level is none", () => {
+function mockConsole(t: TestContext) {
+  console.debug = t.mock.fn();
+  console.log = t.mock.fn();
+  console.info = t.mock.fn();
+  console.warn = t.mock.fn();
+  console.error = t.mock.fn();
+
+  return console as typeof console & {
+    debug: Mocked<typeof console.debug>;
+    log: Mocked<typeof console.log>;
+    info: Mocked<typeof console.info>;
+    warn: Mocked<typeof console.warn>;
+    error: Mocked<typeof console.error>;
+  };
+}
+
+void test("should log nothing when log level is none", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=none`;
 
   logToAll();
 
-  expect(console.debug).toHaveBeenCalledTimes(0);
-  expect(console.info).toHaveBeenCalledTimes(0);
-  expect(console.warn).toHaveBeenCalledTimes(0);
-  expect(console.error).toHaveBeenCalledTimes(0);
-  expect(console.log).toHaveBeenCalledTimes(0);
+  expect(mockedConsole.debug.mock.calls.length).toBe(0);
+  expect(mockedConsole.info.mock.calls.length).toBe(0);
+  expect(mockedConsole.warn.mock.calls.length).toBe(0);
+  expect(mockedConsole.error.mock.calls.length).toBe(0);
+  expect(mockedConsole.log.mock.calls.length).toBe(0);
 });
 
-test("should log everything when log level is debug", () => {
+void test("should log everything when log level is debug", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=debug`;
 
   logToAll();
 
-  expect(console.debug).toHaveBeenCalledTimes(1);
-  expect(console.info).toHaveBeenCalledTimes(1);
-  expect(console.warn).toHaveBeenCalledTimes(1);
-  expect(console.error).toHaveBeenCalledTimes(1);
-  expect(console.log).toHaveBeenCalledTimes(1);
+  expect(mockedConsole.debug.mock.calls.length).toBe(1);
+  expect(mockedConsole.info.mock.calls.length).toBe(1);
+  expect(mockedConsole.warn.mock.calls.length).toBe(1);
+  expect(mockedConsole.error.mock.calls.length).toBe(1);
+  expect(mockedConsole.log.mock.calls.length).toBe(1);
 });
 
-test("should log everything above and including level info when log level is info", () => {
+void test("should log everything above and including level info when log level is info", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=info`;
 
   logToAll();
 
-  expect(console.debug).toHaveBeenCalledTimes(0);
-  expect(console.info).toHaveBeenCalledTimes(1);
-  expect(console.warn).toHaveBeenCalledTimes(1);
-  expect(console.error).toHaveBeenCalledTimes(1);
-  expect(console.log).toHaveBeenCalledTimes(1);
+  expect(mockedConsole.debug.mock.calls.length).toBe(0);
+  expect(mockedConsole.info.mock.calls.length).toBe(1);
+  expect(mockedConsole.warn.mock.calls.length).toBe(1);
+  expect(mockedConsole.error.mock.calls.length).toBe(1);
+  expect(mockedConsole.log.mock.calls.length).toBe(1);
 });
 
-test("should default to info logging for no log level provided", () => {
+void test("should default to info logging for no log level provided", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}`;
 
   logToAll();
 
-  expect(console.debug).toHaveBeenCalledTimes(0);
-  expect(console.info).toHaveBeenCalledTimes(1);
-  expect(console.warn).toHaveBeenCalledTimes(1);
-  expect(console.error).toHaveBeenCalledTimes(1);
-  expect(console.log).toHaveBeenCalledTimes(1);
+  expect(mockedConsole.debug.mock.calls.length).toBe(0);
+  expect(mockedConsole.info.mock.calls.length).toBe(1);
+  expect(mockedConsole.warn.mock.calls.length).toBe(1);
+  expect(mockedConsole.error.mock.calls.length).toBe(1);
+  expect(mockedConsole.log.mock.calls.length).toBe(1);
 });
 
-test("should log everything above and including level warn when log level is warn", () => {
+void test("should log everything above and including level warn when log level is warn", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=warn`;
 
   logToAll();
 
-  expect(console.debug).toHaveBeenCalledTimes(0);
-  expect(console.info).toHaveBeenCalledTimes(0);
-  expect(console.warn).toHaveBeenCalledTimes(1);
-  expect(console.error).toHaveBeenCalledTimes(1);
-  expect(console.log).toHaveBeenCalledTimes(1);
+  expect(mockedConsole.debug.mock.calls.length).toBe(0);
+  expect(mockedConsole.info.mock.calls.length).toBe(0);
+  expect(mockedConsole.warn.mock.calls.length).toBe(1);
+  expect(mockedConsole.error.mock.calls.length).toBe(1);
+  expect(mockedConsole.log.mock.calls.length).toBe(1);
 });
 
-test("should log everything above and including level error when log level is error", () => {
+void test("should log everything above and including level error when log level is error", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=error`;
 
   logToAll();
 
-  expect(console.debug).toHaveBeenCalledTimes(0);
-  expect(console.info).toHaveBeenCalledTimes(0);
-  expect(console.warn).toHaveBeenCalledTimes(0);
-  expect(console.error).toHaveBeenCalledTimes(1);
-  expect(console.log).toHaveBeenCalledTimes(1);
+  expect(mockedConsole.debug.mock.calls.length).toBe(0);
+  expect(mockedConsole.info.mock.calls.length).toBe(0);
+  expect(mockedConsole.warn.mock.calls.length).toBe(0);
+  expect(mockedConsole.error.mock.calls.length).toBe(1);
+  expect(mockedConsole.log.mock.calls.length).toBe(1);
 });
 
-test("should log on debug when log level is debug", () => {
+void test("should log on debug when log level is debug", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=debug`;
 
   LoggerProvider.getLogger("loggerName").debug(someMessage);
 
-  expect(console.debug).toHaveBeenCalledWith(contextPrefix, someMessage);
+  expect(mockedConsole.debug.mock.calls[0].arguments).toStrictEqual([contextPrefix, someMessage]);
 });
 
-test("should log on info when log level is info", () => {
+void test("should log on info when log level is info", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=info`;
 
   LoggerProvider.getLogger("loggerName").info(someMessage);
 
-  expect(console.info).toHaveBeenCalledWith(contextPrefix, someMessage);
+  expect(mockedConsole.info.mock.calls[0].arguments).toStrictEqual([contextPrefix, someMessage]);
 });
 
-test("should log on warn when log level is warn", () => {
+void test("should log on warn when log level is warn", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=warn`;
 
   LoggerProvider.getLogger("loggerName").warn(someMessage);
 
-  expect(console.warn).toHaveBeenCalledWith(contextPrefix, someMessage);
+  expect(mockedConsole.warn.mock.calls[0].arguments).toStrictEqual([contextPrefix, someMessage]);
 });
 
-test("should log on error when log level is error", () => {
+void test("should log on error when log level is error", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = `#${loggerName}=error`;
 
   LoggerProvider.getLogger("loggerName").error(someMessage);
 
-  expect(console.error).toHaveBeenCalledWith(contextPrefix, someMessage);
+  expect(mockedConsole.error.mock.calls[0].arguments).toStrictEqual([contextPrefix, someMessage]);
 });
 
-test("should log on debug when root logger is on debug", () => {
+void test("should log on debug when root logger is on debug", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = "#ckdebug=debug";
 
   LoggerProvider.getLogger("loggerName").debug(someMessage);
 
-  expect(console.debug).toHaveBeenCalledTimes(1);
+  expect(mockedConsole.debug.mock.calls[0].arguments).toStrictEqual([contextPrefix, someMessage]);
 });
 
-test("Backwards compatibility: Should accept verbose as alias for debug", () => {
+void test("Backwards compatibility: Should accept verbose as alias for debug", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = "#ckdebug=verbose";
 
   LoggerProvider.getLogger("loggerName").debug(someMessage);
 
-  expect(console.debug).toHaveBeenCalledTimes(1);
+  expect(mockedConsole.debug.mock.calls[0].arguments).toStrictEqual([contextPrefix, someMessage]);
 });
 
-test("should log on all but debug when root logger is enabled without explicit level", () => {
+void test("should log on all but debug when root logger is enabled without explicit level", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = "#ckdebug";
 
   logToAll();
 
-  expect(console.debug).toHaveBeenCalledTimes(0);
-  expect(console.info).toHaveBeenCalledTimes(1);
-  expect(console.warn).toHaveBeenCalledTimes(1);
-  expect(console.error).toHaveBeenCalledTimes(1);
-  expect(console.log).toHaveBeenCalledTimes(1);
+  expect(mockedConsole.debug.mock.calls.length).toBe(0);
+  expect(mockedConsole.info.mock.calls.length).toBe(1);
+  expect(mockedConsole.warn.mock.calls.length).toBe(1);
+  expect(mockedConsole.error.mock.calls.length).toBe(1);
+  expect(mockedConsole.log.mock.calls.length).toBe(1);
 });
 
-test("should log on logger name level when logger name and ckdebug are specified", () => {
+void test("should log on logger name level when logger name and ckdebug are specified", (t) => {
+  const mockedConsole = mockConsole(t);
   window.location.hash = "#ckdebug=debug&loggerName=info";
 
   LoggerProvider.getLogger("loggerName").info(someMessage);
   LoggerProvider.getLogger("loggerName").debug("Logging stuff on debug");
 
-  expect(console.info).toHaveBeenCalledWith(contextPrefix, someMessage);
-  expect(console.debug).toHaveBeenCalledTimes(0);
+  expect(mockedConsole.info.mock.calls[0].arguments).toStrictEqual([contextPrefix, someMessage]);
+  expect(mockedConsole.debug.mock.calls.length).toBe(0);
 });
 
 /**
@@ -162,12 +191,4 @@ function logToAll(): void {
   LoggerProvider.getLogger(loggerName).warn(someMessage);
   LoggerProvider.getLogger(loggerName).error(someMessage);
   LoggerProvider.getLogger(loggerName).log(someMessage);
-}
-
-function setupConsoleMocks(): void {
-  console.debug = jest.fn();
-  console.log = jest.fn();
-  console.info = jest.fn();
-  console.warn = jest.fn();
-  console.error = jest.fn();
 }
