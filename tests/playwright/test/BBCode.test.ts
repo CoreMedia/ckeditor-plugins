865f715c6843315ee5c8f87f8ccafb87bef7fbb3
Jens Meisner <jmeisner@coremedia.com>
2026-06-01 14:56:15 +0200

    added test BBCode

---
+++ tests/playwright/test/BBCode.test.ts
@@ -0,0 +1,16 @@
+import { ApplicationWrapper } from "./aut/ApplicationWrapper";
+import { editor } from "./locators/editor";
+import { bbCodeUrl } from "./utils/environment";
+import { test } from "./base";
+
+/**
+ * Tests the BBCode feature.
+ */
+test("BBCode: Should show a bold word as strong-tag", async ({ page }) => {
+  await page.goto(bbCodeUrl);
+  await editor(page).waitFor();
+  const application = new ApplicationWrapper(page);
+  const { editor: editorWrapper } = application;
+  await editorWrapper.setData("[b]boldword[/b]");
+  await page.locator(`strong`, { hasText: "boldword" }).waitFor();
+});
